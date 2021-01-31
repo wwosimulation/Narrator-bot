f259df6f0a961efb1e5123e46e597df894a95f31
Shadow <thebotoftheshadow@gmail.com>
2021-01-30 22:42:25 -0600

    Add self to emergency stop

--- commands/emergencystop.js
+++ commands/emergencystop.js
@@ -3,7 +3,7 @@ module.exports = {
   aliases: ["es", "yesstop", "reboot"], 
   run: async (message, args, client) => {
     let nar = message.guild.roles.cache.find(r => r.name === "Narrator")
-    if (message.member.roles.cache.has(nar.id)) {
+    if (message.member.roles.cache.has(nar.id) || message.author.id == "439223656200273932") {
       message.channel.send('Oh no! Looks like i am executing a bug that is unstoppable. Please wait while i reboot.') 
       client.user.setStatus("offline")
       require("node-cmd").run(`pm2 restart ${process.env.pm_id}`)
