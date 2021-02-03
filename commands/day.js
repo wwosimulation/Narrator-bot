ff564ed0de2dec9492917625c369e0002bd51e5e
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-03 19:08:32 +0800

    Update day.js

--- commands/day.js
+++ commands/day.js
@@ -1682,12 +1682,14 @@ module.exports = {
 
           // adding the doused players to the list
           if (douses[j] != "0") {
+            if (guy) {
             if (guy.roles.cache.has(alive.id)) {
               let allDouses = db.get(`doused_${arso[i]}`)
               allDouses.push(douses[j])
               db.set(`doused_${arso[i]}`, allDouses)
               let arsoguy = message.guild.members.cache.find(m => m.nickname === douses[j])
               message.guild.channels.cache.get(arso[i]).send(`<:douse:744574203025686568> Player **${douses[j]} ${arsoguy.user.username}** has been doused!`)
+            }
             }
           }
         }
