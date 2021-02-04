806bbadf3da106080ed299774ea692ba9e5245df
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-04 14:02:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const commandFiles = fs
   .readdirSync("./commands")
   .filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const command = require(`./commands/${file}`)
   client.commands.set(command.name, command);
 }
 
