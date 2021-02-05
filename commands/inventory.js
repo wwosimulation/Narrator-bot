13c387bd80088e54f06a4beaa126f9371859735f
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-05 22:33:05 +0800

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -17,8 +17,8 @@ module.exports = {
                     .setTitle("Inventory")
                     .setAuthor(message.author.tag, message.author.avatarURL())
                     .setDescription("Here is where all your good stuff are!")
-                    .addField("Coins", coins)
-                    .addField("Roses", `Roses (Bought): ${rosesG}\nRoses: ${roses}\nBouquets: ${roseB}`)
+                    .addField("Coins", coins + "<:coin:606434686931173377>")
+                    .addField("Roses", `Roses (Bought): ${roseG}<:rosesingle:807256844191793158>\nRoses: ${roses}<:rosesingle:807256844191793158>\nBouquets: ${roseB}`)
                     .addField("Lootboxes", lootbox)
 
         if (custom != "None") {
@@ -27,7 +27,7 @@ module.exports = {
             roles.forEach(e => {
                 msgroles += `${client.emojis.cache.find(r => r.name === e.toLowerCase().replace(" ", "_"))} ${e}`
             })
-            embed.addField("Custom Maker Item", `To buy roles, use \`+cmi role [role name]\`. Available Roles:\n<:villager:606429671772520468> Villager\n<:gunner:606429179914878996> Gunner\n<:doctor:606429602679881755> Doctor\n<:bodyguard:606431685885820928> Bodyguard\n<:seer:606428943960244227> Seer\n<:jailer:606429387419549696> Jailer\n<:priest:606429079880859659> Priest\n<:aura_seer:606428815027077121> Aura Seer\n<:medium:606430034684543006> Medium\n<:werewolf:606430776023580697> Werewolf\n<:alpha_werewolf:606430945062682635> Alpha Werewolf\n<:wolf_seer:606430871079223297> Wolf Seer\n<:wolf_shaman:607514754210070542> Wolf Shaman\n<:fool:606431284885192744> Fool\n<:headhunter:606431218778636298> Headhunter\n<:serial_killer:606431520953204736> Serial Killer`)
+            embed.addField("Custom Maker Item", `To buy roles, use \`+cmi buy [role name]\`. Available Roles:\n<:villager:606429671772520468> Villager\n<:gunner:606429179914878996> Gunner\n<:doctor:606429602679881755> Doctor\n<:bodyguard:606431685885820928> Bodyguard\n<:seer:606428943960244227> Seer\n<:jailer:606429387419549696> Jailer\n<:priest:606429079880859659> Priest\n<:aura_seer:606428815027077121> Aura Seer\n<:medium:606430034684543006> Medium\n<:werewolf:606430776023580697> Werewolf\n<:alpha_werewolf:606430945062682635> Alpha Werewolf\n<:wolf_seer:606430871079223297> Wolf Seer\n<:wolf_shaman:607514754210070542> Wolf Shaman\n<:fool:606431284885192744> Fool\n<:headhunter:606431218778636298> Headhunter\n<:serial_killer:606431520953204736> Serial Killer`)
         }
     }
 }
