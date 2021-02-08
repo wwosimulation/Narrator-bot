2bd9423ea8b43e8b6c0de72e737caa848c9e89c8
Shadow <william.shadow@outlook.com>
2021-02-08 16:33:12 +0000

    fix hostcheck

--- commands/hostcheck.js
+++ commands/hostcheck.js
@@ -7,9 +7,17 @@ module.exports = {
       message.author.id == "552814709963751425" ||
       message.author.id == "439223656200273932"
     ) {
-      cmd.get("git rev-parse --short HEAD", (data) =>
-        message.channel.send("I am currently running on commit \`" + data + "\`")
-      );
+      const { exec } = require("child_process");
+      exec("git rev-parse --short HEAD", (err, stdout, stderr) => {
+        if (err) {
+          //some err occurred
+          message.channel.send("An error has occured")
+          console.error(err);
+        } else {
+          // the *entire* stdout and stderr (buffered)+
+          message.channel.send("I am currently running on commit `" + stdout + "`");
+        }
+      });
     }
   },
 };
