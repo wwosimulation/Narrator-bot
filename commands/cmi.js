c0be9db08f2714faddfe7f3a270f070d6b7f9bb5
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-09 09:47:34 +0800

    Update cmi.js

--- commands/cmi.js
+++ commands/cmi.js
@@ -97,7 +97,7 @@ module.exports = {
         })
         coins75.forEach(e => {
 	  ehek += `${client.emojis.cache.find(x => x.name === e.toLowerCase().replace(" ", "_"))} ${e} - 75<:coin:606434686931173377>\n`
-	  if (args.length == 0 && ehek.length > 2000) {
+	  if (args.length == 0 && ehek.length > 1980) {
 	  	message.channel.send(
 			new Discord.MessageEmbed() 
 			.setTitle("Roles to buy:") 
@@ -111,7 +111,7 @@ module.exports = {
         })
         coins250.forEach(e => {
 		ehek += `${client.emojis.cache.find(x => x.name === e.toLowerCase().replace(" ", "_"))} ${e} - 75<:coin:606434686931173377>\n`
-		if (args.length == 0 && ehek.length > 2000) {
+		if (args.length == 0 && ehek.length > 1980) {
 	  		message.channel.send(
 			new Discord.MessageEmbed() 
 			.setTitle("Roles to buy:") 
@@ -125,7 +125,7 @@ module.exports = {
         })
         coins1000.forEach(e => {
 	  ehek += `${client.emojis.cache.find(x => x.name === e.toLowerCase().replace(" ", "_"))} ${e} - 1000<:coin:606434686931173377>\n` 
-	  if (args.length == 0 && ehek.length > 2000) {
+	  if (args.length == 0 && ehek.length > 1980) {
 	  		message.channel.send(
 			new Discord.MessageEmbed() 
 			.setTitle("Roles to buy:") 
