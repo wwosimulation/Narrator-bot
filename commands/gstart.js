9c3a5de7f0a6aa13d2b9f2e9f5a5c5456265ba3e
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-09 12:57:45 +0800

    Update gstart.js

--- commands/gstart.js
+++ commands/gstart.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: "gstart",
 	run: async (message, args, client) => {
 		
-		let gwa = client.guild.channels.cache.get("606123798369927168")
+		let gwa = client.channels.cache.get("606123798369927168")
 		if (gwa.permissionsFor(message.member).has(["SEND_MESSAGES"])) {
 			if (args.length < 2) return message.channel.send("You need at least 2 arguments. One for the time and the prize. Optionally you can insert the winner amount. The default winner amount is 1.")
 			let winners = 1
