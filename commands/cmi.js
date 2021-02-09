02811b37784e732e02b2c53bc6deb620fd06765c
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-09 09:11:49 +0800

    Update cmi.js

--- commands/cmi.js
+++ commands/cmi.js
@@ -97,7 +97,7 @@ module.exports = {
         })
         coins75.forEach(e => {
 	  ehek += `${client.emojis.cache.find(x => x.name === e.toLowerCase().replace(" ", "_"))} ${e} - 75<:coin:606434686931173377>\n`
-	  if (args.length == 0 && ehek.length > 2009 && msg.length < 2010) {
+	  if (args.length == 0 && ehek.length > 2000 && msg.length < 2010) {
 	  	message.channel.send(
 			new Discord.MessageEmbed() 
 			.setTitle("Roles to buy:") 
@@ -111,7 +111,7 @@ module.exports = {
         })
         coins250.forEach(e => {
 		ehek += `${client.emojis.cache.find(x => x.name === e.toLowerCase().replace(" ", "_"))} ${e} - 75<:coin:606434686931173377>\n`
-		if (args.length == 0 && ehek.length > 2009 && msg.length < 2010) {
+		if (args.length == 0 && ehek.length > 2000 && msg.length < 2010) {
 	  		message.channel.send(
 			new Discord.MessageEmbed() 
 			.setTitle("Roles to buy:") 
