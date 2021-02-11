4dd74e0d962ee97988d509d7c9e81412d3b3c22a
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-11 17:53:46 +0800

    Update as.js

--- commands/as.js
+++ commands/as.js
@@ -3,7 +3,7 @@ const db = require("quick.db")
 module.exports = {
   name: "as",
   run: async (message, args, client) => {
-     let user = client.users.cache.get(args[0])
+     let user = client.users.cache.get(args[0]) || client.users.cache.find(u => u.username === args[0]) || client.users.cache.find(u => u.tag === args[0])
      if (!user) return message.channel.send("Unknown user!")
      let num = parseInt(args[1])
      if (!num) return message.channel.send("Dumb, that is not a number")
