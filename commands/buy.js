626ba4c2e7546edf94c6518798d8756e00e2d0d2
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-14 14:35:51 +0800

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,1 +1,11 @@
 const Discord = require("discord.js")
+const db = require("quick.db")
+
+module.exports = {
+    name: "buy",
+    run: async (message, args, client) => {
+        if (args.length < 1) return message.channel.send("Now i want you to think really hard about this one.")
+      
+        
+    }
+}
