f2fa761d7252cb7d738aaad68f1a5fe445ddf8d8
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-14 15:15:36 +0800

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -9,29 +9,22 @@ module.exports = {
         if (!args[0] || args[0] == '1') {
             message.channel.send(new Discord.MessageEmbed()
             .setTitle("Werewolf Online Simulation Store")
-            .setColor("#1FFF43")
-            .addField("DJ Role - 450 coins", "You can use this role to play your own songs in the game server!")
-            .addField("Red Colour - 50 coins", "Sassy red makes you look uh... well sassy!")
-            .addField("Blue Colour - 50 coins", "Blue, as calm as the sky. Get lost, you aren't even near to calm.")
-            .addField("Green Colour - 50 coins", "Buy this or suffer for eternity.")
-            .addField("Yellow Colour - 50 coins", "Eww, what a dirty colour. Unless you're a f\*\*\*boy or dirty, just do us a favour and don't buy this.")
-            .addField("Black Colour - 50 coins", "Yes, black. Like everyone's heart. In a space vacuum. Where no one can exist.")
-            .addField("Salmon Colour - 50 coins", "Yummy Salmon. Buy this so i can buy a salmon for myself!")
-            .addField("Pink Colour - 50 coins", "By buying this colour, you agree that you are too girly and watch \"My Little Pony\"")
-            .addField("Turquoise Colour - 50 coins", "Since this is an another version of green, buy or die!")
-            .addField("Crimson Colour - 50 coins", "What the heck is even a crimson...")
-            .addField("Special Role - 500 coins", "Finally, have the role name and colour as your type goofbag.")
-            .addField("Premium Lootboxes - 50 coins", "You just don't have a real life and this proves that you are too desperate to buy this so you can pass time gaining repeated role.")
-            .addField("Elite lootboxes - 50 coins", "Same thing like above but even worse.")
-            .addField("Custom Maker Item - 1500 coins", "You can use this to create your own role list when playing.\n\nThis does not give you the full set of role list. You need to buy more roles using coins or roses to access it! To check the list, do `+command that will be announced soon`.")
-            .addField("Private Channel - 2500 Coins", "You can create your own private channel in this server!")
-            .addField("Emoji - 500 Coins", "Grants access to -write")
-            .addField("Immunity - 500 Coins", "When spectating, it will not change your name to lazy spectatorz")
-            .addField("Roses (Single) - 25 coins", "Buy some rose to trade with other people and buy interesting items!")
-            .addField("Roses (Bouquet) - 250 coins", "Buy these and everyone gets a rose! Yes, including yourself!")
+            .setColor("#1FFF43")            
             .setAuthor(message.author.tag, message.author.avatarURL())
-            .setDescription(`Your Balance: ${db.get(`money_${message.author.id}`)}`)
+            .setFooter("Page [1/2]")
+            .setDescription(`Your Balance: **${db.get(`money_${message.author.id}`)}<:coin:606434686931173377>**\n\n === Shop Items ===\n\n**Roses (Single) - 25<:coin:606434686931173377>**\nBuy some rose to trade with other people and buy interesting items!\n\n**Red Colour - 50<:coin:606434686931173377>**\nSassy red makes you look uh... well sassy!\n\n**Blue Colour - 50<:coin:606434686931173377>**\nBlue, as calm as the sky. Get lost, you aren't even near to calm.\n\n**Green Colour - 50<:coin:606434686931173377>**\nBuy this or suffer for eternity.\n\n**Yellow Colour - 50<:coin:606434686931173377>**\nEww, what a dirty colour. Unless you're a f\*\*\*boy or dirty, just do us a favour and don't buy this.\n\n**Black Colour - 50<:coin:606434686931173377>**\nYes, black. Like everyone's heart. In a space vacuum. Where no one can exist.\n\n**Salmon Colour - 50<:coin:606434686931173377>**\nYummy Salmon. Buy this so i can buy a salmon for myself!\n\n**Pink Colour - 50<:coin:606434686931173377>**\nBy buying this colour, you agree that you are too girly and watch "My Little Pony"\n\n**Turquoise Colour - 50<:coin:606434686931173377>**\nSince this is an another version of green, buy or die!\n\n**Crimson Colour - 50<:coin:606434686931173377>**\nWhat the heck is even a crimson...\n\n`)
             )
+        } else if (args[0] == "2") {
+        
+            message.channel.send(
+                new Discord.MessageEmbed()
+                .setTitle("Werewolf Online Simulation Store")
+                .setColor("#1FFF43")
+                .setFooter("Page [2/2]")
+                .setAuthor(message.author.tag, message.author.avatarURL())
+                .setDescription(`Your Balance: **${db.get(`money_${message.author.id}`)}<:coin:606434686931173377>**\n\n === Shop Items ===\n\n**Premium Lootboxes - 100<:coin:606434686931173377>**\nYou just don't have a real life and this proves that you are too desperate to buy this so you can pass time gaining repeated role.\n\n**Elite lootboxes - 150<:coin:606434686931173377>**\nSame thing like above but even worse.\n\n**Roses (Bouquet) - 250<:coin:606434686931173377>**\nBuy these and everyone gets a rose! Yes, including yourself!\n\n**DJ Role - 450<:coin:606434686931173377>**\nYou can use this role to play your own songs in the game server!\n\n**Special Role - 500<:coin:606434686931173377>**\nFinally, have the role name and colour as your type goofbag.\n\n**Emoji - 500<:coin:606434686931173377>**\nGrants access to -write\n\n**Immunity - 500<:coin:606434686931173377>**\nWhen spectating, it will not change your name to lazy spectatorz\n\n**Custom Maker Item - 1500<:coin:606434686931173377>**\nYou can use this to create your own role list when playing.\n\nThis does not give you the full set of role list. You need to buy more roles using coins or roses to access it! To check the list, do \`+command that will be announced soon\`.\n\n**Private Channel - 2500<:coin:606434686931173377>**\nYou can create your own private channel in this server!`)
+            )
+        
         }
     }
 }
