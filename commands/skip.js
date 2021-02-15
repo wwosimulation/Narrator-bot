c8a5e8a7b22a8570d14c8da247520a79c47a466c
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-15 20:59:44 +0800

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -17,7 +17,10 @@ module.exports = {
       } else {
         let isDay = await db.fetch(`isDay_${message.guild.id}`);
         let day = await db.fetch(`dayCount_${message.guild.id}`);
-        if (isDay != "yes") {
+        let vote = db.get(`commandEnabled_${message.guild.id}`)
+        let skipus = db.get(`skipus_${message.author.id}`)
+        if (day < 5) return message.channel.send("You can only skip after Day 5!")
+        if (isDay != "yes" && vote == "yes") {
           return await message.reply(
             "You can only skip the discussion phase during the day!"
           );
@@ -27,10 +30,16 @@ module.exports = {
               "You can only skip the discussion phase if there are 8 or less players alive!"
             );
           } else {
+            if (skipus == true) return message.channel.send("You already voted to skip the discussion phase!")
             let dayChat = message.guild.channels.cache.find(c => c.name === 'day-chat') 
             let commands = message.guild.channels.cache.find(c => c.name === 'commands')  
             dayChat.send(`Someone voted to skip the discussion phase!`)
             commands.send(`${message.member.nickname} decided to skip the discussion phase!`)
+            db.add(`skippedpl`, 1)
+            if (db.get(`skippedpl`) == alive.members.size - 1) {
+                dayChat.send(`Enough players voted to skip the discussion phase!`)
+                client.commands.get("vt").run(message, args, client)
+            }
           }
         }
       }
