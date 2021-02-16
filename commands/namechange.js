bea167b61d41b8223f9be0e5c16ceab3ff8baf3e
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-16 10:12:42 +0800

    Create namechange.js

---
+++ commands/namechange.js
@@ -0,0 +1,30 @@
+const db = require("quick.db")
+
+module.exports = {
+  name: "namechange",
+  run: async (message, args, client) => {
+    let role = db.get(`srole_${message.author.id}`)
+    
+    if (!role) {
+      let specialrolesname = client.guilds.cache.get("465795320526274561").roles.cache.get("606247032553865227")
+      let colorsrolename = client.guilds.cache.get("465795320526274561").roles.cache.get("606247387496972292")
+      let allsprole = client.guilds.cache.get("465795320526274561").roles.cache.filter(r => r.position < specialrolesname.position && r.position > colorsrolename.position)
+      let hassprole = false
+      allsprole.forEach(e => {
+        if (client.guilds.cache.get("465795320526274561").members.cache.get(message.author.id).roles.cache.has(e.id)) {
+          db.set(`srole_${message.author.id}`, e.id)
+          let role = e.id
+        }
+      })
+    }
+    
+    
+    if (!role) return message.channel.send("I cannot find your special role! In case this doesn't make sense, try DMing Ashish#0540 with the proof it's not working.")
+    
+    if (args.length < 1) return message.channel.send("Stop. being. stupid. you. dumb. weirdo.")
+    
+    if (args.join(" ").length > 99) return message.channel.send("Too many characters!")
+    
+    client.guilds.cache.get("465795320526274561").roles.cache.get(role).edit({name: args.join(" ")})
+  }
+}
