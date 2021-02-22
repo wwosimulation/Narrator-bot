fe263d062361cc28f9ca03eee66eeef62c5a8e9a
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-22 10:02:28 +0800

    Update player.js

--- schemas/player.js
+++ schemas/player.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    user: { type: String, unique: true }, //user id
-    role: { type: String, default: ""}, // the role
+    user: { type: String, unique: true }, // user id
+    role: { type: String, default: "" }, // the role
+    channelrole: { type: String, default: "" }, // the channel for the role
     atag: { type: String, default: null}, // avenger tag
-    jwwtag: { type: String, defualt: null}, // jwwtag
-    balance: { type: Number, default: 0 }, //currency balance
+    jwwtag: { type: String, defualt: null}, // junior-werewolf tag
+    balance: { type: Number, default: 0 }, // currency balance
     inventory: {
-        cmi: { type: Boolean, default: false }, //true or false has the cmi
-        profile: { type: Boolean, default: false }, //true or false has the profile cmd
-        rose: { type: Number, default: 0 }, // number of roses
+        cmi: { type: Boolean, default: false }, // true or false has the cmi
+        profile: { type: Boolean, default: false }, // true or false has the profile cmd
+        roseG: { type: Number, default: 0 }, // number of roses bought
+        rose: { type: Number, default: 0 }, // number of roses to be used
         bouquet: { type: Number, default: 0 }, // number of bouquets
-        privatechannel: { type: String, default: "" }, //private channel ID
-        customrole: { type: String, default: "" }, //custom role id
+        privatechannel: { type: String, default: "" }, // private channel ID
+        customrole: { type: String, default: "" }, // custom role id
     }
 });
 
