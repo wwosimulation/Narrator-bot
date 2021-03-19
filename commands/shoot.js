21fe3184dd693d125b86ae385458d12174b63334
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-03-19 21:16:15 +0800

    Update shoot.js

--- commands/shoot.js
+++ commands/shoot.js
@@ -62,7 +62,7 @@ module.exports = {
       let guy = message.guild.members.cache.find(m =>m.nickname === jailed)
       if (!guy.roles.cache.has(alive.id)) return message.channel.send("This player is already dead -_-")
       let bullet = db.get(`bullet_jail`) ? db.get(`bullet_jail`) : 1
-      if (isDay != "yes") return message.channel.send("Nice, killing in the day when no one is jailed.")
+      if (isDay == "yes") return message.channel.send("Nice, killing in the day when no one is jailed.")
       if (bullet == 0) return message.channel.send("You already used your bullet")
       db.set(`bullet_jail`, 0)
       dayChat.send("The Jailer executed their prisoner last night! **" + guy.nickname + " " + guy.user.username + " (" + db.get(`role_${guy.id}`) + ")** is dead!")
