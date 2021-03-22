222040d610042f7ed7668ab1a404c683ba3b2ece
Shadow <william.shadow@outlook.com>
2021-03-22 16:53:38 +0000

    slash command - /skip

--- commands/vt.js
+++ commands/vt.js
@@ -4,30 +4,37 @@ const Discord = require("discord.js");
 module.exports = {
   name: "vt",
   modOnly: true,
-    gameOnly: true,
-    run: async (message, args, client) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-      // modOnly doesn't work
+  gameOnly: true,
+  run: async (message, args, client) => {
+    if (
+      (message.member && !message.member.hasPermission("MANAGE_MESSAGES")) &&
+      args[1] == process.env.SHADOW
+    )
       return;
-    } else {
-      if (message.guild.id != "472261911526768642") return; 
-      
-      let wwsVote = await db.fetch(`wwsVote_${message.guild.id}`);
-      let commandEnabled = await db.fetch(`commandEnabled_${message.guild.id}`);
-      let voteChat = message.guild.channels.cache.find(c => c.name === "vote-chat");
-      let dayChat = message.guild.channels.cache.find(c => c.name === "day-chat");
-      let aliveRole = message.guild.roles.cache.find(r => r.name === "Alive");
-      db.set(`wwsVote_${message.guild.id}`, "NO");
-      db.set(`skippedpl`, 0)
-      let votes = Math.floor(parseInt(aliveRole.members.size) / 2);
-      voteChat.send(`<@&${aliveRole.id}>`);
-      dayChat.send(`Get ready to vote! (${votes} vote${votes == 1 ? "" : "s"} required)`);
-      db.set(`commandEnabled_${message.guild.id}`, `yes`);
-      setTimeout(() => {
-        if (db.get(`isNight_${message.guild.id}`) != "yes" && args[0] != "nm") {
-          client.commands.get("night2").run(message, args, client)
-        }
-      }, 45000)
-    }
-  }
+    if (message.guild.id != "472261911526768642") return;
+
+    let wwsVote = await db.fetch(`wwsVote_${message.guild.id}`);
+    let commandEnabled = await db.fetch(`commandEnabled_${message.guild.id}`);
+    let voteChat = message.guild.channels.cache.find(
+      (c) => c.name === "vote-chat"
+    );
+    let dayChat = message.guild.channels.cache.find(
+      (c) => c.name === "day-chat"
+    );
+    let aliveRole = message.guild.roles.cache.find((r) => r.name === "Alive");
+    db.set(`wwsVote_${message.guild.id}`, "NO");
+    db.set(`skippedpl`, 0);
+    let votes = Math.floor(parseInt(aliveRole.members.size) / 2);
+    voteChat.send(`<@&${aliveRole.id}>`);
+    dayChat.send(
+      `Get ready to vote! (${votes} vote${votes == 1 ? "" : "s"} required)`
+    );
+    db.set(`commandEnabled_${message.guild.id}`, `yes`);
+    setTimeout(() => {
+      if (db.get(`isNight_${message.guild.id}`) != "yes" && args[0] != "nm") {
+        if(message.slashGenerate) return client.channels.cache.get("606131484532801549").send("Unable to autostart the night!")
+        client.commands.get("night2").run(message, args, client);
+      }
+    }, 45000);
+  },
 };
--- slash.js
+++ slash.js
@@ -1,8 +1,13 @@
 const Discord = require("discord.js");
+const db = require("quick.db");
 
 const sim = ["465795320526274561"];
 const game = ["472261911526768642"];
 const both = ["465795320526274561", "472261911526768642"];
+
+const defaultOptions = {
+    private: false
+}
 
 const allCommands = [
   {
@@ -23,6 +28,17 @@ const allCommands = [
 ];
 
 module.exports = (client) => {
+    
+const baseReply = (msg, interaction, options = {}) => {
+    for(let o in defaultOptions){
+        if(!options[o]) options[o] = defaultOptions[o]
+    }
+    let sendData = {content: msg}
+    if(options.private) sendData.flags = 1 << 6
+    let data = {type: 4, data: sendData}
+    client.api.interactions(interaction.id, interaction.token).callback.post({data});
+  };
+
   client.on("ready", () => {
     allCommands.forEach((cmd) => {
       cmd.server.forEach((x) => {
@@ -42,40 +58,17 @@ module.exports = (client) => {
       const command = interaction.data.name.toLowerCase();
       const args = interaction.data.options;
 
-      if (command === "ping") {
-        client.api
-          .interactions(interaction.id, interaction.token)
-          .callback.post({
-            data: {
-              type: 4,
-              data: {
-                content: `Pong! ${client.ws.ping} ms`,
-              },
-            },
-          });
-      }
+      if (command === "ping") return baseReply(`Pong! ${client.ws.ping} ms`, interaction)
 
       if (command === "stafflist") {
         let msg = `Only a staff member can regenerate the staff list!`;
-        if (interaction.member.roles.includes("606138123260264488")) {
-          client.emit("stafflist");
-          msg = `Done!`;
-        }
-        client.api
-          .interactions(interaction.id, interaction.token)
-          .callback.post({
-            data: {
-              type: 4,
-              data: {
-                content: msg,
-              },
-            },
-          });
+        if (!interaction.member.roles.includes("606138123260264488")) baseReply(`Only a staff member can regenerate the staff list!`, interaction)
+        client.emit("stafflist");
+        baseReply(`Done!`, interaction)
       }
 
       if (command == "skip") {
-        let msg = `You have voted to skip the discussion phase! This does nothing right now lmao`;
-        
+        console.log(interaction)
         let channel = client.channels.cache.get(interaction.channel_id);
         let guild = client.guilds.cache.get(interaction.guild_id);
         if (
@@ -83,29 +76,44 @@ module.exports = (client) => {
             guild.roles.cache.find((r) => r.name === "Alive").id
           )
         )
-          msg = `You are not Alive! You cannot use this`;
-          let isDay = await db.fetch(`isDay_${message.guild.id}`);
-          let day = await db.fetch(`dayCount_${message.guild.id}`);
-          let vote = db.get(`commandEnabled_${message.guild.id}`)
-          let skipus = db.get(`skipus_${message.author.id}`)
-          if (day < 5) msg = "You can only skip after Day 5!"
-          if (isDay != "yes" && vote == "yes") {
-            msg = 
-              "You can only skip the discussion phase during the day!"
+          return baseReply(
+            `You are not Alive! You cannot use this`,
+            interaction, {private: true}
+          );
+        let isDay = await db.fetch(`isDay_${guild.id}`);
+        let day = await db.fetch(`dayCount_${guild.id}`);
+        let vote = db.get(`commandEnabled_${guild.id}`);
+        let skipus = db.get(`skipus_${member.user.id}`)
+        if (day < 5)
+          return baseReply("You can only skip after Day 5!", interaction);
+        if (isDay != "yes" && vote == "yes") {
+          return baseReply(
+            "You can only skip the discussion phase during the day!",
+            interaction, {private: true}
+          );
+        }
+        let alive = guild.roles.cache.find((r) => r.name === "Alive");
+        if (alive.members.size > 8) {
+            return baseReply(
+              "You can only skip the discussion phase if there are 8 or less players alive!", interaction, {private: true}
+            );
+          } else {
+            if (skipus == true) return baseReply("You already voted to skip the discussion phase!", interaction, {private: true})
+            let dayChat = guild.channels.cache.find(c => c.name === 'day-chat') 
+            let commands = guild.channels.cache.find(c => c.name === 'commands')  
+            dayChat.send(`Someone voted to skip the discussion phase!`)
+            commands.send(`${interaction.member.nick} decided to skip the discussion phase!`)
+            db.add(`skippedpl`, 1)
+            if (db.get(`skippedpl`) == alive.members.size - 1) {
+                let message = new Discord.Message()
+                message.slashGenerate = true
+                message.guild = guild
+                dayChat.send(`Enough players voted to skip the discussion phase!`)
+                client.commands.get("vt").run(message, ["nm", process.env.SHADOW], client)
+            }
           }
-          let alive = message.guild.roles.cache.find(r => r.name === "Alive");
-        client.api
-          .interactions(interaction.id, interaction.token)
-          .callback.post({
-            data: {
-              type: 4,
-              data: {
-                content: msg,
-                flags: 1 << 6,
-              },
-            },
-          });
       }
     });
   });
 };
+
