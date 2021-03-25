fb3b9f0217ad952f0d1dd30ee3978fc9a8902e80
Shadow <thebotoftheshadow@gmail.com>
2021-03-25 10:10:33 -0500

    jelmer

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "say",
     run: async (message, args, client) => {
-        if (["552814709963751425", "632294140511518751"].includes(message.author.id)) {
+        if (["552814709963751425", "406412325973786624"].includes(message.author.id)) {
             message.delete()
             let content = ""
             let channel = message.mentions.channels.first() || message.channel
