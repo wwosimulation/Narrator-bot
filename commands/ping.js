d691527bdead67fdf3cc1599a2f42e5cd7cbf792
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-03-31 18:07:36 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,6 @@ module.exports = {
   name: "ping",
   description: "Replies with bot ping.",
   aliases: ["heartbeat", "response"],
-  cooldown: 15,
-  show: true,
   run: async (message, args) => {
     message.channel.send(`Pong! ${Math.ceil(message.client.ws.ping)} ms.`);
     message.channel.send("Yeet")
