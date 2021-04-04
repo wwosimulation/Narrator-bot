30c7438ef013aadeb64d073336c9be3c63e37d27
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-04-04 10:45:25 +0800

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -11,6 +11,7 @@ module.exports = {
     let amount = ""
     let cooldown = 86400000
     let smessage = '' 
+    let emote = ""
     let date = db.get(`thedate_${message.author.id}`) || 0
 
     let daily = db.get(`daily_${message.author.id}`) || 0
@@ -29,31 +30,38 @@ module.exports = {
      
       if (date == 0) {
         amount = 10
+        emote = "<:coin:606434686931173377>"
         item = "coins"
         db.add(`money_${message.author.id}`, 10)
       } else if (date == 1) {
         item = "rose"
+        emote = "<:rosesingle:807256844191793158>"
         amount = 1
         db.add(`roseG_${message.author.id}`, 1)
       } else if (date == 2) {
         amount = 1
         item = "rose bouquet"
+        emote = "<:rosebouquet:808545517209387008>"
         db.add(`roseBouquet_${message.author.id}`, 1)
       } else if (date == 3) {
         amount = 1
+        emote = "<:lootbox:808548473548963861>"
         item = "lootbox!\n\nTo use it, do `+use lootbox`"
         db.add(`lootbox_${message.author.id}`, 1)
       } else if (date == 4) {
         item = "coins"
+        emote = "<:coin:606434686931173377>"
         amount = 20
         db.add(`money_${message.author.id}`, 20)
       } else if (date == 5) {
         item = "roses"
+        emote = "<:rosesingle:807256844191793158>"
         amount = 5
         db.add(`rosesG_${message.author.id}`, 5)
       } else if (date == 6) {
         amount = 30
         item = "coins"
+        emote = "<:coin:606434686931173377>"
         db.set(`thedate_${message.author.id}`, -1)
         db.add(`money_${message.author.id}`, 30)
       } 
@@ -61,7 +69,7 @@ module.exports = {
       message.channel.send(
       	new Discord.MessageEmbed()
         .setTitle("Daily Rewards! Woohooo!") 
-        .setDescription(`Nice! You have recieved ${amount} ${item}!`) 
+        .setDescription(`${emote} Nice! You have recieved ${amount} ${item}!`) 
       
       ) 
         db.add(`thedate_${message.author.id}`, 1)
