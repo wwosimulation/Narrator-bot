f4bea8739d77947608ef80b1e90c7c63b468f264
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-04-11 17:43:33 +0800

    Update visit.js

--- commands/visit.js
+++ commands/visit.js
@@ -7,6 +7,7 @@ module.exports = {
     run: async (message, args, client) => {
         if (message.channel.name === "priv-red-lady") {
             let alive = message.guild.roles.cache.find(r => r.name === "Alive")
+            let isNight = db.get(`isNight_${message.guild.id}`)
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send("Umm no. Listen to yourself stupid..")
             if (!args[0]) return message.channel.send("I am not even gonna tell what's going on")
 
@@ -15,6 +16,8 @@ module.exports = {
             message.guild.members.cache.find(m => m.user.username === args.join(" ")) ||
             message.guild.members.cache.find(m => m.user.tag === args.join(" "))
 
+            if (isNight != "yes") return message.channel.send("Yo prostitute, pls do this in the night.")
+            
             if (!guy || guy.id == message.author.id) return message.reply("Invalid Target!")
 
             if (!guy.roles.cache.has(alive.id)) return message.channel.send("Pls commit suicide... ")
