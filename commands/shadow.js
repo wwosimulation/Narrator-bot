0f05b89be96288d4b634df85000403889872b704
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-04-11 17:10:12 +0800

    Update shadow.js

--- commands/shadow.js
+++ commands/shadow.js
@@ -7,8 +7,10 @@ module.exports = {
         if (message.channel.name == "priv-shadow-wolf") {
             let alive = message.guild.roles.cache.find(r => r.name === "Alive")
             let abil = db.get(`shadow_${message.channel.id}`) 
+            let isDay = db.get(`isDay_${message.guild.id}`)
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send("You can't manipulate voting when dead, dumb.")
             if (abil == "yes") return message.channel.send("YOU ALREADY ENABLED MANIPULATING STUPID.")
+            if (isDay != "yes") return message.channel.send("Yup, trying to manipulate during the night.")
             message.guild.channels.cache.find(c => c.name === "day-chat").send("<:shadow:744573018147127394> The Shadow Wolf has manipulated voting today!")
             db.set(`vtshadow`, true)
             db.set(`shadow_${message.channel.id}`, "yes")
