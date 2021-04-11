59fbc1c70b5b42bb45dfa814e0db8e4f0fd4ce17
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-04-11 08:52:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ class Message extends Structures.get("Message") {
 
 Structures.extend("Message", () => Message);
 
-const client = new Discord.Client({ws: {intents: ["GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES"]}})
+const client = new Discord.Client({ws: {intents: ["GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS", "GUILD_PRESENCES"]}})
 const bot = new Discord.Client()
 
 //Prefix and token from config file
