e5b7716edc2beb3336a7fa75413dda185aecbacb
Shadow <william.shadow@outlook.com>
2021-04-27 22:16:13 +0000

    export XP values

--- commands/narrator/win.js
+++ commands/narrator/win.js
@@ -1,16 +1,18 @@
 const Discord = require("discord.js")
 const db = require("quick.db")
-const {fn, xp} = require("../../config.js")
+const {fn, xp, roles} = require("../../config.js")
 
 module.exports = {
   name: "win",
   gameOnly: true,
   narratorOnly: true,
   run: async (message, args, client) => {
+    if(args.length < 2) message.channel.send(`Please specify the winning team and its players!\n\`\`\`${process.env.PREFIX}win <team> <players>\`\`\``)
     let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
-    let dead = message.guild.roles.cache.find((r) => r.name === "Dead")
-    let solokillers = ["Serial Killer", "Arsonist", "Bomber", "Cannibal", "Corruptor", "Illusionist", "Bandit", "Accomplice", "Sect Leader", "Zombie"]
     if (alive.members.size != "0") return message.channel.send('To use this command, everyone must have the "Dead" role! Use `+suicideall` if you need to kill everyone at once.')
+    let dead = message.guild.roles.cache.find((r) => r.name === "Dead")
+    let soloKillers = roles.filter(x => x.soloKiller)
+    
 
     let allPlayers = []
     for (let i = 1; i <= dead.members.size; i++) {
@@ -26,7 +28,9 @@ module.exports = {
     let won = ""
     let winTeam = args[0].toLowerCase()
     giveXP = xp.team[winTeam]
-    if(!giveXP) return message.channel.send("Error! XP not found for that team")
+    if(!giveXP) return message.channel.send("XP data not found for that team")
+
+    let embed = new Discord.MessageEmbed().setColor("#008800")
     
 
     if (args[0].toLowerCase() == "couple") {
@@ -58,13 +62,11 @@ module.exports = {
           db.set(`winstreak_${guy.id}`, 0)
           if (guy.presence.status != "offline") {
             db.add(`xp_${guy.id}`, xp.team.tie)
-            guy.send(new Discord.MessageEmbed().setTitle("Game Over - Tie").setDescription("Finished Game:\t\t15xp").setColor("#FF0000").setFooter("You lost!"))
+            guy.send(embed.setTitle("Game Over - Tie").setDescription("Finished Game: 15xp").setFooter("You lost!").setColor(0xff0000))
           }
         }
       }
       return
-    } else {
-      return message.channel.send("Team not found! Available teams: `Couple` `Fool` `Headhunter` `Solo` `Village` `Werewolf`")
     }
 
     for (let i = 1; i < args.length; i++) {
@@ -81,29 +83,29 @@ module.exports = {
         }
         let fwotd = db.get(`firstwinoftheday_${guy.id}`) || -1
         let today = new Date().getDate()
-        let themsg = `Win as ${args[0]}\t\t${xp}xp`
+        let themsg = `Win as ${args[0]} ${xp}xp`
         db.add(`${won}_${guy.id}`, 1)
         db.add(`xp_${guy.id}`, xp)
         db.add(`winstreak_${guy.id}`, 1)
-        let t = await guy.send(new Discord.MessageEmbed().setTitle("Game ended").setColor("#008800").setDescription(`Win as ${args[0]}:\t\t${xp}xp`)).catch((e) => message.channel.send("I could not send the details to " + guy.user.tag + "!"))
+        let t = await guy.send(embed.setTitle("Game Over").setColor("#008800").setDescription(`Win as ${args[0]}: ${xp}xp`)).catch((e) => message.channel.send("I could not send the details to " + guy.user.tag + "!"))
         setTimeout(async () => {
-          if (guy.presence.status !== "offline") await t.edit(new Discord.MessageEmbed().setTitle("Game ended").setColor("#008800").setDescription(`${themsg}\nFinished Game:\t\t15xp`))
-          themsg += `\nFinished Game:\t\t15xp`
+          if (guy.presence.status !== "offline") await t.edit(embed.setTitle("Game Ended").setDescription(`${themsg}\nFinished Game: 15xp`))
+          themsg += `\nFinished Game: 15xp`
           db.add(`xp_${guy.id}`, xp[finishGame])
         }, 1000)
         setTimeout(async () => {
           if (db.get(`winstreak_${guy.id}`) > 1) {
-            await t.edit(new Discord.MessageEmbed().setTitle("Game ended").setColor("#008800").setDescription(`${themsg}\nWin Streak:\t\t25xp`))
+            await t.edit(embed.setTitle("Game Ended").setDescription(`${themsg}\nWin Streak: 25xp`))
             db.add(`xp_${guy.id}`, xp[winStreak])
-            themsg += `\nWin Streak:\t\t25xp`
+            themsg += `\nWin Streak: ${xp[winStreak]}xp`
           }
         }, 2000)
         setTimeout(async () => {
           if (fwotd < today) {
-            await t.edit(new Discord.MessageEmbed().setTitle("Game Ended").setColor("#008800").setDescription(`${themsg}\nFirst win of the day:\t100xp`))
+            await t.edit(embed.setTitle("Game Ended").setDescription(`${themsg}\nFirst win of the day:\t${xp[firstWinOfTheDay]}xp`))
             db.add(`xp_${guy.id}`, xp[firstWinOfTheDay])
             db.set(`firstwinoftheday_${guy.id}`, today)
-            themsg += `\nFirst win of the day:\t100xp`
+            themsg += `\nFirst win of the day:\t${xp[firstWinOfTheDay]}xp`
           }
         }, 3000)
       }
@@ -119,7 +121,7 @@ module.exports = {
           if (!db.get(`xpreq_${guy.id}`)) {
             db.set(`xpreq_${guy.id}`, 1000)
           }
-          guy.send(new Discord.MessageEmbed().setTitle("Game Ended").setColor("#880000").setDescription("Finished Game:\t\t15xp")).catch((e) => message.channel.send(`I could not send the details to ${guy.user.tag}`))
+          guy.send(embed.setTitle("Game Ended").setColor("#880000").setDescription("Finished Game: 15xp")).catch((e) => message.channel.send(`I could not send the details to ${guy.user.tag}`))
           db.add(`xp_${guy.id}`, 15)
         }
         let role = db.get(`role_${guy.id}`)
