960925e00a88c649807c947bd0094a56f08c555e
Shadow <william.shadow@outlook.com>
2021-04-29 08:40:21 -0500

    add +emoji command (#27)

---
+++ commands/bot/emoji.js
@@ -0,0 +1,24 @@
+const config = require("../../config.js")
+const Discord = require("discord.js")
+module.exports = {
+    name: "emoji",
+    aliases: ["e"],
+    run: async (message, args, client) => {
+        if (!client.guilds.cache.get(config.ids.server.sim).members.cache.get(message.author.id).roles.cache.has("663389088354664477")) return
+        message.delete()
+        let emoji = client.userEmojis.find((x) => x.name.toLowerCase() == args.join(" ").toLowerCase())
+        if (!emoji) return message.author.send("Unable to find that emoji!")
+        if (message.channel.permissionsFor(message.guild.me).has("MANAGE_WEBHOOKS")) {
+            let allHooks = await message.channel.fetchWebhooks()
+            let hook = allHooks.find(x => x.owner.id == client.user.id)
+            if(!hook) hook = await message.channel.createWebhook(client.user.username, {
+                avatar: client.user.avatarURL(),
+                reason: `${process.env.PREFIX}emoji command`
+              })
+            hook.send(`${emoji}`, {username: message.member.nickname ? message.member.nickname : message.author.username, avatarURL: message.author.avatarURL()})
+        } else {
+            let userEmbed = new Discord.MessageEmbed().setDescription(`<@${message.author.id}>`).setColor("#1FFF43")
+            message.channel.send(`${emoji}`, userEmbed)
+        }
+    },
+}
