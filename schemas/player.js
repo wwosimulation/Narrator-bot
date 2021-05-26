ddd0302c8018836018a39bd608787ecc22f0dad8
Shadow <william.shadow@outlook.com>
2021-05-26 00:31:38 -0500

    new stats

--- schemas/player.js
+++ schemas/player.js
@@ -4,7 +4,7 @@ const schema = new mongoose.Schema({
     user: { type: String, unique: true }, // user id
     coins: {type: Number, default: 0}, // coins aka balance
     roses: {type: Number, default: 0}, // roses CURRENCY
-    roses: {type: Number, default: 0}, // gems
+    gems: {type: Number, default: 0}, // gems
     inventory: {
         cmi: { type: Boolean, default: false }, // true or false has the cmi
         profile: { type: Boolean, default: false }, // true or false has the profile cmd
@@ -14,7 +14,10 @@ const schema = new mongoose.Schema({
         bouquet: { type: Number, default: 0 }, // number of bouquets
         privateChannel: { type: String, default: "" }, // private channel ID
         customRole: { type: String, default: "" }, // custom role id
-    }
+    },
+    stats: {
+        
+    },
 });
 
 module.exports = mongoose.model(`${__filename.split(`${__dirname}/`).pop().split(`.`).shift()}`, schema);
