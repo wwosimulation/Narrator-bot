9ff5fb83f91915894d5f827c12acc52b67f959f5
Shadow <william.shadow@outlook.com>
2021-06-03 17:12:02 +0000

    write command and migrate to fun category

---
+++ commands/fun/write.js
@@ -0,0 +1,32 @@
+const config = require("../../config.js")
+const Discord = require("discord.js")
+module.exports = {
+    name: "write",
+    aliases: ["w"],
+    run: async (message, args, client) => {
+        if (!client.guilds.cache.get(config.ids.server.sim).members.cache.get(message.author.id).roles.cache.has("663389088354664477")) return console.log("No perms for emojis")
+        message.delete()
+        let newz = ""
+        args.join(" ")
+            .split("")
+            .forEach((letter) => {
+                let e = client.guilds.cache.get("850035156445298768").emojis.cache.find((x) => x.name == letter + "_")
+                if (e) {
+                  newz += `${e}`
+                } else newz += `${letter}`
+            })
+        if (message.channel.permissionsFor(message.guild.me).has("MANAGE_WEBHOOKS")) {
+            let allHooks = await message.channel.fetchWebhooks()
+            let hook = allHooks.find((x) => x.owner.id == client.user.id)
+            if (!hook)
+                hook = await message.channel.createWebhook(client.user.username, {
+                    avatar: client.user.avatarURL(),
+                    reason: `${process.env.PREFIX}emoji command`,
+                })
+            hook.send(`${newz}`, { username: message.member.nickname ? message.member.nickname : message.author.username, avatarURL: message.author.avatarURL() })
+        } else {
+            let userEmbed = new Discord.MessageEmbed().setDescription(`<@${message.author.id}>`).setColor("#1FFF43")
+            message.channel.send(`${newz}`, userEmbed)
+        }
+    },
+}
