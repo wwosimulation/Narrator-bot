724cb998d2e2df24e9ca6e54a296c446407c0191
Shadow <william.shadow@outlook.com>
2021-06-11 05:51:39 +0000

    new github tracking

--- commands/bot/suggest.js
+++ commands/bot/suggest.js
@@ -1,14 +1,24 @@
+const { ids } = require("../../config.js")
+
 module.exports = {
-  name: "suggest",
-  run: async (message, args, client) => {
-    if (!args[0]) return message.channel.send("Stop. You're making your life worse")
-    let t = ""
-    if (message.attachments.size > 0) {
-      message.attachments.forEach((a) => (t += a.url + "\n"))
-    }
-    client.channels.cache
-      .get("608673032361345034")
-      .send(`Suggestion by ${message.author.tag}:\n${args.join(" ")}\n${t}`)
-    message.channel.send("Your suggestion has been sent")
-  },
+    name: "suggest",
+    run: async (message, args, client) => {
+        if (!args[0]) return message.channel.send("Invalid suggestion")
+        let t = ""
+        if (message.attachments.size > 0) {
+            if (message.guild.id == ids.server.game) {
+                message.channel.send("Images attached to a suggestion from the game server cannot be submitted! Please use the link below to add your attachments.")
+            } else {
+                message.attachments.forEach((a) => (t += a.url + "\n"))
+            }
+        }
+        let issue = {
+            title: `Bug report ${message.id}`,
+            body: `${args.join(" ")}\n\n** **\nThe above suggestion was sent by ${message.author.tag}\nUser ID: ${message.author.id}\nLocation: #${message.channel.name} (${message.channel.id}) in ${message.guild.name}`,
+            labels: ["Bug", "Unverified"],
+        }
+        let done = await client.github.post("/issues", issue)
+        message.channel.send(`Your suggestion has been sent! Please provide any screenshots or more information here:\n<${done.data.html_url}>`)
+        console.log(done)
+    },
 }
