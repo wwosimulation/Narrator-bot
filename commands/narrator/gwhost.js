fea44f4fc64ff6553407095002b4f7ecd566b018
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-06-24 22:29:27 +0800

    Added host
    - Added host in the database for buttons

--- commands/narrator/gwhost.js
+++ commands/narrator/gwhost.js
@@ -9,7 +9,7 @@ module.exports = {
         if (db.get(`game`) != null) return message.channel.send("Another game is being hosted!")
         let sup = ""
         if (message.member.roles.cache.has(mininarr.id)) {
-            let guy = message.guild.members.cache.get(a + rgs[0]) || message.guild.members.cache.find((m) => m.nickname === args[0]) || message.guild.members.cache.find((m) => m.user.username === args[0]) || message.guild.members.cache.find((m) => m.user.tag === args[0])
+            let guy = message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((m) => m.nickname === args[0]) || message.guild.members.cache.find((m) => m.user.username === args[0]) || message.guild.members.cache.find((m) => m.user.tag === args[0])
             if (!guy) return message.channel.send(`Supervisor \`${args[0]}\` was not found!`)
             let rol = message.guild.roles.cache.find((r) => r.name === "Supervisor")
             if (!guy.roles.cache.has(rol.id)) return message.channel.send(`${guy.user.tag} is not a supervisor!`)
@@ -21,5 +21,6 @@ module.exports = {
         const embed = new MessageEmbed().setTitle("Player and Spectator List:").setDescription("** **").setColor(0x327210)
         let m = await message.guild.channels.cache.get("606123818305585167").send(`<@&606123686633799680>, we are now starting game ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about the game, go to <#606123783605977108> and react with 🎮${sup ? `\n\n${sup}` : ""}`, { embed, components: [row] })
         db.set(`game`, m.id)
+        db.set(`hoster`, message.author.id)
     },
 }
