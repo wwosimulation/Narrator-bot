cdceafdabeb7aa8da398e5471d84937eab3cc1bd
Shadow <william.shadow@outlook.com>
2021-06-28 05:26:26 -0500

    Economy bugs
    Fixes https://github.com/wwosimulation/tracker/issues/103
    Fixes https://github.com/wwosimulation/tracker/issues/104

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -15,6 +15,11 @@ module.exports = {
 
         let lastDaily = data.daily.last
         let extra = ""
+        let bonus = 1
+        if(client.guilds.cache.get(config.ids.server.sim).members.cache.get(message.author.id).premiumSince) {
+                bonus = 2
+                extra = "\nBecause you are a booster, you received double the normal rewards amount!"
+            }
 
         if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
             let time = ms(cooldown - (Date.now() - lastDaily))
@@ -28,45 +33,41 @@ module.exports = {
             message.reply(`No. Come back in ${hrs} ${min} ${sec}`)
         } else {
             if (date == 0) {
-                amount = 10
+                amount = 10 * bonus
                 emote = "<:coin:606434686931173377>"
                 item = "coins"
-                data.coins += 10
+                data.coins += 10 * bonus
             } else if (date == 1) {
                 item = "rose"
                 emote = "<:rosesingle:807256844191793158>"
-                amount = 1
-                data.inventory.rose += 1
+                amount = 1 * bonus
+                data.inventory.rose += 1 * bonus
             } else if (date == 2) {
-                amount = 1
+                amount = 1 * bonus
                 item = "rose bouquet"
                 emote = "<:rosebouquet:808545517209387008>"
-                data.inventory.bouquet += 1
+                data.inventory.bouquet += 1 * bonus
             } else if (date == 3) {
-                amount = 1
+                amount = 1 * bonus
                 emote = "<:lootbox:808548473548963861>"
                 item = "lootbox!\n\nTo use it, do `+use lootbox`"
-                data.inventory.lootbox += 1
+                data.inventory.lootbox += 1 * bonus
             } else if (date == 4) {
                 item = "coins"
                 emote = "<:coin:606434686931173377>"
-                amount = 20
-                data.coins += 20
+                amount = 20 * bonus
+                data.coins += 20 * bonus
             } else if (date == 5) {
                 item = "roses"
                 emote = "<:rosesingle:807256844191793158>"
-                amount = 5
-                data.inventory.roses += 5
+                amount = 5 * bonus
+                data.inventory.roses += 5 * bonus
             } else if (date == 6) {
-                amount = 30
+                amount = 30 * bonus
                 item = "coins"
                 emote = "<:coin:606434686931173377>"
                 data.daily.day = -1
-                data.coins += 30
-            }
-            if(client.guilds.cache.get(config.ids.server.sim).members.cache.get(message.author.id).premiumSinceTimestamp > 0) {
-                amount = amount * 2
-                extra = "\nBecause you are a booster, you received double the normal rewards amount!"
+                data.coins += 30 * bonus
             }
 
             message.channel.send(new Discord.MessageEmbed().setTitle("Daily Rewards! Woohooo!").setDescription(`${emote} Nice! You have recieved ${amount} ${item}!${extra}`))
--- commands/staff/rm.js
+++ commands/staff/rm.js
@@ -1,20 +1,21 @@
 const { fn, emojis } = require("../../config.js")
 const db = require("quick.db")
+const {players} = require("../../db.js")
 
 module.exports = {
-  name: "rm",
-  aliases: ["removemoney"],
+  name: "am",
+  aliases: ["addmoney"],
   staffOnly: true,
   run: async (message, args, client) => {
     let amount = parseInt(args[0])
     if (!amount) return message.channel.send(`${args[0]} is not a valid amount`)
     args.shift()
     msg = ``
-    args.forEach((x) => {
+    amount = 0 - amount
+    args.forEach(async (x) => {
       let user = fn.getUser(x, message)
       if (user) {
-        db.subtract(`money_${user.id}`, amount)
-        console.log(user)
+        let data = players.findOneAndUpdate({user: user.id}, {$inc : {coins: amount}}).exec()
         msg += `Removed ${amount} ${emojis.coin} from ${user.user.tag}\n`
       } else {
         msg += `Unable to find the user ${x}\n`
