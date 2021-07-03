b7f378d95a952f41d7f4e6b0867d974c704d48db
Groot <67421407+Gorott@users.noreply.github.com>
2021-07-03 19:53:02 +0200

    Part 1 suggestion #118

--- commands/narrator/day.js
+++ commands/narrator/day.js
@@ -1480,6 +1480,7 @@ module.exports = {
                           VIEW_CHANNEL: true,
                           READ_MESSAGE_HISTORY: true,
                         })
+                        sected.send(`${alive}\n${guy.user.username} is now a sect member`)
                         let thechan = message.guild.channels.cache.filter((c) => c.name === `priv-${db.get(`role_${guy.id}`).toLowerCase().replace(" ", "-")}`).keyArray("id")
                         for (let k = 0; k < thechan.length; k++) {
                           let chan = message.guild.channels.cache.get(thechan[k])
