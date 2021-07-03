c5f93281e7d60988f164a464f8db2109c2d9b5af
Groot <67421407+Gorott@users.noreply.github.com>
2021-07-03 18:24:56 +0200

    Seems like it could work but probably won't lmao

--- commands/narrator/srole.js
+++ commands/narrator/srole.js
@@ -207,11 +207,12 @@ module.exports = {
         shuffle(finalRoleList)
         let sorcChats = []
         for (let k = 0; k < alive.members.size; k++) {
-            let theirRole = finalRoleList[k].name
+            let theirRole = finalRoleList[k]
             let role = getRole(theirRole)
             rolelist.push(theirRole)
             let guy = message.guild.members.cache.find((x) => x.nickname == `${k + 1}`)
-            let lol = await message.guild.channels.create(`priv-${theirRole.replace(" ", "-")}`, {
+            db.delete(`suicided_${guy.id}`)
+            let lol = await message.guild.channels.create(`priv-${role.name.replace(" ", "-")}`, {
                 parent: "748959630520090626",
                 permissionOverwrites: [
                     {
