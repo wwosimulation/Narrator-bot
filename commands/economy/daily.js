5605c13a452dd22dc78456dff72d10471a32e64c
Groot <67421407+Gorott@users.noreply.github.com>
2021-07-04 12:32:55 +0200

    Fixes #131

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -70,7 +70,11 @@ module.exports = {
                 data.coins += 30 * bonus
             }
 
+            let dailymsg = new Discord.MessageEmbed().setTitle("Daily Rewards! Woohooo!").setDescription(`${emote} Nice! You have recieved ${amount} ${item}!${extra}`)
+
             message.channel.send(new Discord.MessageEmbed().setTitle("Daily Rewards! Woohooo!").setDescription(`${emote} Nice! You have recieved ${amount} ${item}!${extra}`))
+            message.channel.send({embeds: [dailymsg]})
+
             data.daily.dau += 1
             data.daily.last = Date.now()
         }
