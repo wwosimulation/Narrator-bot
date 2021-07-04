ddc70b0b8ef91c55879bcb993add3c598cbcd1f5
Groot <67421407+Gorott@users.noreply.github.com>
2021-07-04 09:31:03 +0200

    Fixes #131

--- commands/bot/roleinfo.js
+++ commands/bot/roleinfo.js
@@ -6,6 +6,6 @@ module.exports = {
     name: "roleinfo",
     run: async (message, args, client) => {
         let role = getRole(args.join(" "))
-        message.channel.send(new Discord.MessageEmbed().setTitle(role.name).setDescription(role.description).setThumbnail(role.icon))
+        message.channel.send({ embeds: [new Discord.MessageEmbed().setTitle(role.name).setDescription(role.description).setThumbnail(role.icon)])
     },
 }
