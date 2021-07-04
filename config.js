55402638f4c5cb8f624b1efdbcc461540adb139f
thewilloftheshadow <thewilloftheshadow@users.noreply.github.com>
2021-07-04 15:34:01 +0000

    Prettified Code!

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-const repoConfig = require("../config/index.js");
+const repoConfig = require("../config/index.js")
 
 if (!repoConfig) throw new Error("Cannot find the config module from GitHub!")
 
 module.exports = repoConfig
-
