65b9c661b709899f14a7762e466390872532b59b
Shadow <william.shadow@outlook.com>
2021-07-08 22:54:48 -0500

    Fix broken color shop
    Fixes https://github.com/wwosimulation/tracker/issues/138

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -5,15 +5,14 @@ const { shop, emojis, fn } = require("../../config")
 module.exports = {
     name: "shop",
     run: async (message, args, client) => {
-        let embeds = [new MessageEmbed()]
 
         if (["color", "colors", "colour", "colours"].includes(args[0])) {
-            embeds[0].setDescription("Available colors:\n\nUse `+buy <color> role` to purchase a color")
+            let embed = new MessageEmbed().setDescription("Available colors:\n\nUse `+buy <color> role` to purchase a color")
             shop.colors.forEach((x) => {
-                embeds[0].description += `${x.name} Color\n`
+                embed.description += `${x.name} Color\n`
             })
-            embeds[0].setTitle("Wolvesville Simulation Store").setColor("#1FFF43")
-            return message.channel.send(embeds[0])
+            embed.setTitle("Wolvesville Simulation Store").setColor("#1FFF43")
+            return message.channel.send({embeds: [embed]})
         } else {
             let row = new MessageActionRow()
             for (let i = 0; i < shop.embeds.length; i++) {
