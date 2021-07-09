62ff5de4103029765dc85d192ddb56ed7d99f9fa
thewilloftheshadow <william.shadow@outlook.com>
2021-07-09 16:39:47 +0000

    vote fix again

--- events/dropdown.js
+++ events/dropdown.js
@@ -4,7 +4,6 @@ module.exports = (client) => {
     client.on("interactionCreate", async (interaction) => {
         if (!interaction.isSelectMenu()) return
         if (interaction.customID.startsWith("votephase")) {
-
             if (interaction.values[0].split("-")[1] == interaction.member.nickname) return interaction.reply({ content: `Trying to win as fool by voting yourself won't get you anywhere. Get a life dude.`, ephemeral: true })
             if (interaction.values[0].split("-")[1] == "cancel") {
                 await interaction.deferUpdate()
@@ -20,17 +19,16 @@ module.exports = (client) => {
             } else {
                 await interaction.deferUpdate()
                 let voted = db.get(`votemsgid_${interaction.member.id}`)
-                if (voted)
+                if (voted) {
                     let tmestodel = await interaction.message.channel.messages.fetch(voted).catch((e) => console.log(e.message))
-                if (tmestodel) {
-                    await tmestodel.delete()
+                    if (tmestodel) {
+                        await tmestodel.delete()
+                    }
                 }
             }
             let omg = await interaction.message.channel.send(`${interaction.member.nickname} voted ${interaction.values[0].split("-")[1]}`)
             db.set(`vote_${interaction.member.id}`, interaction.values[0].split("-")[1])
             db.set(`votemsgid_${interaction.member.id}`, omg.id)
-
         }
-
     })
 }
