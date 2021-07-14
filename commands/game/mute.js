ee2a936160be6c9cd2437266231f8362fcce4482
FinnyMarigold58 <59434139+FinnyMarigold58@users.noreply.github.com>
2021-07-14 17:04:42 -0500

    Added hacker

--- commands/game/mute.js
+++ commands/game/mute.js
@@ -28,7 +28,32 @@ module.exports = {
                     }
                 }
             }
-        } else {
+        } else if (message.channel.name == "priv-hacker") {
+            let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
+            let dead = message.guild.roles.cache.find((r) => r.name === "Dead")
+            let night = await db.fetch(`nightCount`)
+            let isNight = await db.fetch(`isNight`)
+            let guy = message.guild.members.cache.find((m) => m.nickname === args[0])
+            let ownself = message.guild.members.cache.find((m) => m.nickname === message.member.nickname)
+            let hacked = await db.fetch(`firsthacked_${message.channel.id}`)
+            if (parseInt(args[0]) > parseInt(alive.members.size) + parseInt(dead.members.size) || parseInt(args[0]) < 1) {
+                return await message.reply("Invalid target!")
+            } else if (args[0] === message.member.nickname) {
+                return await message.reply("You can't mute yourself!")
+            } else {
+                if (isNight != "yes") {
+                    return await message.reply("You can only do this during the night!")
+                } else {
+                    if (hacked == null) {
+                        return await message.reply("You haven't hacked anyone!")
+                } else {
+                if (hacked.includes(guy.nickname)) {
+                    return await message.reply("You can only mute people hacked!")
+                }
+                    } else {
+                        db.set(`mute_${message.channel.id}`, args[0])
+                        message.react("475775342007549962")
+        }else {
             return
         }
     },
