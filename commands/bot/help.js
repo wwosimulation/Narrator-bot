7c282af709b9eae3b81069b95769b5d6fdf44b39
Sampada Petkar <sampada.petkar@seagate.com>
2021-07-17 23:48:37 +0530

    add variable names

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js")
 
 module.exports = {
     name: "help",
-    run: async (message, _, _) => {
+    run: async (message, args, client) => {
         let embed = new MessageEmbed()
             .setTitle("What do I got?")
             .setColor(0x7419b4)
@@ -17,26 +17,26 @@ For any questions head over to the <#606123788257591297>.
 `
             )
             .addField(
-              "Economy",
-              "`shop` Buy different items in the shop.\n" +
-              "`daily` Get coins, roses, items as a daily reward.\n" +
-              "`inventory` Find the roses, coins, items in your inventory.\n" +
-              "`balance` Check how many narrator bot coins you got.\n" +
-              "`namechange` Give your custom role a new name.\n" +
-              "`use` Use this command with the item name such as lootbox, icon to use them."
+                "Economy",
+                "`shop` Buy different items in the shop.\n" +
+                "`daily` Get coins, roses, items as a daily reward.\n" +
+                "`inventory` Find the roses, coins, items in your inventory.\n" +
+                "`balance` Check how many narrator bot coins you got.\n" +
+                "`namechange` Give your custom role a new name.\n" +
+                "`use` Use this command with the item name such as lootbox, icon to use them."
             )
             .addField(
-              "Fun",
-              "`emoji` use emojis from different servers available from `emojilist`.\n" +
-              "`write` Write something with fancy letters.\n"
+                "Fun",
+                "`emoji` use emojis from different servers available from `emojilist`.\n" +
+                "`write` Write something with fancy letters.\n"
             )
             .addField(
-              "Bot",
-              "`botinfo` Get to know more about the bot.\n" +
-              "`bug` Report a bug to the development team.\n" +
-              "`suggest` Suggest a improvement, enhancement for the simulation.\n" +
-              "`roleinfo` Know more about the in game roles and interactions.\n"
+                "Bot",
+                "`botinfo` Get to know more about the bot.\n" +
+                "`bug` Report a bug to the development team.\n" +
+                "`suggest` Suggest a improvement, enhancement for the simulation.\n" +
+                "`roleinfo` Know more about the in game roles and interactions.\n"
             )
-            message.channel.send({ embeds: [embed] })
-      },
+        message.channel.send({ embeds: [embed] })
+    },
 }
