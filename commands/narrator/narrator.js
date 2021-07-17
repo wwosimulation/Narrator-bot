5f67377ca3428356e3d750468c01e9d9ffae82a9
Shadow <william.shadow@outlook.com>
2021-07-17 09:52:49 -0500

    Delete narrator.js
    Fixes https://github.com/wwosimulation/tracker/issues/163

--- commands/narrator/narrator.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "narrator",
-    gameOnly: true,
-    run: async (message, args, client) => {
-        let narrator = message.guild.roles.cache.find((r) => r.name === "Narrator")
-        message.member.removeRole(narrator.id)
-    },
-}
