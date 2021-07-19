09727b35412896625b49620763cb7926e14d8a24
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-07-19 00:20:38 -0500

    Update night.js

--- commands/narrator/night.js
+++ commands/narrator/night.js
@@ -194,6 +194,7 @@ module.exports = {
             }
             for (let i = 0; i < hacker.length; i++) {
                 db.set(`hashacked_${hacker[i]}`, false)
+                db.set(`mute_${hacker[i]}`, false)
             }
             for (let i = 0; i < det.length; i++) {
                 db.set(`detCheck_${det[i]}`, "no")
