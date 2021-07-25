104df783066d9a713d5d19c8cdda8b26cfcf238d
Hackante <kante.hagen@gmail.com>
2021-07-25 21:43:38 +0200

    aliases in exclude

--- commands/narrator/exclude.js
+++ commands/narrator/exclude.js
@@ -11,8 +11,20 @@ module.exports = {
         })
 
         let roles = config.roles.map((x) => x.name.toLowerCase())
+        let aliases = require('../../config/src/aliases')
 
         let rolestoexclude = []
+
+        for (const key in aliases) {
+            if(args.includes(key)) {
+                if (Object.hasOwnProperty.call(aliases, key)) {
+                    const element = aliases[key];
+                    rolestoexclude.push(element)
+                    let index = args.indexOf(key)
+                    args.splice(index, 1)
+                }
+            }
+        }
 
         for (let arg of args) {
             arg = arg.toLowerCase().replace("-", " ")
