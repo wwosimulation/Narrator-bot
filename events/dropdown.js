f6ea86bcfc0dea32ea9d653a6edd021c89ca1819
thewilloftheshadow <thewilloftheshadow@users.noreply.github.com>
2021-07-28 18:20:29 +0000

    Prettified Code!

--- events/dropdown.js
+++ events/dropdown.js
@@ -4,13 +4,12 @@ const { players } = require("../db")
 module.exports = (client) => {
     client.on("interactionCreate", async (interaction) => {
         if (!interaction.isSelectMenu()) return
-        if(interaction.customId.startsWith("configLanguage")) {
+        if (interaction.customId.startsWith("configLanguage")) {
             let user = interaction.member.id
-            await players.findOneAndUpdate({user}, {language: interaction.values[0]}).exec()
-            interaction.reply({content: `Your language has been set to ${interaction.values[0]}!`, ephemeral: true})
-            
+            await players.findOneAndUpdate({ user }, { language: interaction.values[0] }).exec()
+            interaction.reply({ content: `Your language has been set to ${interaction.values[0]}!`, ephemeral: true })
         }
-        
+
         if (interaction.customId.startsWith("votephase")) {
             if (interaction.values[0].split("-")[1] == interaction.member.nickname) return interaction.reply({ content: `Trying to win as fool by voting yourself won't get you anywhere. Get a life dude.`, ephemeral: true })
             if (interaction.member.roles.cache.has(ids.dead)) return interaction.reply({ content: `You're dead, you can't vote!`, ephemeral: true })
