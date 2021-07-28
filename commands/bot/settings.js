f991f26fc574f5d64df9641aec340d28094c8e0d
thewilloftheshadow <thewilloftheshadow@users.noreply.github.com>
2021-07-28 18:16:36 +0000

    Prettified Code!

--- commands/bot/settings.js
+++ commands/bot/settings.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async (message, args) => {
         let languageDropdown = new MessageSelectMenu().setCustomId(`configLanguage-${message.author.id}`).setMaxValues(1)
         let allLanguages = require("../../i10n/allLanguages.js")
-        allLanguages.forEach(x => languageDropdown.addOptions({label: x, value: x}))
-        message.channel.send({content: "Customize your settings below!", components: [new MessageActionRow().addComponents(languageDropdown)]})
+        allLanguages.forEach((x) => languageDropdown.addOptions({ label: x, value: x }))
+        message.channel.send({ content: "Customize your settings below!", components: [new MessageActionRow().addComponents(languageDropdown)] })
     },
 }
