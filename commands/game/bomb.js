b668474a85d3c3dc82e2ef5983808211b1a458ed
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-07-29 12:05:30 -0500

    Fix my code

--- commands/game/bomb.js
+++ commands/game/bomb.js
@@ -31,7 +31,7 @@ module.exports = {
             let sected = message.guild.channels.cache.find((c) => c.name === "sect-members")
             let cupid = message.guild.channels.cache.filter((c) => c.name === "priv-cupid").keyArray("id")
             for (let x = 0; x < cupid.length; x++) {
-                let couple = db.get(`couple_${cupid[x]}`)
+                let couple = db.get(`couple_${cupid[x]}`) || [0, 0]
                 if (message.author.nickname === couple[0]) {
                     if (!sected.permissionsFor(message.member).has(["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]) && (guy1.nickname === couple[1] || guy2.nickname === couple[1] || guy3.nickname === couple[1])) return message.channel.send("You can not bomb your lover!")
                 }
