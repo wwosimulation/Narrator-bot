0ade7b1238b39be1b4e87692d8c04b4a09ac0c31
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-07-29 12:18:20 -0500

    Fix my code

--- commands/game/shoot.js
+++ commands/game/shoot.js
@@ -35,7 +35,7 @@ module.exports = {
                 if (sected.permissionsFor(message.member).has(["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]) && db.get(`role_${guy.id}`) === "Sect Leader") return message.channel.send("You can not shoot the leader of the sect if you are sected!")
                 let cupid = message.guild.channels.cache.filter((c) => c.name === "priv-cupid").keyArray("id")
                 for (let x = 0; x < cupid.length; x++) {
-                    let couple = db.get(`couple_${cupid[x]}`)
+                    let couple = db.get(`couple_${cupid[x]}`) || [0, 0]
                     if (message.author.nickname === couple[0]) {
                         if (!sected.permissionsFor(message.member).has(["VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]) && guy.nickname === couple[1]) return message.channel.send("You can not shoot your lover!")
                     }
