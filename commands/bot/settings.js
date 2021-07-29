0fd55b1cf99e6f389a78faf3fe65a7271e6f955c
Shadow <william.shadow@outlook.com>
2021-07-29 09:51:34 -0500

    Update settings.js

--- commands/bot/settings.js
+++ commands/bot/settings.js
@@ -8,7 +8,7 @@ module.exports = {
         let languageDropdown = new MessageSelectMenu().setCustomId(`configLanguage-${message.author.id}`).setMaxValues(1).setPlaceholder("Language")
         let allLanguages = require("../../i10n/allLanguages.js")
         allLanguages.forEach((x) => {
-            let langName = getLangNameFromCode(x).native
+            let langName = getLangNameFromCode(x)?.native || x
             languageDropdown.addOptions({ label: langName, value: x })
         })
         message.channel.send({ content: message.i10n("customizeSettings"), components: [new MessageActionRow().addComponents(languageDropdown)] })
