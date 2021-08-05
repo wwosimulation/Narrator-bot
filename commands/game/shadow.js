55c1cd244cdfb64b79f96780f418b580454b9bfd
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-04 20:31:13 -0500

    Update shadow.js

--- commands/game/shadow.js
+++ commands/game/shadow.js
@@ -7,6 +7,7 @@ module.exports = {
     run: async (message, args, client) => {
         if (message.channel.name == "priv-shadow-wolf") {
             let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
+            let dead = message.guild.roles.cache.find((r) => r.name === "Dead")
             let abil = db.get(`shadow_${message.channel.id}`)
             let isDay = db.get(`isDay`)
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send("You can't manipulate voting when dead, dumb.")
