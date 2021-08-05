8f7ac1361e5d3ebf1b4f6678ad7c33b8de5debd9
FinnyMarigold58 <FinnyMarigold58@users.noreply.github.com>
2021-08-05 01:34:06 +0000

    Prettified Code!

--- commands/narrator/enable.js
+++ commands/narrator/enable.js
@@ -3,32 +3,32 @@ module.exports = {
     gameOnly: true,
     narratorOnly: true,
     run: async (message, args, client) => {
-       let mid = db.get("entermsg")
-       if (!args[0]) return message.channel.send(`Please specify the button to enable (join/spec)`)
-       if (args[0] == "join") {
-        message.guild.channels.cache
-        .find((x) => x.name == "enter-game")
-        .messages.fetch(mid)
-        .then((m) => {
-            let allc = m.components
-            let row = allc[0]
-            let jgbutton = row.components[0]
-            let specbutton = row.components[1]
-            jgbutton.disabled = false
-            m.edit({ components: [new MessageActionRow().addComponents(jgbutton, specbutton)] })
-        })
-       } else if (args[0] == "spec") {
-        message.guild.channels.cache
-        .find((x) => x.name == "enter-game")
-        .messages.fetch(mid)
-        .then((m) => {
-            let allc = m.components
-            let row = allc[0]
-            let jgbutton = row.components[0]
-            let specbutton = row.components[1]
-            specbutton.disabled = false
-            m.edit({ components: [new MessageActionRow().addComponents(jgbutton, specbutton)] })
-        })
-       } else return message.channel.send(`I could not find the button to enable`)
+        let mid = db.get("entermsg")
+        if (!args[0]) return message.channel.send(`Please specify the button to enable (join/spec)`)
+        if (args[0] == "join") {
+            message.guild.channels.cache
+                .find((x) => x.name == "enter-game")
+                .messages.fetch(mid)
+                .then((m) => {
+                    let allc = m.components
+                    let row = allc[0]
+                    let jgbutton = row.components[0]
+                    let specbutton = row.components[1]
+                    jgbutton.disabled = false
+                    m.edit({ components: [new MessageActionRow().addComponents(jgbutton, specbutton)] })
+                })
+        } else if (args[0] == "spec") {
+            message.guild.channels.cache
+                .find((x) => x.name == "enter-game")
+                .messages.fetch(mid)
+                .then((m) => {
+                    let allc = m.components
+                    let row = allc[0]
+                    let jgbutton = row.components[0]
+                    let specbutton = row.components[1]
+                    specbutton.disabled = false
+                    m.edit({ components: [new MessageActionRow().addComponents(jgbutton, specbutton)] })
+                })
+        } else return message.channel.send(`I could not find the button to enable`)
     },
 }
