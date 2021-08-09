3709d5b1660f5318e68051ea624ffbf70118e91d
FinnyMarigold58 <finny6420@gmail.com>
2021-08-09 00:53:46 -0500

    Added disable and enable command

---
+++ commands/narrator/enable.js
@@ -0,0 +1,37 @@
+const db = require("quick.db")
+const { MessageActionRow } = require("discord.js")
+
+module.exports = {
+    name: "enable",
+    gameOnly: true,
+    narratorOnly: true,
+    run: async (message, args, client) => {
+        let mid = db.get("entermsg")
+        if (!args[0]) return message.channel.send(`Please specify the button to enable (join/spec)`)
+        if (args[0] == "join") {
+            message.guild.cache
+                .find((c) => c.name == "enter-game")
+                .messages.fetch(mid)
+                .then((m) => {
+                    let allc = m.components
+                    let row = allc[0]
+                    let jgbutton = row.components[0]
+                    let specbutton = row.components[1]
+                    jgbutton.disabled = false
+                    m.edit({ components: [new MessageActionRow().addComponents(jgbutton, specbutton)] })
+                })
+        } else if (args[0] == "spec") {
+            message.guild.cache
+                .find((c) => c.name == "enter-game")
+                .messages.fetch(mid)
+                .then((m) => {
+                    let allc = m.components
+                    let row = allc[0]
+                    let jgbutton = row.components[0]
+                    let specbutton = row.components[1]
+                    specbutton.disabled = false
+                    m.edit({ components: [new MessageActionRow().addComponents(jgbutton, specbutton)] })
+                })
+        } else return message.channel.send(`I could not find the button.`)
+    }
+}
