a4d4d60b3d0f5d47abec3e8b339ce7eed6d674db
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-11 01:04:03 -0500

    Update srole.js
    Fixes wwosimualtion/tracker#237

--- commands/narrator/srole.js
+++ commands/narrator/srole.js
@@ -21,9 +21,9 @@ module.exports = {
             allPlayers.push(guy.id)
         }
         let exists = false
-        let allchan = message.guild.channels.cache.filter((c) => c.name.startsWith("priv"))
-        for (let a = 0; a < allchan.keyArray("id").length; a++) {
-            let chan = message.guild.channels.cache.get(allchan.keyArray("id")[a])
+        let allchan = message.guild.channels.cache.filter((c) => c.name.startsWith("priv")).keyArray("id")
+        for (let a = 0; a < allchan.length; a++) {
+            let chan = message.guild.channels.cache.get(allchan[a])
             if (chan) {
                 for (let b = 1; b <= alive.members.size; b++) {
                     let tt = message.guild.members.cache.find((m) => m.nickname === b.toString())
