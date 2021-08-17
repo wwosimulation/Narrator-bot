0ff7b0940f5beb6782fa26e3e6c4bb5d485825bc
Shadow <william.shadow@outlook.com>
2021-08-16 23:29:07 -0500

    Update suggest.js

--- commands/bot/suggest.js
+++ commands/bot/suggest.js
@@ -1,27 +1,27 @@
-const { ids, github } = require("../../config")
+// const { ids, github } = require("../../config")
 
-module.exports = {
-    name: "suggest",
-    run: async (message, args, client) => {
-        if (!args[0]) return message.channel.send(message.i10n("suggestNoArguments"))
-        let t = ""
-        if (message.attachments.size > 0) {
-            if (message.guild.id == ids.server.game) {
-                message.channel.send(message.i10n("suggestNoGameServer"))
-            } else {
-                message.attachments.forEach((a) => (t += a.url + "\n"))
-            }
-        }
-        let issue = {
-            title: `Suggestion ${message.id}`,
-            body: `${args.join(" ")}\n\n** **\nThe above suggestion was sent by ${message.author.tag}\nUser ID: ${message.author.id}\nLocation: #${message.channel.name} (${message.channel.id}) in ${message.guild.name}`,
-            labels: ["Suggestion"],
+// module.exports = {
+//     name: "suggest",
+//     run: async (message, args, client) => {
+//         if (!args[0]) return message.channel.send(message.i10n("suggestNoArguments"))
+//         let t = ""
+//         if (message.attachments.size > 0) {
+//             if (message.guild.id == ids.server.game) {
+//                 message.channel.send(message.i10n("suggestNoGameServer"))
+//             } else {
+//                 message.attachments.forEach((a) => (t += a.url + "\n"))
+//             }
+//         }
+//         let issue = {
+//             title: `Suggestion ${message.id}`,
+//             body: `${args.join(" ")}\n\n** **\nThe above suggestion was sent by ${message.author.tag}\nUser ID: ${message.author.id}\nLocation: #${message.channel.name} (${message.channel.id}) in ${message.guild.name}`,
+//             labels: ["Suggestion"],
 
-            owner: github.org,
-            repo: github.repo,
-        }
+//             owner: github.org,
+//             repo: github.repo,
+//         }
 
-        let done = await client.github.request(`POST /repos/${github.org}/${github.repo}/issues`, issue)
-        message.channel.send(message.i10n("suggestSuccess", { url: done.data.html_url }))
-    },
-}
+//         let done = await client.github.request(`POST /repos/${github.org}/${github.repo}/issues`, issue)
+//         message.channel.send(message.i10n("suggestSuccess", { url: done.data.html_url }))
+//     },
+// }
