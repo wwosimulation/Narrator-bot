23f768514cf09e6ee5483ce1619852325b70b7d8
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-20 22:40:27 -0500

    Fixes alias matching with revive

--- commands/fun/reverse.js
+++ commands/fun/reverse.js
@@ -3,7 +3,6 @@ module.exports = {
     name: "reverse",
     description: "Use this command to reverse sentences or words.",
     usage: `${process.env.PREFIX}reverse`,
-    aliases: ["rev"],
     run: async (message, args, client) => {
         const newargs = args.join(" ")
         const msg = newargs.split("").reverse().join("")
