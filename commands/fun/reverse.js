24d7a9fdc68013d87dc08f0aa2c7b7ee51f387e4
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-20 22:47:48 -0500

    Checks there is args

--- commands/fun/reverse.js
+++ commands/fun/reverse.js
@@ -4,6 +4,7 @@ module.exports = {
     description: "Use this command to reverse sentences or words.",
     usage: `${process.env.PREFIX}reverse`,
     run: async (message, args, client) => {
+        if (!args[0]) return message.channel.send(`Please use the correct command format (${process.env.PREFIX}reverse <sentence>) `)
         const newargs = args.join(" ")
         const msg = newargs.split("").reverse().join("")
 
