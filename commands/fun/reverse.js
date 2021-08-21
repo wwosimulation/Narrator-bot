0a7ac7c6c63e72af0365c51ec9a8ea48baf87e0d
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-20 23:02:04 -0500

    Uses webhooks

--- commands/fun/reverse.js
+++ commands/fun/reverse.js
@@ -1,3 +1,5 @@
+const config = require("../../config")
+
 module.exports = {
     name: "reverse",
     description: "Use this command to reverse sentences or words.",
@@ -6,7 +8,19 @@ module.exports = {
         if (!args[0]) return message.channel.send(`Please use the correct command format (${process.env.PREFIX}reverse <sentence>) `)
         const newargs = args.join(" ")
         const msg = newargs.split("").reverse().join("")
-
-        message.channel.send(msg)
+        message.delete()
+        if (message.channel.permissionsFor(message.guild.me).has("MANAGE_WEBHOOKS")) {
+            let allHooks = await message.channel.fetchWebhooks()
+            let hook = allHooks.find((x) => x.owner.id == client.user.id)
+            if (!hook)
+                hook = await message.channel.createWebhook(client.user.username, {
+                    avatar: client.user.avatarURL(),
+                    reason: `${process.env.PREFIX}reverse command`,
+                })
+            hook.send(`${msg}`, { username: message.member.nickname ? message.member.nickname : message.author.username, avatarURL: message.author.avatarURL() })
+        } else {
+            let userEmbed = new Discord.MessageEmbed().setDescription(`<@${message.author.id}>`).setColor("#1FFF43")
+            message.channel.send(msg)
+        }
     },
 }
