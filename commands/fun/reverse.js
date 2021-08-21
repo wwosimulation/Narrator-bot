d8dd00cbbd7990a4d7ca43452351befffdcf6294
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-20 22:37:52 -0500

    Adds advanced help stuff for Hackante

--- commands/fun/reverse.js
+++ commands/fun/reverse.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js")
 module.exports = {
     name: "reverse",
+    description: "Use this command to reverse sentences or words.",
+    usage: `${process.env.PREFIX}reverse`,
     aliases: ["rev"],
     run: async (message, args, client) => {
         const newargs = args.join(" ")
