5ffa238941ba4ad9b72a6126b80fb828c5052b63
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-08-20 22:26:49 -0500

    Init reverse command

---
+++ commands/fun/reverse.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+module.exports = {
+    name: "reverse",
+    aliases: ["rev"],
+    run: async (message, args, client) => {
+        const newargs = args.join(" ")
+        const msg = newargs.split("").reverse().join("")
+        
+        message.channel.send(msg)
+    },
+}
