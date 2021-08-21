40af16ddc630f650ccc2bbd7ff4b04f445f73991
FinnyMarigold58 <FinnyMarigold58@users.noreply.github.com>
2021-08-21 03:27:12 +0000

    Prettified Code!

--- commands/fun/reverse.js
+++ commands/fun/reverse.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async (message, args, client) => {
         const newargs = args.join(" ")
         const msg = newargs.split("").reverse().join("")
-        
+
         message.channel.send(msg)
     },
 }
