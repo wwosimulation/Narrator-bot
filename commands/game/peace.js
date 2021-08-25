14f4c50b150508f38e73e1a9346d18e56d2e79bc
Hackante <Hackante@users.noreply.github.com>
2021-08-25 16:24:16 +0000

    Prettified Code!

--- commands/game/peace.js
+++ commands/game/peace.js
@@ -4,19 +4,19 @@ module.exports = {
     name: "peace",
     gameOnly: true,
     run: async (message, args, client) => {
-        if(message.channel.name === "priv-prognosticator") {
+        if (message.channel.name === "priv-prognosticator") {
             let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
             let ability = db.get(`peace_${message.channel.id}`) || "no"
             let dayChat = message.guild.channels.cache.find((c) => c.name === "day-chat")
             let isNight = db.get(`isNight`)
             let nightCount = db.get(`nightCount`) || 1
-            if(!message.member.roles.cache.has(alive.id)) return message.channel.send({content: "You are dead..."})
-            if(ability == "yes") return message.channel.send({content: "You already used that ability!"})
-            if(isNight && nightCount > 1) {
-                dayChat.send({content:`${alive}\nPeace be upon you dear villagers. No one can die the following night!`})
+            if (!message.member.roles.cache.has(alive.id)) return message.channel.send({ content: "You are dead..." })
+            if (ability == "yes") return message.channel.send({ content: "You already used that ability!" })
+            if (isNight && nightCount > 1) {
+                dayChat.send({ content: `${alive}\nPeace be upon you dear villagers. No one can die the following night!` })
                 db.set(`peace_${message.channel.id}`, nightCount + 1)
-                message.channel.send({content: "Your message has reached the villagers."})
+                message.channel.send({ content: "Your message has reached the villagers." })
             }
         }
-    }
+    },
 }
