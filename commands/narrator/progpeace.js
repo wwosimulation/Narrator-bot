40cd5a07ae39e456ef106eae85b85191feeb5c45
Hackante <Hackante@users.noreply.github.com>
2021-08-26 20:16:26 +0000

    Prettified Code!

--- commands/narrator/progpeace.js
+++ commands/narrator/progpeace.js
@@ -6,5 +6,5 @@ module.exports = {
         let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
         let dayChat = message.guild.channels.cache.find((c) => c.name === "day-chat")
         dayChat.send({ content: `${alive}\nPeace be upon you dear villagers. No one can die the following night!` })
-    }
+    },
 }
