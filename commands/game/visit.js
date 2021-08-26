2f232702bc65675f6a5cae3ca1d2544fee0a9564
Hackante <kante.hagen@gmail.com>
2021-08-26 21:46:59 +0200

    rl and bomber included

--- commands/game/visit.js
+++ commands/game/visit.js
@@ -1,4 +1,5 @@
 const db = require("quick.db")
+const { fn } = require("../../config")
 
 module.exports = {
     name: "visit",
@@ -8,6 +9,7 @@ module.exports = {
             let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
             let isNight = db.get(`isNight`)
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send("You cannot use the ability now!")
+            if (fn.peaceCheck(message, db) === true && isNight == "yes") return message.channel.send({content:"We have a peaceful night. You decided to stay at home today."})
             if (!args[0]) return message.channel.send("Who are you visiting? Mention the player.")
 
             let guy = message.guild.members.cache.find((m) => m.nickname === args[0]) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((m) => m.user.username === args.join(" ")) || message.guild.members.cache.find((m) => m.user.tag === args.join(" "))
