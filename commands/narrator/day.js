06f0c3be9721d53a8b0e1a343920bed8d28330ce
Groot <67421407+Gorott@users.noreply.github.com>
2021-08-28 14:39:12 +0200

    Update day.js

--- commands/narrator/day.js
+++ commands/narrator/day.js
@@ -924,6 +924,7 @@ module.exports = {
         }
         // checking dc kills
         for (let m = 1; m <= alive.members.size + dead.members.size; m++) {
+            let tempguy = message.guild.members.cache.find((m) => m.nickname === m.toString())
             let role = db.get(`role_${tempguy.id}`).toLowerCase()
             if (role == 'dreamcatcher') {
                 TheDC = tempguy
