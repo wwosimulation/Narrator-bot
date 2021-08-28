c7293f92b85a7cf90808a752bdb7ae9880e7bfe8
Hackante <Hackante@users.noreply.github.com>
2021-08-28 15:54:36 +0000

    Prettified Code!

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ client.buttonPaginator = async (authorID, msg, embeds, pageNowIndex) => {
     collector.on("collect", (interaction) => {
         // let embedFilter = (embed) => (embed.footer === interaction.message.embeds[0].footer)
         // pageNowIndex = embeds.findIndex(embedFilter)
-        let footerArr = interaction.message.embeds[0].footer.text.replace("Page ", "").split('/')
+        let footerArr = interaction.message.embeds[0].footer.text.replace("Page ", "").split("/")
         pageNowIndex = parseInt(footerArr[0]) - 1
         console.log(interaction.message.embeds)
 
