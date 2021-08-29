d6caebdc2c0a2fb9e7331a6403e0745fac61c1d8
Hackante <Hackante@users.noreply.github.com>
2021-08-29 22:34:51 +0000

    Prettified Code!

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -67,14 +67,14 @@ module.exports = {
                 amount = 30 * bonus
                 item = "coins"
                 emote = `${config.getEmoji("coin", client)}`
-                await data.updateOne({ $set: { 'daily.day': -1 } })
+                await data.updateOne({ $set: { "daily.day": -1 } })
                 await data.updateOne({ $inc: { coins: amount } })
             }
             let dailymsg = new Discord.MessageEmbed().setTitle("Daily Rewards! Woohooo!").setDescription(`${message.i10n("daily", { emoji: emote, number: amount, prize: item })}${extra}`)
             message.channel.send({ embeds: [dailymsg] })
 
-            await data.updateOne({ $inc: { 'daily.day': 1 } })
-            await data.updateOne({ $set: { 'daily.last': Date.now() } })
+            await data.updateOne({ $inc: { "daily.day": 1 } })
+            await data.updateOne({ $set: { "daily.last": Date.now() } })
             /*data.daily.day++
             data.daily.last = 
 
