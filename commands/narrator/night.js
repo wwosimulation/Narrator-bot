c36deb07d58ab380dbe6bd73dd8941352ab6ff9a
Groot <67421407+Gorott@users.noreply.github.com>
2021-08-30 21:57:07 +0200

    Update night.js

--- commands/narrator/night.js
+++ commands/narrator/night.js
@@ -311,13 +311,13 @@ module.exports = {
                                         chan1.send(`<@&${alive.id}>\n` + getRole(db.get(`role_${guy.id}`).replace(" ", "-").toLowerCase()).description)
                                         db.set(`hypnotized_${dc[a]}`, guy.nickname)
                                         if (db.get(`role_${guy.id}`) == 'Bomber') {
-                                            chan.permissionOverwrites.edit(tempguy.id, {
+                                            chan.permissionOverwrites.edit(player.id, {
 
                                                 SEND_MESSAGES: false,
                                             })
                                             message.channel.send(`Your channel has been locked for 1 minute due to a bug including dreamcatcher and bomber.`)
                                             setTimeout(async function () {
-                                                chan.permissionOverwrites.edit(tempguy.id, {
+                                                chan.permissionOverwrites.edit(player.id, {
                                                     SEND_MESSAGES: true,
                                                 })
                                                 message.channel.send(`<@&${alive.id}>, Your channel has been opened.`)
