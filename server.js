11d975819b9c56ee01dcf572a6e9c0137d0a046a
Hackante <85803963+Hackante@users.noreply.github.com>
2021-08-30 22:00:08 +0200

    Leaderboard (#136)
    * Update leaderboard.js
    
    * Leaderboard now works with paginator
    
    * Prettified Code!
    
    * missing s at players
    
    * fixes
    
    * fixes
    
    * embeds in array
    
    * gets members a different way
    
    * Prettified Code!
    
    * trying to await
    
    * Prettified Code!
    
    * fetch() ther user
    
    * fixes
    
    * changed the user getting
    
    * Prettified Code!
    
    * tests
    
    * Prettified Code!
    
    * tests 2
    
    * normal again
    
    * pixing paginator issue
    
    * paginator change
    
    * Prettified Code!
    
    * error handling
    
    * Prettified Code!
    
    * paginator back to origin
    
    * Prettified Code!
    
    * created and used buttonPaginator
    
    * Prettified Code!
    
    * different embed getting
    
    * fixing error
    
    * defer replying
    
    * inactive
    
    * interaction.update()
    
    * embedFilter updated
    
    * Prettified Code!
    
    * indexOf()
    
    * filter update
    
    * Prettified Code!
    
    * -1 issue resolved
    
    * new way to get index
    
    * Prettified Code!
    
    * logging embeds
    
    * footer text usage
    
    * Prettified Code!
    
    * logging to find errs
    
    * back to no errs
    
    * back
    
    * changed buttonPaginaator
    
    * Prettified Code!
    
    * embeds in array
    
    * working with quickdb
    
    * updated buttonPaginator
    
    * Prettified Code!
    
    * more than one interaction
    
    * Prettified Code!
    
    * fixing errs
    
    * awaiting components
    
    * Prettified Code!
    
    * error handling
    
    * errors 2
    
    * msg edit new msg
    
    * not awaiting
    
    * awaiting
    
    * changeing back to collector
    
    * Prettified Code!
    
    * err log
    
    * Prettified Code!
    
    * removing console log
    
    * button Paginator changes
    
    * Prettified Code!
    
    * p++ => p = p + 1
    
    * defining p
    
    * removing commeted code
    
    * only gt 0 results
    
    * Prettified Code!
    
    * not including embeds with 1 or less embeds
    
    Co-authored-by: Hackante <Hackante@users.noreply.github.com>

--- server.js
+++ server.js
@@ -109,6 +109,44 @@ client.paginator = async (author, msg, embeds, pageNow, addReactions = true) => 
     }
 }
 
+client.buttonPaginator = async (authorID, msg, embeds, page, addButtons = true) => {
+    if(embeds.length <= 1) return
+
+    // buttons
+    let buttonBegin = new Discord.MessageButton({ style: "SUCCESS", emoji: "⏪", customId: "begin" })
+    let buttonBack = new Discord.MessageButton({ style: "SUCCESS", emoji: "◀", customId: "back" })
+    let buttonNext = new Discord.MessageButton({ style: "SUCCESS", emoji: "▶", customId: "next" })
+    let buttonEnd = new Discord.MessageButton({ style: "SUCCESS", emoji: "⏩", customId: "end" })
+
+    // rows
+    let activeRow = new Discord.MessageActionRow().addComponents([buttonBegin, buttonBack, buttonNext, buttonEnd])
+    let deadRow = new Discord.MessageActionRow().addComponents([buttonBegin.setDisabled(), buttonBack.setDisabled(), buttonNext.setDisabled(), buttonEnd.setDisabled()])
+
+    // adding buttons
+    if (addButtons) msg.edit({ components: [activeRow] })
+
+    // collecting interactions
+    let filter = (interaction) => interaction.isButton() === true && interaction.user.id === authorID
+    let collector = msg.createMessageComponentCollector({ filter, time: 30 * 1000 })
+
+    let p = --page
+
+    collector.on("collect", async (button) => {
+        if (button.customId === "begin") p = 0
+        else if (button.customId === "back") {
+            if (p != 0) p--
+            else p = embeds.length - 1
+        } else if (button.customId === "next") {
+            if (p != embeds.length - 1) p++
+            else p = 0
+        } else if (button.customId === "end") p = embeds.length - 1
+        await button.update({ embeds: [embeds[p]] })
+    })
+    collector.on("end", () => {
+        msg.edit({ components: [deadRow] })
+    })
+}
+
 client.debug = async (options = { game: false }) => {
     let data = {}
     data.night = db.get(`nightCount`)
