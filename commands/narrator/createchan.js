5ce9f5683a7cba2c7897b5c44d21ce9d3ee44adc
Groot <67421407+Gorott@users.noreply.github.com>
2021-09-01 14:27:27 +0200

    Create createchan.js

---
+++ commands/narrator/createchan.js
@@ -0,0 +1,18 @@
+const { getRole } = require('../../config')
+
+module.exports = {
+    name: "createchan",
+    description: "Kill a player in corrupter's name.",
+    usage: `${process.env.PREFIX}corrkill <player>`,
+    aliases: ['cc'],
+    gameOnly: true,
+    narratorOnly: true,
+    run: async (message, args, client) => {
+        let chan1 = await message.guild.channels.create(`priv-${args[0].replace(" ", "-").toLowerCase()}`, {
+            parent: args[1],
+        })
+        let chan2 = await message.guild.channels.create(`priv-${args[0].replace(" ", "-").toLowerCase()}`, {
+            parent: args[1],
+        })
+    }
+}
