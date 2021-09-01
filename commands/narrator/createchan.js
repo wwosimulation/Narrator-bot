0d2857c10b6ad9fa356d44dec9c0922622520372
Gorott <Gorott@users.noreply.github.com>
2021-09-01 12:28:04 +0000

    Prettified Code!

--- commands/narrator/createchan.js
+++ commands/narrator/createchan.js
@@ -1,10 +1,10 @@
-const { getRole } = require('../../config')
+const { getRole } = require("../../config")
 
 module.exports = {
     name: "createchan",
     description: "Kill a player in corrupter's name.",
     usage: `${process.env.PREFIX}corrkill <player>`,
-    aliases: ['cc'],
+    aliases: ["cc"],
     gameOnly: true,
     narratorOnly: true,
     run: async (message, args, client) => {
@@ -14,5 +14,5 @@ module.exports = {
         let chan2 = await message.guild.channels.create(`priv-${args[0].replace(" ", "-").toLowerCase()}`, {
             parent: args[1],
         })
-    }
+    },
 }
