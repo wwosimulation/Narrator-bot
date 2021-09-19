b558821355a2047e98b4e2127eadfa75c9e27682
Hackante <Hackante@users.noreply.github.com>
2021-09-19 20:00:47 +0000

    Prettified Code!

--- commands/bot/register.js
+++ commands/bot/register.js
@@ -6,18 +6,18 @@ module.exports = {
     description: "Register your invite link to be able to get the Invite badge.",
     usage: `${process.env.PREFIX}register <invite>`,
     run: async (message, args, client) => {
-        if(!args) message.channel.send({content:`Invalid format! Use \`${this.usage}\``})
+        if (!args) message.channel.send({ content: `Invalid format! Use \`${this.usage}\`` })
         let code = ""
         let sim = client.guilds.get("simID")
 
-        if(args[0].startsWith("https://discord.gg/")) {
-            if(sim.invites.cache.has(args[0])) {
-                if(!sim.invites.cache.get([args[0]]).inviter === message.author) return message.channel.send({content:"It seems like this invite was not created by you. Please use your own invite!"})
+        if (args[0].startsWith("https://discord.gg/")) {
+            if (sim.invites.cache.has(args[0])) {
+                if (!sim.invites.cache.get([args[0]]).inviter === message.author) return message.channel.send({ content: "It seems like this invite was not created by you. Please use your own invite!" })
                 code = args[0].split("https://discord.gg/")[1]
-                await players.findOneAndUpdate({user: message.author.id}, {$set:{"badges.invite.link": code}}, {upsert: true})
+                await players.findOneAndUpdate({ user: message.author.id }, { $set: { "badges.invite.link": code } }, { upsert: true })
             } else {
-                message.channel.send({content:`${args[0]} is not an invite code from this server!`})
+                message.channel.send({ content: `${args[0]} is not an invite code from this server!` })
             }
         }
-    }
+    },
 }
