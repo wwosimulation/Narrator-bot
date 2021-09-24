2f3264a355b1582691f02306eb170f448235ecee
Hackante <Hackante@users.noreply.github.com>
2021-09-24 22:11:28 +0000

    Prettified Code!

--- commands/staff/player.js
+++ commands/staff/player.js
@@ -26,61 +26,60 @@ module.exports = {
         let operators = ["set", "add", "remove"]
         let force = false
 
-        if(!target) return message.channel.send({content:`Invalid player! Use\n\`${this.usage}\``})
-        if(!columns.includes(column)) return message.channel.send({content:`Invalid column! Available columns are ${columns.map((column) => `\`${column}\``).join(" ")}`})
-        if(!operators.includes(operator)) return message.channel.send({content:`Invalid operator! Available operators are ${operators.map((operator) => `\`${operator}\``).join(" ")}`})
-        if(!value) return message.channel.send({content:`Invalid usage! Use\n\`${this.usage}\``})
-        if(options !== "none" && ["-f", "force", "--force"].includes(options)) force = true
+        if (!target) return message.channel.send({ content: `Invalid player! Use\n\`${this.usage}\`` })
+        if (!columns.includes(column)) return message.channel.send({ content: `Invalid column! Available columns are ${columns.map((column) => `\`${column}\``).join(" ")}` })
+        if (!operators.includes(operator)) return message.channel.send({ content: `Invalid operator! Available operators are ${operators.map((operator) => `\`${operator}\``).join(" ")}` })
+        if (!value) return message.channel.send({ content: `Invalid usage! Use\n\`${this.usage}\`` })
+        if (options !== "none" && ["-f", "force", "--force"].includes(options)) force = true
 
-        let playerData = await players.findOne({user: target.id})
+        let playerData = await players.findOne({ user: target.id })
 
-        if(columns.slice(0, 7).includes(column)) { 
-            if(["rose", "bouquet", "lootbox"].includes(column)) column = `inventory.${column}`
+        if (columns.slice(0, 7).includes(column)) {
+            if (["rose", "bouquet", "lootbox"].includes(column)) column = `inventory.${column}`
 
             let update = {}
             let operatorObj = {}
 
             let amount = args[3]
-            if(isNaN(args[3]) || amount % 1 != 0 || amount <= 0) return message.channel.send({content:message.i10n("amountInvalid", {amount: amount})})
+            if (isNaN(args[3]) || amount % 1 != 0 || amount <= 0) return message.channel.send({ content: message.i10n("amountInvalid", { amount: amount }) })
 
             switch (operator) {
                 case "set":
                     update[column] = amount
-                    operatorObj['$set'] = update
-                    break;
-                case "add": 
+                    operatorObj["$set"] = update
+                    break
+                case "add":
                     update[column] = amount
-                    operatorObj['$inc'] = update
-                    break;
+                    operatorObj["$inc"] = update
+                    break
                 case "remove":
-                    if((playerData && playerData.coins > amount) || force) {
+                    if ((playerData && playerData.coins > amount) || force) {
                         update[column] = -amount
-                        operatorObj['$inc'] = update
+                        operatorObj["$inc"] = update
+                    } else if (!playerData || !playerData[column] < amount) {
+                        return message.channel.send({ content: `You try to remove more ${column} than the user has. If you want to continue run this command again with \`force\` at the end.` })
                     }
-                    else if(!playerData || !playerData[column] < amount) {
-                        return message.channel.send({content:`You try to remove more ${column} than the user has. If you want to continue run this command again with \`force\` at the end.`})
-                    }
-                    break;
+                    break
             }
-            await players.updateOne({user: target.id}, operatorObj, {upsert: true}) //upsert in case there is no player with this id
-            message.channel.send({content:`${capitalizeFirstLetter(column)} updated for ${target.user ? target.user.tag : target.tag}`})
+            await players.updateOne({ user: target.id }, operatorObj, { upsert: true }) //upsert in case there is no player with this id
+            message.channel.send({ content: `${capitalizeFirstLetter(column)} updated for ${target.user ? target.user.tag : target.tag}` })
         }
         if (column === "badges") {
             let update = {}
             let operatorObj = {}
 
-            if(value === "invite") {
+            if (value === "invite") {
                 switch (operator) {
                     case "add":
                         update["badges.invite.unlocked"] = true
                     case "remove":
                         update["badges.invite.unlocked"] = false
                     case "set":
-                        return message.channel.send({content:"This operator does not work for badges."})
+                        return message.channel.send({ content: "This operator does not work for badges." })
                 }
                 operatorObj["$set"] = update
-                await players.updateOne({user: target.id}, operatorObj, {upsert: true})
-                return message.channel.send({content: message.i10n("done")})
+                await players.updateOne({ user: target.id }, operatorObj, { upsert: true })
+                return message.channel.send({ content: message.i10n("done") })
             }
 
             switch (operator) {
@@ -91,10 +90,10 @@ module.exports = {
                     update[value] = playerData.badges[value]
                     operatorObj["$unset"] = update
                 case "set":
-                    return message.channel.send({content:"This operator does not work for badges."})
+                    return message.channel.send({ content: "This operator does not work for badges." })
             }
-            await players.updateOne({user: target.id}, operatorObj, {upsert: true})
-            return message.channel.send({content: message.i10n("done")})
+            await players.updateOne({ user: target.id }, operatorObj, { upsert: true })
+            return message.channel.send({ content: message.i10n("done") })
         }
-    }
+    },
 }
