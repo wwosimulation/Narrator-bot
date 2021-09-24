226d81a8ef80d0976cde59d4edc521d271ab2778
Hackante <kante.hagen@gmail.com>
2021-09-25 00:14:15 +0200

    fixing err

--- commands/bot/badges.js
+++ commands/bot/badges.js
@@ -9,7 +9,7 @@ module.exports = {
     name: "badges",
     description: "Displays all your badges.",
     usage: `${process.env.PREFIX}badges [user]`,
-    run: (message, args, client) => {
+    run: async (message, args, client) => {
         let guy = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((x) => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0] || x.user.tag === args[0])
         if (!guy) guy = message.author
 
