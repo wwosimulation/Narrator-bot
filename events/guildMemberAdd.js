b34ca792327424ecc596f5552d33330340dd10af
Hackante <Hackante@users.noreply.github.com>
2021-09-26 14:59:25 +0000

    Prettified Code!

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,6 @@ const { ids } = require("../config")
 
 module.exports = (client) => {
     client.on("guildMemberAdd", async (member) => {
-
         member.dbUser = await players.findOne({ user: member.id }).exec()
         if (!member.dbUser) member.dbUser = new players({ user: member.author.id }).save()
 
@@ -17,14 +16,14 @@ module.exports = (client) => {
         if (member.guild.id !== sim.id) return
         client.invites.every(async (invite) => {
             let inv = await sim.invites.resolve(invite.code)
-            if(inv.uses !== invite.uses) {
-                await players.updateOne({"badges.invite.code": invite.code}, {$inc:{"badges.invite.members": 1}})
-                let guy = await players.findOne({"badges.invite.code": invite.code})
-                if(guy.badges.invite.unlocked === true) return
-                if(guy.badges.invite.members = 15) {
-                    await players.updateOne({"badges.invite.code": invite.code}, {$set:{"badges.invite.unlocked": true}})
+            if (inv.uses !== invite.uses) {
+                await players.updateOne({ "badges.invite.code": invite.code }, { $inc: { "badges.invite.members": 1 } })
+                let guy = await players.findOne({ "badges.invite.code": invite.code })
+                if (guy.badges.invite.unlocked === true) return
+                if ((guy.badges.invite.members = 15)) {
+                    await players.updateOne({ "badges.invite.code": invite.code }, { $set: { "badges.invite.unlocked": true } })
                     let guyUser = client.users.resolve(guy.user)
-                    guyUser.send({content:member.i10n("inviteBadgeUnlocked", { code: invite.code })})
+                    guyUser.send({ content: member.i10n("inviteBadgeUnlocked", { code: invite.code }) })
                 }
             }
         })
