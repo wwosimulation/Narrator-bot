3e26ea306032a9e40b51770502cef5440c4c7355
Hackante <kante.hagen@gmail.com>
2021-09-27 20:55:45 +0200

    trying to get errs

--- commands/dev/reloadslash.js
+++ commands/dev/reloadslash.js
@@ -5,17 +5,22 @@ module.exports = {
     description: "Loads slash commands, add them if they don't exist yet and overrides the permissions.",
     usage: `${process.env.PREFIX}reloadslash`,
     run: (message, args, client) => {
-        if (!message.member.roles.cache.has("859099415515627540") && !message.member.roles.cache.has("606123616228343812")) return message.reply({ content: "You are missing permissions to do that!" })
-        let done = 0
-        client.slashCommands.each((cmd) => {
-            cmd.server.forEach((server) => {
-                let id = ids.server[server]
-                client.application.commands.create(cmd.command, id).then((command) => {
-                    if (cmd.permissions[server]) command.permissions.set({ command: command, permissions: cmd.permissions[server] })
+        try {
+            if (!message.member.roles.cache.has("859099415515627540") && !message.member.roles.cache.has("606123616228343812")) return message.reply({ content: "You are missing permissions to do that!" })
+            let done = 0
+            client.slashCommands.each((cmd) => {
+                cmd.server.forEach((server) => {
+                    let id = ids.server[server]
+                    client.application.commands.create(cmd.command, id).then((command) => {
+                        if (cmd.permissions[server]) command.permissions.set({ command: command, permissions: cmd.permissions[server] })
+                    })
+                    done += 1
                 })
-                done += 1
             })
-        })
-        message.channel.send({ content: `${done} slash commands created/updated.` })
+            message.channel.send({ content: `${done} slash commands created/updated.` })
+        }catch(err){
+            console.error
+        }
+
     },
 }
