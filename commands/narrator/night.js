becc9246c77d28d33bedf766ce7ce5241d1fc705
Hackante <kante.hagen@gmail.com>
2021-09-28 16:45:37 +0200

    prog fixes

--- commands/narrator/night.js
+++ commands/narrator/night.js
@@ -6,7 +6,7 @@ function peaceCheck(message) {
     let res = false
     for (let i = 0; i < prog.length; i++) {
         let peace = db.get(`peace_${prog[i]}`) || "none"
-        if (peace !== "none" && peace - 1 === nightCount) return true
+        if (peace !== "none" && peace === nightCount) return true
     }
     return res
 }
