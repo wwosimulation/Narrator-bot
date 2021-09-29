50261f9c47d1a0a35bce8c488e534b506d5af275
Groot <67421407+Gorott@users.noreply.github.com>
2021-09-29 18:23:11 +0200

    update frenzy.js

--- commands/game/frenzy.js
+++ commands/game/frenzy.js
@@ -13,13 +13,14 @@ module.exports = {
             const wolfChat = message.guild.channels.cache.find((c) => c.name === "werewolves-chat")
             let isDay = db.get(`isDay`)
             const abil = db.get(`abil_${message.channel.id}`) || "no"
-
+            
             if (isDay != "yes") return message.channel.send("You can only use frenzy during the day!")
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send("You cannot use the ability now!")
 
             if (abil == "yes") return message.channel.send("You have used your ability already.")
 
             db.set(`frenzy_${message.channel.id}`, true)
+            db.set(`abil_${message.channel.id}`, "yes")
             message.react("744573088204718412")
             wolfChat.send(`${getEmoji("frenzy", client)} The Werewolf Berserk has activated it's frenzy tonight!`)
         }
