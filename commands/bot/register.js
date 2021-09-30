beae1a7066f604367b46e387aa578d1fc21931c8
Hackante <Hackante@users.noreply.github.com>
2021-09-30 15:12:33 +0000

    Prettified Code!

--- commands/bot/register.js
+++ commands/bot/register.js
@@ -10,24 +10,21 @@ module.exports = {
         let sim = client.guilds.cache.get(ids.server.sim)
         let response = new MessageEmbed().setThumbnail(message.author.avatarURL()).setTimestamp().setFooter(`Want to check which invite you registered? Use ${process.env.PREFIX}register`)
 
-        
         let guy = await players.findOne({ user: message.author.id })
         if (guy.badges.invite.code && guy.badges.invite.code !== "none") {
             response.setColor("GREEN").setDescription(message.i10n("registeredInvite", { code: guy.badges.invite.code, uses: guy.badges.invite.members }))
         } else {
             response.setColor("RED").setDescription(message.i10n("noInviteRegistered", { usage: this.usage }))
         }
-        
-        
+
         let sim = client.guilds.resolve(ids.server.sim)
         let invite = sim.invites.create("606123774978293772", { maxAge: 0, unique: true, reson: `Invite registered by ${message.author.tag}` })
         await players.findOneAndUpdate({ user: message.author.id }, { $set: { "badges.invite.code": invite.code } }, { upsert: true })
         client.allinvites.set(invite.code, sim.invites.resolve(invite.code))
         response
-        .setColor("GREEN")
-        .setDescription(message.i10n("inviteRegistered", { code: invite.code }))
-        .setTitle(message.i10n("inviteAdded"))
-
+            .setColor("GREEN")
+            .setDescription(message.i10n("inviteRegistered", { code: invite.code }))
+            .setTitle(message.i10n("inviteAdded"))
 
         /*switch (status) {
             case "valid":
