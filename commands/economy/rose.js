90e8a1a7e54ffdebd6e109ee5a8884cbd1c01d51
Hackante <85803963+Hackante@users.noreply.github.com>
2021-10-04 22:11:32 +0200

    Rose fix (#164)
    * Prettified Code!
    
    * finding error
    
    * finding error
    
    * Prettified Code!
    
    * finding error
    
    * Prettified Code!
    
    * finding error
    
    * finding error
    
    * Prettified Code!
    
    * finding error
    
    * finding error
    
    * Prettified Code!
    
    * finding error
    
    * finding error
    
    * finding error
    
    * requirements
    
    * requirements
    
    * final commit
    
    * Prettified Code!
    
    * final commit
    
    * final commit
    
    * Prettified Code!
    
    * final commit
    
    * fixing the profile command
    
    Co-authored-by: Hackante <Hackante@users.noreply.github.com>

--- commands/economy/rose.js
+++ commands/economy/rose.js
@@ -1,4 +1,4 @@
-const { players } = require("../../db.js")
+const players = require("../../schemas/players")
 
 module.exports = {
     name: "rose",
@@ -6,7 +6,7 @@ module.exports = {
     usage: `${process.env.PREFIX}rose <bouquet | single <user> [amount]>`,
     gameOnly: true,
     run: async (message, args, client) => {
-        let data = await players.findOne({ user: message.author.id }).exec()
+        let data = await players.findOne({ user: message.author.id })
         let mininarr = message.guild.roles.cache.find((r) => r.name === "Narrator Trainee")
         let narrator = message.guild.roles.cache.find((r) => r.name === "Narrator")
         let spec = message.guild.roles.cache.find((r) => r.name === "Spectator")
@@ -17,20 +17,21 @@ module.exports = {
     if (message.member.roles.cache.has(spec.id)) return message.channel.send("You can't give the rose as a spectator!")
     if (message.member.roles.cache.has(mininarr.id) || message.member.roles.cache.has(narrator.id)) return message.channel.send("You can't give the rose as a narrator!")
     */
-
+        if (!["single", "bouquet"].includes(args[0])) return message.channel.send(`Invalid format! Please use ${process.env.PREFIX}rose <bouquet | single <user> [amount]>`)
         if (!args[0]) return message.channel.send("You need to state if you want to give a rose to a player or as a bouquet!\n\nOptions: `single [player] [amount]` or `bouquet`")
+        if ((args[2] && isNaN(args[2])) || (args[2] && !isNaN(args[2]) && parseInt(args[2]) % 1 != 0) || parseInt(args[2]) <= 0) return message.channel.send("Please state an integer as amount!")
 
         if (args[0] == "single") {
-            let amount = parseInt(args[2])
-            if (data.inventory.roses < amount) return message.channel.send(`You can't give ${amount} roses if you don't have that many in your inventory!`)
+            let amount = parseInt(args[2]) || 1
+            if (data.inventory.rose < amount) return message.channel.send(`You can't give ${amount} roses if you don't have that many in your inventory!`)
             let guy = message.guild.members.cache.find((m) => m.nickname === args[1]) || message.guild.members.cache.find((m) => m.id === args[1]) || message.guild.members.cache.find((m) => m.user.username === args[1]) || message.guild.members.cache.find((m) => m.user.tag === args[1])
             if (!guy) return message.channel.send("Player does not exist!")
             if (message.member == guy) return message.channel.send("You cannot give a rose to yourself!")
-            data.inventory.rose = data.inventory.rose - amount
+            await data.updateOne({ $inc: { "inventory.rose": -amount } })
             players.findOneAndUpdate({ user: guy.id }, { $inc: { roses: amount } }).exec()
-            return message.channel.send(`You have successfully given ${args[1]} ${amount} rose${amount == 1 ? "" : "s"}!`)
+            return message.channel.send(`You have successfully given ${args[1]} ${amount} rose${amount === 1 ? "" : "s"}!`)
         } else if (args[0] == "bouquet") {
-            if (data.inventory.bouquet == 0) return message.channel.send("You don't have any bouquet!")
+            if (data.inventory.bouquet === 0) return message.channel.send("You don't have any bouquet!")
             for (let i = 0; i <= alive.members.size + dead.members.size; i++) {
                 console.log(i)
                 let guy = message.guild.members.cache.find((m) => m.nickname === i.toString())
@@ -39,9 +40,8 @@ module.exports = {
                     players.findOneAndUpdate({ user: guy.id }, { $inc: { roses: 1 } }).exec()
                 }
             }
-            data.inventory.bouquet = data.inventory.bouquet - 1
+            await data.updateOne({ $inc: { "inventory.bouquet": -1 } })
             return message.channel.send(`You have successfully given a rose to every player in the server!`)
         }
-        data.save()
     },
 }
