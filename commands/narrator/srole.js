77c04537a962f97ea5d62e1edff801e73bb6e2e5
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-10-07 21:51:55 -0500

    Update srole.js

--- commands/narrator/srole.js
+++ commands/narrator/srole.js
@@ -420,6 +420,7 @@ module.exports = {
         })
         client.commands.get("playerinfo").run(message, args, client)
         message.channel.send("If everything looks correct, use `+startgame` to start the game!")
+        db.set(`gamePhase`, -1)
         db.set(`gamemode`, args[0])
     },
 }
