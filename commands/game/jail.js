af7a11919752107adfaadd47da2a67b338c7042b
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-10-07 22:44:32 -0500

    Update jail.js

--- commands/game/jail.js
+++ commands/game/jail.js
@@ -12,11 +12,11 @@ module.exports = {
             return message.channel.send("Okay, your action has been canceled")
         }
         let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
-        let isNight = db.get(`isNight`)
+        let gamePhase = db.get(`gamePhase`)
         if (!message.member.roles.cache.has(alive.id)) return message.channel.send("You cannot use the ability now!")
         if (!args[0]) return message.channel.send("Who are you jailing? Mention the player.")
         let guy = message.guild.members.cache.find((m) => m.nickname === args[0]) || message.guild.members.cache.find((m) => m.user.username === args[0]) || message.guild.members.cache.find((m) => m.user.tag === args[0]) || message.guild.members.cache.find((m) => m.id === args[0])
-        if (isNight == "yes") return message.channel.send("You can use your ability only during the day!")
+        if (gamePhase % 3 != 0) return message.channel.send("You can use your ability only during the day!")
         if (!guy || message.member == guy) return message.reply("Invalid Target")
         db.set(`jail_${message.channel.id}`, guy.nickname)
         message.channel.send("You have decided to jail **" + guy.nickname + " " + guy.user.username + "**!")
