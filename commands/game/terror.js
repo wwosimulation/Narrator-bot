71ac3ff0e55ac3314eb98d7e17fbdb667a206ea8
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-10-07 22:58:33 -0500

    Update terror.js

--- commands/game/terror.js
+++ commands/game/terror.js
@@ -8,13 +8,13 @@ module.exports = {
             let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
             let ability = db.get(`terror_${message.channel.id}`) || "no"
             let dayChat = message.guild.channels.cache.find((c) => c.name === "day-chat")
-            let isDay = db.get(`isDay`)
+            let gamePhase = db.get(`gamePhase`)
             let dayCount = db.get(`dayCount`) || 1
             let guy = message.guild.members.cache.find((m) => m.nickname === args[0]) || message.guild.members.cache.find((m) => m.id === args[0]) || message.guild.members.cache.find((m) => m.user.username === args[0]) || message.guild.members.cache.find((m) => m.user.tag === args[0])
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send({ content: "You are dead..." })
             if (ability !== "no") return message.channel.send({ content: "You already used that ability!" })
             if (!guy || !guy.roles.cache.has(alive.id)) return message.channel.send({ content: "Invalid target!" })
-            if (isDay) {
+            if (gamePhase % 3 == 1) {
                 dayChat.send({ content: `**${guy.nickname + " " + guy.user.username || guy}** won't be able to vote anymore from tomorrow on.` })
                 db.set(`terror_${message.channel.id}`, { day: dayCount + 1, guy: guy.nickname })
                 message.channel.send({ content: "The villagers are informed!" })
