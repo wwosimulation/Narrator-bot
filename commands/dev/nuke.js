1124af74b893c76b8901548f4c2bc8811793d1d9
Hackante <Hackante@users.noreply.github.com>
2021-10-09 10:14:49 +0000

    Prettified Code!

--- commands/dev/nuke.js
+++ commands/dev/nuke.js
@@ -7,12 +7,14 @@ module.exports = {
     run: (message, args, client) => {
         try {
             if (!message.member.roles.cache.has("859099415515627540") && !message.member.roles.cache.has("606123616228343812")) return message.reply({ content: "You are missing permissions to do that!" })
-	    client.guilds.cache.filter((guild) => guild.id === ids.server.sim || guild.id === ids.server.game).each((server) => {
-		server.commands.set([])
-	    })
+            client.guilds.cache
+                .filter((guild) => guild.id === ids.server.sim || guild.id === ids.server.game)
+                .each((server) => {
+                    server.commands.set([])
+                })
             message.channel.send({ content: `The servers have the following count of slash commands:\nSim:${client.guilds.resolve(ids.server.sim).commands.cache.size}\nGame:${client.guilds.resolve(ids.server.game).commands.cache.size}` })
         } catch (err) {
-	    message.channel.send("An error occurred!")
+            message.channel.send("An error occurred!")
             console.error
         }
     },
