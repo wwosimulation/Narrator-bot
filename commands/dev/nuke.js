84f1fd47430bbde97652624eb100121c3ffd54a4
Hackante <kante.hagen@gmail.com>
2021-10-15 23:05:57 +0200

    rebase (6)

--- commands/dev/nuke.js
+++ commands/dev/nuke.js
@@ -6,66 +6,61 @@ module.exports = {
     description: "Deletes all or specific slash commands of the bot you are using.",
     usage: `${process.env.PREFIX}nuke [(here) <commands...>]`,
     run: (message, args, client) => {
-        try {
-            if (!message.member.roles.cache.has("859099415515627540") && !message.member.roles.cache.has("606123616228343812")) return message.reply({ content: "You are missing permissions to do that!" })
-            let s = ["sim", "game"]
-            let answer = "**Following actions were executed:**\n"
-            let skiped = []
-            if (args) {
-                if (args[0] === "here" && args[1]) {
-                    let cmdManager = message.guild.commands
-                    args.slice(1).forEach((arg) => {
-                        // Nuking command IDs and names
-                        if (cmdManager.cache.has(arg))
-                            cmdManager.cache
-                                .get(arg)
+        if (!message.member.roles.cache.has("859099415515627540") && !message.member.roles.cache.has("606123616228343812")) return message.reply({ content: "You are missing permissions to do that!" })
+        let s = ["sim", "game"]
+        let answer = "**Following actions were executed:**\n"
+        let skiped = []
+        if (args) {
+            if (args[0] === "here" && args[1]) {
+                let cmdManager = message.guild.commands
+                args.slice(1).forEach((arg) => {
+                    // Nuking command IDs and names
+                    if (cmdManager.cache.has(arg))
+                        cmdManager.cache
+                            .get(arg)
+                            .delete()
+                            .then((cmd) => (answer += `Delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
+                    else if (cmdManager.cache.find((cmd) => cmd.name === arg).size == 1)
+                        cmdManager.cache
+                            .find((cmd) => cmd.name === arg)
+                            .delete()
+                            .then((cmd) => (answer += `Delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
+                    else skiped.push(arg)
+                })
+            } else {
+                // Seaching in all servers
+                args.forEach((arg) => {
+                    // Nuking one server
+                    if (s.includes(arg)) return client.guilds.resolve(ids[arg]).commands.set([])((answer += `Bulk delete of server: \`${client.guilds.resolve(ids[arg]).name}\`\n`))
+                    // Nuking command IDs and names
+                    s.forEach((ser) => {
+                        if (client.guilds.resolve(ids[ser]).commands.cache.has(arg))
+                            client.guilds
+                                .resolve(ids[ser])
+                                .commands.cache.get(arg)
                                 .delete()
-                                .then((cmd) => (answer += `Delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
-                        else if (cmdManager.cache.find((cmd) => cmd.name === arg).size == 1)
-                            cmdManager.cache
-                                .find((cmd) => cmd.name === arg)
+                                .then((cmd) => (answer += `ID delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
+                        else if (client.guilds.resolve(ids[ser]).commands.cache.find((cmd) => cmd.name === arg).size == 1)
+                            client.guilds
+                                .resolve(ids[ser])
+                                .commands.cache.find((cmd) => cmd.name === arg)
                                 .delete()
-                                .then((cmd) => (answer += `Delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
+                                .then((cmd) => (answer += `Name delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
                         else skiped.push(arg)
                     })
-                } else {
-                    // Seaching in all servers
-                    args.forEach((arg) => {
-                        // Nuking one server
-                        if (s.includes(arg)) return client.guilds.resolve(ids[arg]).commands.set([])((answer += `Bulk delete of server: \`${client.guilds.resolve(ids[arg]).name}\`\n`))
-                        // Nuking command IDs and names
-                        s.forEach((ser) => {
-                            if (client.guilds.resolve(ids[ser]).commands.cache.has(arg))
-                                client.guilds
-                                    .resolve(ids[ser])
-                                    .commands.cache.get(arg)
-                                    .delete()
-                                    .then((cmd) => (answer += `ID delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
-                            else if (client.guilds.resolve(ids[ser]).commands.cache.find((cmd) => cmd.name === arg).size == 1)
-                                client.guilds
-                                    .resolve(ids[ser])
-                                    .commands.cache.find((cmd) => cmd.name === arg)
-                                    .delete()
-                                    .then((cmd) => (answer += `Name delete \`${cmd.name}\` (\`${cmd.id}\`, \`${cmd.guild.name}\`)\n`))
-                            else skiped.push(arg)
-                        })
-                    })
-                }
-                if (skiped.length !== 0) answer += `\n**Following arguments couln't be resolved:**\n${skiped.map((element) => `\`${element}\``).join(" ")}\nValid CommandResolvables are \`commandID\`, \`commandName\`, \`sim\` and \`game\`!\nUse \`${process.env.PREFIX}nuke here <commands...>\` to delete commands in the current server only.`
-                return message.channel.send({ embeds: [new MessageEmbed().setDescription(answer).setColor(0x7419b4).setThumbnail(client.user.avatarURL())] })
-            } else {
-                client.guilds.cache
-                    .filter((guild) => guild.id === ids.server.sim || guild.id === ids.server.game)
-                    .each((server) => {
-                        server.commands.set([])
-                        answer += `Bulk delete of server: \`${server.name}\`\n`
-                    })
-                answer += `**The servers have the following count of slash commands:**\nSim: \`${client.guilds.resolve(ids.server.sim).commands.cache.size}\`\n\`Game: ${client.guilds.resolve(ids.server.game).commands.cache.size}\``
-                return message.channel.send({ embeds: [new MessageEmbed().setDescription(answer).setColor(0x7419b4).setThumbnail(client.user.avatarURL())] })
+                })
             }
-        } catch (err) {
-            message.channel.send("An error occurred!")
-            console.error
+            if (skiped.length !== 0) answer += `\n**Following arguments couln't be resolved:**\n${skiped.map((element) => `\`${element}\``).join(" ")}\nValid CommandResolvables are \`commandID\`, \`commandName\`, \`sim\` and \`game\`!\nUse \`${process.env.PREFIX}nuke here <commands...>\` to delete commands in the current server only.`
+            return message.channel.send({ embeds: [new MessageEmbed().setDescription(answer).setColor(0x7419b4).setThumbnail(client.user.avatarURL())] })
+        } else {
+            client.guilds.cache
+                .filter((guild) => guild.id === ids.server.sim || guild.id === ids.server.game)
+                .each((server) => {
+                    server.commands.set([])
+                    answer += `Bulk delete of server: \`${server.name}\`\n`
+                })
+            answer += `**The servers have the following count of slash commands:**\nSim: \`${client.guilds.resolve(ids.server.sim).commands.cache.size}\`\n\`Game: ${client.guilds.resolve(ids.server.game).commands.cache.size}\``
+            return message.channel.send({ embeds: [new MessageEmbed().setDescription(answer).setColor(0x7419b4).setThumbnail(client.user.avatarURL())] })
         }
     },
 }
