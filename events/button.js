4827b464acb139f8239009b5ef35f95830941d1c
Groot <67421407+Gorott@users.noreply.github.com>
2021-10-19 19:40:08 +0200

    update button.js

--- events/button.js
+++ events/button.js
@@ -87,8 +87,8 @@ module.exports = (client) => {
                 case "request":
                     let nextTime = db.get("nextRequest")
                     if (nextTime && nextTime > Date.now()) return interaction.reply({ content: `A game can only be requested once per every 30 minutes! The next game can be requested <t:${Math.round(nextTime / 1000)}:R>`, ephemeral: true })
-                    canHost.customId += `hostrequest-no;${interaction.member.id}`
-                    canNotHost.customId += `hostrequest-yes;${interaction.member.id}`
+                    canHost.customId += ${interaction.member.id}
+                    canNotHost.customId += ${interaction.member.id}
                     const row = new MessageActionRow().addComponents(canHost, canNotHost)
                     client.channels.cache.get("606123759514025985").send({ content: `${interaction.member} is requesting a game! \n\nThe below buttons will send a DM to the requesting user about your choice.`, components: [row] })
                     interaction.reply({ content: "Your request has been sent to the narrators!", ephemeral: true })
