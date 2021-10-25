db877f5dfecea1e45a387136a7b13dd8f1f72265
Groot <67421407+Gorott@users.noreply.github.com>
2021-10-25 18:31:29 +0200

    update day.js

--- commands/narrator/day.js
+++ commands/narrator/day.js
@@ -218,8 +218,8 @@ module.exports = {
                 }
             }
             let players = db.get(`trickortreat_${jack[i]}`)
+            if (players != null) {
             for (let a = 0; a < players.length; a++) {
-                if (players[a] != null) {
                     let guy = message.guild.members.cache.find((m) => m.nickname === players[a])
                     let allChannels = message.guild.channels.cache.filter((c) => c.name === `priv-${db.get(`role_${guy.id}`).toLowerCase().replace(" ", "-")}`).map((x) => x.id)
                     for (let b = 0; b < allChannels.length; b++) {
@@ -323,7 +323,8 @@ module.exports = {
                             }
                         }
                     }
-                }
+                
+            }
             }
         }
         // removing all kills if peace is activated
