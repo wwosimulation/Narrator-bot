5d43f62171133ed2e862c6efb5ab69fb2ef2659d
Finny <59434139+FinnyMarigold58@users.noreply.github.com>
2021-10-28 15:33:31 -0500

    Add content

--- events/button.js
+++ events/button.js
@@ -109,13 +109,11 @@ module.exports = (client) => {
             switch (action) {
                 case "no":
                     foundUser.send({ content: `Hey there, we received your request for a game! Unfortunately, no one is able to host a game right now.` })
-                    interaction.reply(`No one can host, so the user has been informed. Thank you ${interaction.member}`)
-                    interaction.deferUpdate()
+                    interaction.reply({ content: `No one can host, so the user has been informed. Thank you ${interaction.member}`})
                     break
                 case "yes":
                     foundUser.send({ content: `Hey there, we received your request for a game, so ${interaction.member} is starting one soon!.` })
-                    interaction.reply(`${interaction.member} is now hosting a game! The user has been informed.`)
-                    interaction.deferUpdate()
+                    interaction.reply({ content: `${interaction.member} is now hosting a game! The user has been informed.`})
                     break
                 default:
                     break
