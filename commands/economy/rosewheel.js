d4c466fb184d30daa7d4d46dce01098f87461e23
Hackante <Hackante@users.noreply.github.com>
2021-11-02 22:05:25 +0000

    Prettified Code!

--- commands/economy/rosewheel.js
+++ commands/economy/rosewheel.js
@@ -21,11 +21,11 @@ module.exports = {
         let prize = rosewheel[Math.floor(Math.random() * rosewheel.length)]
         let res = ["1", "2", "3", "4"]
 
-        let update = {"roses": - 30}
+        let update = { roses: -30 }
         let response = message.l10n("rosewheel" + res[Math.floor(Math.random() * res.length)], { prize: prize.name, prizeAmount: prize.amount, prizeName: prize.name.split(" ").slice(-1)[0] })
 
         update[prize.item] = prize.amount
-        await guy.updateOne({ $inc: update})
+        await guy.updateOne({ $inc: update })
         message.reply("The wheel is spinning...").then((msg) => {
             setTimeout(() => {
                 msg.edit(response)
