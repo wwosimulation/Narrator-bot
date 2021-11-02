1cb3028db724ca965eade617156c3d26f07087f0
Hackante <Hackante@users.noreply.github.com>
2021-11-02 20:51:04 +0000

    Prettified Code!

--- commands/economy/rosewheel.js
+++ commands/economy/rosewheel.js
@@ -8,16 +8,11 @@ module.exports = {
     usage: `${process.env.PREFIX}rosewheel`,
     aliases: ["rw", "spin"],
     run: async (message, args, client) => {
-        let guy = await players.findOne({user: message.author.id})
-        if(!guy || !guy.roses >= 30) return message.channel.send(message.l10n("notEnoughCurrency", {currency: "roses"}))
+        let guy = await players.findOne({ user: message.author.id })
+        if (!guy || !guy.roses >= 30) return message.channel.send(message.l10n("notEnoughCurrency", { currency: "roses" }))
 
         let prize = rosewheel[Math.floor(Math.random() * rosewheel.length)]
-        let res = [
-            "Congratulations! You won {prize}!",
-            "{prizeAmount} wild {prizeName} appeared! You successfully cought the {prizeName}.",
-            "You found {prize}!",
-            "The rosewheel reveals {prize}!"
-        ] // Will be replaced with l10n strings later
+        let res = ["Congratulations! You won {prize}!", "{prizeAmount} wild {prizeName} appeared! You successfully cought the {prizeName}.", "You found {prize}!", "The rosewheel reveals {prize}!"] // Will be replaced with l10n strings later
         let msg = message.channel.send("The wheel is spinning...")
         await sleep(2500)
         let update = {}
@@ -26,20 +21,20 @@ module.exports = {
             .replace(/{prizeAmount}/g, prize.amount)
             .replace(/{prizeName}/g, prize.name.split(" ").slice(-1)[0])
 
-        switch(prize.item){
-            case("coin"):{
+        switch (prize.item) {
+            case "coin": {
                 update["coins"] = prize.amount
                 break
             }
-            case("lootbox"):{
+            case "lootbox": {
                 update["inventory.lootbox"] = prize.amount
                 break
             }
-            case("rose"):{
+            case "rose": {
                 update["inventory.rose"] = prize.amount
                 break
             }
-            case("other"):{
+            case "other": {
                 update["coins"] = prize.amount
                 break
             }
@@ -48,8 +43,7 @@ module.exports = {
                 break
             }
         }
-        await guy.updateOne({$inc:update})
+        await guy.updateOne({ $inc: update })
         msg.edit(response)
-        
-    }
+    },
 }
