c4d1ad279283c394f8da3d48137ffb47b4a384db
Shadow <thebotoftheshadow@gmail.com>
2021-11-06 16:56:03 -0500

    Adjust bye command
    Fixes https://github.com/wwosimulation/tracker/issues/354

--- commands/narrator/bye.js
+++ commands/narrator/bye.js
@@ -11,17 +11,15 @@ module.exports = {
     run: async (message, args, client) => {
         await kickPlayers(message)
         await kickSpectators(message)
-        let m = await message.channel.send("Players have been kicked, I am now clearing channels. (This may take a while)")
+        let m = await message.channel.send("Players have been kicked, I am now ending the game and deleting the role .")
         await sleep(3000)
-        await clearMainChannels(message)
-        await sleep(1000)
-        message.guild.channels.cache.filter((c) => c.parentID === "892046231516368906").forEach((x) => x.delete())
-        await sleep(1000)
-        await clearSettings(message)
-        await sleep(1000)
+        //await clearMainChannels(message)
+        //await sleep(1000)
+        //await clearSettings(message)
+        //await sleep(1000)
         await clearJoin(client)
         await endGame(client)
-        m.edit("Clearing is complete!").catch(() => {})
+        m.edit("Game end complete!").catch(() => {})
     },
 }
 
@@ -57,34 +55,34 @@ const kickSpectators = async (message) => {
     })
 }
 
-const clearMainChannels = async (message) => {
-    let chans = ["vote-chat", "music-commands", "shadow-votes", "jailed-chat", "werewolves-chat", "time", "dead-chat", "day-chat", "enter-game", "game-lobby"]
-    let ingame = message.guild.channels.cache.filter((c) => chans.includes(c.name))
-    asyncForEach(ingame, async (e, a, b) => {
-        let ashish = await e.messages.fetch()
-        let filt = ashish.filter((c) => !c.pinned)
-        if (filt.size < 50) {
-            await e.bulkDelete(filt)
-            console.log(`Cleared #${e.name}`)
-        } else {
-            for (let i = 0; i < filt.size; i + 50) await e.bulkDelete(50)
-            await e.bulkDelete(filt)
-            console.log(`Bulk cleared #${e.name}`)
-        }
-    })
-}
+// const clearMainChannels = async (message) => {
+//     let chans = ["vote-chat", "music-commands", "shadow-votes", "jailed-chat", "werewolves-chat", "time", "dead-chat", "day-chat", "enter-game", "game-lobby"]
+//     let ingame = message.guild.channels.cache.filter((c) => chans.includes(c.name))
+//     asyncForEach(ingame, async (e, a, b) => {
+//         let ashish = await e.messages.fetch()
+//         let filt = ashish.filter((c) => !c.pinned)
+//         if (filt.size < 50) {
+//             await e.bulkDelete(filt)
+//             console.log(`Cleared #${e.name}`)
+//         } else {
+//             for (let i = 0; i < filt.size; i + 50) await e.bulkDelete(50)
+//             await e.bulkDelete(filt)
+//             console.log(`Bulk cleared #${e.name}`)
+//         }
+//     })
+// }
 
-const clearSettings = async (message) => {
-    let settings = message.guild.channels.cache.filter((c) => c.parentID === "892046231516368906")
-    asyncForEach(settings, async (e) => {
-        let oki = await e.messages.fetch()
-        let hmm = oki.filter((m) => !m.pinned && Date.now() - m.createdTimestamp < 60 * 60 * 24 * 14)
-        if (hmm.size > 0) {
-            e.bulkDelete(hmm)
-            console.log(`Cleared #${e.name}`)
-        }
-    })
-}
+// const clearSettings = async (message) => {
+//     let settings = message.guild.channels.cache.filter((c) => c.parentID === "892046231516368906")
+//     asyncForEach(settings, async (e) => {
+//         let oki = await e.messages.fetch()
+//         let hmm = oki.filter((m) => !m.pinned && Date.now() - m.createdTimestamp < 60 * 60 * 24 * 14)
+//         if (hmm.size > 0) {
+//             e.bulkDelete(hmm)
+//             console.log(`Cleared #${e.name}`)
+//         }
+//     })
+// }
 
 const clearJoin = async (client) => {
     let t = client.guilds.cache.get(ids.server.sim).roles.cache.get("606123676668133428").members
