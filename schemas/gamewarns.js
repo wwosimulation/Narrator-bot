b3cb63023e00ae5d47f1f381de3edb6bf3ae48e6
Hackante <kante.hagen@gmail.com>
2022-01-10 21:47:03 +0100

    basic schema

--- schemas/gamewarns.js
+++ schemas/gamewarns.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
+    index: {type: Number, default: 0},
     user: {type: String, required: true},
-    reason: {type: String, }
+    reason: {type: String, default: "*No reason given*"},
+    gamecode: {type: String, default: "*No game code given*"},
+    date: {type: Date, default: () => {return Date.now()}},
 })
+
+module.exports = mongoose.model("gamewarns", schema)
