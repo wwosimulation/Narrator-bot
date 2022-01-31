4c567567a15e3c98ac41ca250b326a5716747379
Hackante <Hackante@users.noreply.github.com>
2022-01-31 18:59:28 +0000

    Prettified Code!

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -71,7 +71,7 @@ module.exports = {
             }
             let dbName
             let i = ["desciption", "icon", "special", "channel"].indexOf(item.id)
-            if(i == -1 && !obj[`inventory.${item.id}`] && !chart.id) dbName = item.id
+            if (i == -1 && !obj[`inventory.${item.id}`] && !chart.id) dbName = item.id
             else dbName = ["profileDesc", "profileIcon", "customRole", "privateChannel"][i]
             if (dbName && guy[dbName] !== false && guy[dbName] !== "") return l10nMesssage({ code: "alreadyPurchasedItem", toReplace: { item: item.name } })
 
