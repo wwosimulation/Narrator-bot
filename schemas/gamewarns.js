cdc0a21253bf951e7665f3b627718ad726e7beb3
Hackante <kante.hagen@gmail.com>
2022-02-05 18:05:32 +0100

    Schema stupid

--- schemas/gamewarns.js
+++ schemas/gamewarns.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-    index: { type: Number, default: 0 },
+    index: { type: Number, index: true, unique: true, default: async () => {
+        await mongoose.model("gamewarns", schema).find({}).transform((doc) => doc.length)
+    }},
     user: { type: String, required: true },
     reason: { type: String, default: "*No reason given*" },
     gamecode: { type: String, default: "*No game code given*" },
