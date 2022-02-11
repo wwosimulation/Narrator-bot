7b042cf593992ae285a3cd48c8f72fab9a39e3b9
Hackante <Hackante@users.noreply.github.com>
2022-02-11 21:20:23 +0000

    Prettified Code!

--- slashCommands/botSlash/bugSlash.js
+++ slashCommands/botSlash/bugSlash.js
@@ -11,29 +11,32 @@ module.exports = {
                 name: "title",
                 description: "Select a short fitting title for this bug",
                 required: true,
-            }, {
+            },
+            {
                 type: "STRING",
                 name: "description",
                 description: "Describe the bug more detailed and how you can reproduce it",
                 required: true,
-            }, {
+            },
+            {
                 type: "STRING",
                 name: "part",
                 description: "What part of the bot are you seeing the problem on?",
                 required: true,
                 choices: [
-                    {name: "Game - Narrator", value: "Game - Narrator"},
-                    {name: "Game - Player", value: "Game - Player"},
-                    {name: "Economy", value: "Economy"},
-                    {name: "Other", value: "Other"}
+                    { name: "Game - Narrator", value: "Game - Narrator" },
+                    { name: "Game - Player", value: "Game - Player" },
+                    { name: "Economy", value: "Economy" },
+                    { name: "Other", value: "Other" },
                 ],
-            }, {
+            },
+            {
                 type: "STRING",
                 name: "gamecode",
                 description: "If this is a bug in a game, what was the game code?",
                 required: false,
-            }
-        ]
+            },
+        ],
     },
     permissions: {
         sim: [{ id: "606138123260264488", type: "ROLE", permission: true }],
@@ -41,8 +44,7 @@ module.exports = {
     },
     server: ["sim", "game"],
     run: async (interaction, client) => {
-        let body = 
-`### What happened?
+        let body = `### What happened?
 
 ${interaction.options.getString("description")}
 
@@ -50,12 +52,11 @@ ${interaction.options.getString("description")}
 
 ${interaction.options.getString("part") || "Other"}`
 
-        if(interaction.options.getString("part").startsWith("Game") && interaction.options.getString("gamecode")) body += `\n### Game Code\n\n${interaction.options.getString("gamecode")}`
+        if (interaction.options.getString("part").startsWith("Game") && interaction.options.getString("gamecode")) body += `\n### Game Code\n\n${interaction.options.getString("gamecode")}`
         body += `\n<hr>\nThe above bug was reported by ${interaction.user.tag}\nUser ID: ${interaction.user.id}\Location: ${(interaction.guildId == ids.server.game ? "" : `[#${interaction.channel.name}](https://discord.com/channels/${interaction.guildId + "/" + interaction.channelId}) (${interaction.channel.id}) in `) + `[${interaction.guild.name}](https://discord.com/channels/${interaction.guildId})`}`
 
         let labels = ["Bug", "Unverified"]
-        if(interaction.options.getString("part") == "Economy") labels.push("Economy")
-
+        if (interaction.options.getString("part") == "Economy") labels.push("Economy")
 
         let issue = {
             title: `BUG: ${interaction.options.getString("title") || "N/A"}`,
@@ -66,6 +67,6 @@ ${interaction.options.getString("part") || "Other"}`
         }
 
         let done = await client.github.request(`POST /repos/${github.org}/${github.repo}/issues`, issue)
-        interaction.reply({content: interaction.l10n("bugSuccess", { url: done.data.html_url }), ephemeral: true})
+        interaction.reply({ content: interaction.l10n("bugSuccess", { url: done.data.html_url }), ephemeral: true })
     },
 }
--- slashCommands/botSlash/suggestSlash.js
+++ slashCommands/botSlash/suggestSlash.js
@@ -11,36 +11,39 @@ module.exports = {
                 name: "title",
                 description: "Select a short fitting title for this suggestion",
                 required: true,
-            }, {
+            },
+            {
                 type: "STRING",
                 name: "description",
                 description: "Describe the suggestion more detailed. Include everything you want us to know",
                 required: true,
-            }, {
+            },
+            {
                 type: "STRING",
                 name: "part",
                 description: "For which part of the bot is this suggestion intended",
                 required: true,
                 choices: [
-                    {name: "Game - Narrator", value: "Game - Narrator"},
-                    {name: "Game - Player", value: "Game - Player"},
-                    {name: "Economy", value: "Economy"},
-                    {name: "Other", value: "Other"}
+                    { name: "Game - Narrator", value: "Game - Narrator" },
+                    { name: "Game - Player", value: "Game - Player" },
+                    { name: "Economy", value: "Economy" },
+                    { name: "Other", value: "Other" },
                 ],
-            }, {
+            },
+            {
                 type: "STRING",
                 name: "origin",
                 description: "Where is this suggestion from?",
                 required: true,
                 choices: [
-                    {name: "Staff Vote", value: "Staff Vote"},
-                    {name: "Community Vote", value: "Community Vote"},
-                    {name: "Dev Assistant", value: "Dev Assistant"},
-                    {name: "Member", value: "Member"},
-                    {name: "Other", value: "Other"},
-                ]
-            }
-        ]
+                    { name: "Staff Vote", value: "Staff Vote" },
+                    { name: "Community Vote", value: "Community Vote" },
+                    { name: "Dev Assistant", value: "Dev Assistant" },
+                    { name: "Member", value: "Member" },
+                    { name: "Other", value: "Other" },
+                ],
+            },
+        ],
     },
     permissions: {
         sim: [{ id: "606138123260264488", type: "ROLE", permission: true }],
@@ -48,8 +51,7 @@ module.exports = {
     },
     server: ["sim", "game"],
     run: async (interaction, client) => {
-        let body = 
-`### What is your suggestion?
+        let body = `### What is your suggestion?
 
 ${interaction.options.getString("description")}
 
@@ -64,8 +66,7 @@ ${interaction.options.getString("origin") ?? "Other"}`
         body += `\n<hr>\nThe above suggestion was sent by ${interaction.user.tag}\nUser ID: ${interaction.user.id}\Location: ${(interaction.guildId == ids.server.game ? "" : `[#${interaction.channel.name}](https://discord.com/channels/${interaction.guildId + "/" + interaction.channelId}) (${interaction.channel.id}) in `) + `[${interaction.guild.name}](https://discord.com/channels/${interaction.guildId})`}`
 
         let labels = ["Suggestion"]
-        if(interaction.options.getString("part") == "Economy") labels.push("Economy")
-
+        if (interaction.options.getString("part") == "Economy") labels.push("Economy")
 
         let issue = {
             title: `SUGGESTION: ${interaction.options.getString("title") ?? "N/A"}`,
@@ -76,6 +77,6 @@ ${interaction.options.getString("origin") ?? "Other"}`
         }
 
         let done = await client.github.request(`POST /repos/${github.org}/${github.repo}/issues`, issue)
-        interaction.reply({content: interaction.l10n("suggestSuccess", { url: done.data.html_url }), ephemeral: true})
+        interaction.reply({ content: interaction.l10n("suggestSuccess", { url: done.data.html_url }), ephemeral: true })
     },
 }
