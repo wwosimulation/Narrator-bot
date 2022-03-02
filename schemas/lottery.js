d9d67b5ee25ac30330a67cd6512143da66e25432
Gorott <Gorott@users.noreply.github.com>
2022-03-02 13:02:49 +0000

    Prettified Code!

--- schemas/lottery.js
+++ schemas/lottery.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
     msg: { type: String, default: "" },
     pot: { type: Number, default: 0 },
     ticketsBought: { type: Number, default: 0 },
-    endDate: { type: String, default: ""}
+    endDate: { type: String, default: "" },
 })
 
 module.exports = mongoose.model("lottery", schema)
