a5a62a2ddb4827a16f87e0c8489a00d5b820c98b
Gorott <Gorott@users.noreply.github.com>
2022-03-02 10:14:28 +0000

    Prettified Code!

--- schemas/lottery.js
+++ schemas/lottery.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  participants: { type: Array, default: [] },
-  pot: { type: Number, default: 0 },
-  ticketsBought: { type: Number, default: 0 },
-  endDate: { type: String }
-});
+    id: { type: String, required: true, unique: true },
+    participants: { type: Array, default: [] },
+    pot: { type: Number, default: 0 },
+    ticketsBought: { type: Number, default: 0 },
+    endDate: { type: String },
+})
 
-module.exports = mongoose.model("lottery", schema);
+module.exports = mongoose.model("lottery", schema)
