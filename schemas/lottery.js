b2490dea15acb6f2256e7734710fc46f68e296c9
Gorott <thegorott@gmail.com>
2022-03-02 12:57:18 +0100

    rebase

--- schemas/lottery.js
+++ schemas/lottery.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
     participants: { type: Array, default: [] },
     pot: { type: Number, default: 0 },
     ticketsBought: { type: Number, default: 0 },
