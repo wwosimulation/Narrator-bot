4ec7d8573013b393b1bcd53e2d9844fe4d490205
Gorott <thegorott@gmail.com>
2022-03-02 14:02:15 +0100

    groot the pro

--- schemas/lottery.js
+++ schemas/lottery.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
     participants: { type: Array, default: [] },
-    msg: { type: String, default: "" }
+    msg: { type: String, default: "" },
     pot: { type: Number, default: 0 },
     ticketsBought: { type: Number, default: 0 },
-    endDate: { type: String, default: ""},
+    endDate: { type: String, default: ""}
 })
 
 module.exports = mongoose.model("lottery", schema)
