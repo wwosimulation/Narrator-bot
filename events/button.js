39a8d9ccb2ffd898667a47c6eaf6ee2e99b10064
Gorott <thegorott@gmail.com>
2022-03-03 10:06:17 +0100

    testes

--- events/button.js
+++ events/button.js
@@ -192,15 +192,15 @@ module.exports = (client) => {
             if (tickets > lot.max) {
                 interaction.message.embeds[0].fields[0].value = `\u200b`
                 interaction.update({ embeds: [interaction.message.embeds[0]] })
-                return await interaction.message.channel.send({ content: `You can only buy ${lotsLeft} more tickets!`, ephemeral: true })
+                return interaction.followUp({ content: `You can only buy ${lotsLeft} more tickets!`, ephemeral: true })
             } else {
                 let cost = lot.cost * tickets
                 if (cost > player.coins) {
                     interaction.message.embeds[0].fields[0].value = `\u200b`
                     interaction.update({ embeds: [interaction.message.embeds[0]] })
-                    return interaction.message.channel.send({ content: `You don't have enough coins for that amount of lottery tickets!`, ephemeral: true })
+                    return interaction.followUp({ content: `You don't have enough coins for that amount of lottery tickets!`, ephemeral: true })
                 }
-                interaction.message.channel.send({ content: `You have bought ${tickets} tickets!`, ephemeral: true })
+                interaction.followUp({content: `You have bought ${tickets} tickets!`, ephemeral: true })
             }
         }
     })
