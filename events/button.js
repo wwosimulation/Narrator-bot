025633448015fa0882e6bf7f2127ed5dcfcc9640
Gorott <thegorott@gmail.com>
2022-03-03 10:43:31 +0100

    testes

--- events/button.js
+++ events/button.js
@@ -193,7 +193,7 @@ module.exports = (client) => {
                 console.log(interaction)
                 interaction.message.embeds[0].fields[0].value = `\u200b`
                 interaction.update({ embeds: [interaction.message.embeds[0]] })
-                interaction.reply.defer()
+                interaction.reply.deferUpdate()
                 interaction.reply({ content: `You can only buy ${lotsLeft} more tickets!`, ephemeral: true })
                 return
             } else {
