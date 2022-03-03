0732d3da20d93221f974a360394b3bff5d4ef7ae
Gorott <thegorott@gmail.com>
2022-03-03 10:51:34 +0100

    testes

--- events/button.js
+++ events/button.js
@@ -192,8 +192,7 @@ module.exports = (client) => {
             if (tickets > lot.max) {
                 console.log(interaction)
                 interaction.reply({ content: `You can only buy ${lotsLeft} more tickets!`, ephemeral: true })
-                interaction.message.embeds[0].fields[0].value = `\u200b`
-                interaction.update({ embeds: [interaction.message.embeds[0]] })
+                interaction.message.delete({ timeout: 1000})
                 return
             } else {
                 let cost = lot.cost * tickets
