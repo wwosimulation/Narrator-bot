8a4faa1e5903e59ed0855ec169f65a79e6b88959
Hackante <kante.hagen@gmail.com>
2022-03-04 17:11:02 +0100

    lottery schema

---
+++ schemas/lottery.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  participants: { type: Array, default: [] },
+  pot: { type: Number, default: 0 },
+  ticketsBought: { type: Number, default: 0 },
+  endDate: { type: String }
+});
+
+module.exports = mongoose.model("lottery", schema);
