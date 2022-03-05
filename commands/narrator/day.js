ef1953469d0896ab493d7caa8ac9dca1509d8f25
Hackante <kante.hagen@gmail.com>
2022-03-05 16:10:16 +0100

    emoji added

--- commands/narrator/day.js
+++ commands/narrator/day.js
@@ -3511,7 +3511,7 @@ module.exports = {
                 let guy = message.guild.members.cache.find( m => m.displayName == f)
                 if (db.get(`bread_${tempchan.id}`) == f && guy) {
                     message.guild.channels.cache.find((x) => x.name == "private channels").children.each((chan) => {
-                        if(chan.permissionsFor(guy).has("VIEW_CHANNEL")) chan.send("You received a bread from the baker. Your vote will count twice today.")
+                        if(chan.permissionsFor(guy).has("VIEW_CHANNEL")) chan.send(`${getEmoji("baker_bread", client)} You received a bread from the baker. Your vote will count twice today.`)
                     })
                 }
             }
