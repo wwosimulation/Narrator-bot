5aaf9a3b1facc685c3c6e546a38185467c60f787
Hackante <85803963+Hackante@users.noreply.github.com>
2022-03-06 15:42:20 +0100

    Update hypnotize.js

--- commands/game/hypnotize.js
+++ commands/game/hypnotize.js
@@ -3,6 +3,7 @@ const db = require("quick.db")
 module.exports = {
     name: "hypnotize",
     description: "The ability to control someone at night.",
+    usage: `${process.env.PREFIX}hypnotise (cancel | <player>)`,
     gameOnly: true,
     run: async (message, args, client) => {
         if (message.channel.name != "priv-dreamcatcher") return
