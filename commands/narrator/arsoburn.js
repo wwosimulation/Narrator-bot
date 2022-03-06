8649e7fbd64c802a9983c6b69d58e23b85474b39
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-06 23:26:21 +0800

    Fixed arsoburn
    - removed Junior Werewolf exception
    - code changes from v11 to v13

--- commands/narrator/arsoburn.js
+++ commands/narrator/arsoburn.js
@@ -9,24 +9,14 @@ module.exports = {
     narratorOnly: true,
     run: async (message, args, client) => {
         for (let i = 0; i < args.length; i++) {
-            let day = message.guild.channels.find((c) => c.name === "day-chat")
-            let alive = message.guild.roles.find((r) => r.name === "Alive")
-            let dead = message.guild.roles.find((r) => r.name === "Dead")
-            let guy = message.guild.members.find((m) => m.nickname === args[i])
+            let day = message.guild.channels.cache.find((c) => c.name === "day-chat")
+            let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
+            let dead = message.guild.roles.cache.find((r) => r.name === "Dead")
+            let guy = message.guild.members.cache.find((m) => m.nickname === args[i])
             let role = await db.fetch(`role_${guy.id}`)
             day.send(`The Arsonist ignites **${args[i]} ${guy.user.username} (${role})**!`)
-            guy.removeRole(alive.id)
-            guy.addRole(dead.id)
-            if (role == "Junior Werewolf") {
-                let tag = await db.fetch(`jtag_${guy.id}`)
-                if (tag.roles.has(alive.id) || tag != null) {
-                    let byebye = message.guild.members.find((m) => m.nickname === tag)
-                    let gg = await db.fetch(`role_${byebye.id}`)
-                    day.send(`The Junior Werewolf's death has been avenged! **${tag} ${byebye.username} (${gg})** has died!`)
-                    byebye.addRole(dead.id)
-                    byebye.removeRole(alive.id)
-                }
-            }
+            guy.roles.remove(alive.id)
+            guy.roles.add(dead.id)
         }
     },
 }
