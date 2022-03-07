58a8c2e0b4284a3a219708d6d3d3f9a2467e7f04
Gorott <Gorott@users.noreply.github.com>
2022-03-07 20:17:10 +0000

    Prettified Code!

--- slashCommands/staffSlash/lotterySlash.js
+++ slashCommands/staffSlash/lotterySlash.js
@@ -65,27 +65,27 @@ module.exports = {
         let sub = interaction.options.getSubcommand()
         switch (sub) {
             case "create": {
- let cost = interaction.options.getString("cost") 
-let duration = interaction.options.getString("duration") 
-let max = interaction.options.getString("max") 
+                let cost = interaction.options.getString("cost")
+                let duration = interaction.options.getString("duration")
+                let max = interaction.options.getString("max")
 
-let lot = lottery.find() 
-if (lot.length == 1) { 
-return interaction.reply({ content: `There already is a lottery going on, please wait for it to end or end it yourself.`, ephemeral: true }) 
-} 
-if (isNaN(cost) || cost <= 0) return interaction.reply({ content: interaction.l10n("amountInvalid", { amount: cost }), ephemeral: true }) 
-if (isNaN(max) || max <= 0) return interaction.reply({ content: interaction.l10n("amountInvalid", { amount: max }), ephemeral: true }) 
+                let lot = lottery.find()
+                if (lot.length == 1) {
+                    return interaction.reply({ content: `There already is a lottery going on, please wait for it to end or end it yourself.`, ephemeral: true })
+                }
+                if (isNaN(cost) || cost <= 0) return interaction.reply({ content: interaction.l10n("amountInvalid", { amount: cost }), ephemeral: true })
+                if (isNaN(max) || max <= 0) return interaction.reply({ content: interaction.l10n("amountInvalid", { amount: max }), ephemeral: true })
 
-let time = ms(duration) 
-if (!time) return interaction.reply({ content: interaction.l10n("timeInvalidFormat"), ephemeral: true }) 
+                let time = ms(duration)
+                if (!time) return interaction.reply({ content: interaction.l10n("timeInvalidFormat"), ephemeral: true })
 
-let embed = new discord.MessageEmbed().setTitle(`New Lottery!`).setDescription(`Ticket cost: ${cost} ${getEmoji("coin", client)}\nclick 🎟 to enter!\nEnds in: <t:${Math.floor(new Date(Date.now() + time) / 1000)}:R>\n\nParticipants: 0\nTickets bought: 0 \nPot size: 0 ${getEmoji("coin", client)}`) 
-let button = new discord.MessageButton().setStyle("SUCCESS").setEmoji("🎟").setCustomId("joinlottery") 
-let row = new discord.MessageActionRow().addComponents(button) 
+                let embed = new discord.MessageEmbed().setTitle(`New Lottery!`).setDescription(`Ticket cost: ${cost} ${getEmoji("coin", client)}\nclick 🎟 to enter!\nEnds in: <t:${Math.floor(new Date(Date.now() + time) / 1000)}:R>\n\nParticipants: 0\nTickets bought: 0 \nPot size: 0 ${getEmoji("coin", client)}`)
+                let button = new discord.MessageButton().setStyle("SUCCESS").setEmoji("🎟").setCustomId("joinlottery")
+                let row = new discord.MessageActionRow().addComponents(button)
 
-let chan = client.channels.cache.get("947930500725616700") 
-let msg = await chan.send({ embeds: [embed], components: [row] }) 
-lottery.create({ endDate: `${Math.floor(new Date(Date.now() + time))}`, msg: msg.id, max: max, cost: cost })
+                let chan = client.channels.cache.get("947930500725616700")
+                let msg = await chan.send({ embeds: [embed], components: [row] })
+                lottery.create({ endDate: `${Math.floor(new Date(Date.now() + time))}`, msg: msg.id, max: max, cost: cost })
                 break
             }
             case "end": {
