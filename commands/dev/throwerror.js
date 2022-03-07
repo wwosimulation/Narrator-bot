9ffb984dd146b7aace5f53d1564a1453b98b247e
Hackante <kante.hagen@gmail.com>
2022-03-07 18:49:48 +0100

    updated ids

--- commands/dev/throwerror.js
+++ commands/dev/throwerror.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Throw an error to test error handling",
     usage: `${process.env.PREFIX}throwerror`,
     run: async (message, args, client) => {
-        if (!["439223656200273932"].includes(message.author.id)) return
+        if (!["439223656200273932", "801726595378315264", "263472056753061889", "517335997172809728", "552814709963751425"].includes(message.author.id)) return
         throw new Error("Testing, good job")
     },
 }
