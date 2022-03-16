0dc65e4b99b60c32e7af25859a6d409495837b92
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-17 02:13:55 +0800

    Fix Red Lady (#284)
    - fixed a bug where the red lady could not visit at night

--- commands/game/visit.js
+++ commands/game/visit.js
@@ -11,7 +11,7 @@ module.exports = {
             let alive = message.guild.roles.cache.find((r) => r.name === "Alive")
             let dc
             if (db.get(`role_${message.author.id}`) == "Dreamcatcher") dc = fn.dcActions(message, db, alive)
-            let isNight = db.get(`isNight`)
+            let gamePhase = await db.fetch(`gamePhase`)
             if (!message.member.roles.cache.has(alive.id)) return message.channel.send("You cannot use the ability now!")
             if (fn.peaceCheck(message, db) === true && isNight == "yes") return message.channel.send({ content: "We have a peaceful night. You decided to stay at home today." })
             if (!args[0]) return message.channel.send("Who are you visiting? Mention the player.")
@@ -19,7 +19,7 @@ module.exports = {
             let guy = message.guild.members.cache.find((m) => m.nickname === args[0]) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((m) => m.user.username === args.join(" ")) || message.guild.members.cache.find((m) => m.user.tag === args.join(" "))
             if (typeof dc !== "undefined" && guy.nickname == db.get(`hypnotized_${dc.tempchan}`)) return message.channel.send(`This just makes the red lady look weird.`)
 
-            if (isNight != "yes") return message.channel.send("You can use your ability only at night!")
+            if (gamePhase % 3 != 0) return message.channel.send("You can use your ability only at night!")
 
             if (!guy || guy.id == message.author.id) return message.reply("The player is not in game! Mention the correct player number.")
 
