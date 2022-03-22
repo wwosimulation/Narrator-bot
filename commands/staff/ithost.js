bf42a835825b1c5b053dde96e5125aeabecbd0b8
Hackante <kante.hagen@gmail.com>
2022-03-22 22:05:52 +0100

    basic system done

--- commands/staff/ithost.js
+++ commands/staff/ithost.js
@@ -1,13 +1,14 @@
 const db = require("quick.db")
 const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
 const { ids } = require("../../config")
+
 module.exports = {
     name: "ithost",
     description: "Start a interaction test.",
     usage: `${process.env.PREFIX}ithost <test...>`,
     staffOnly: true,
     run: async (message, args, client) => {
-        if (db.get(`game`) != null) return message.channel.send("Another game is being hosted!")
+        if (db.get(`game`) != null) return message.channel.send("Another game/test is being hosted!")
         let button = new MessageButton()
             .setStyle("SECONDARY")
             .setLabel("Join Test")
@@ -18,8 +19,8 @@ module.exports = {
             description: "** **",
             color: 0x327210,
         })
-        let m = await message.guild.channels.cache.get("606123818305585167").send({ content: `<@&955141762010202173>, we are now starting an interaction test ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about the game, go to <#862712560511221791> and click on 🧪.`, components: [row] })
-        db.set(`game`, m.id)
+        let m = await message.guild.channels.cache.find(x => x.id == "606123818305585167").send({ content: `<@&955141762010202173>, we are now starting an interaction test ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about tests, go to <#862712560511221791> and click on 🧪.`, embeds: [embed], components: [row] })
+        db.set("game", m.id)
         db.set("gameCode", args.join(" "))
     },
 }
--- events/button.js
+++ events/button.js
@@ -217,24 +217,24 @@ module.exports = (client) => {
             player.coins -= lot.cost * parseInt(tickets)
             player.save()
             lot.save()
+        }
 
-            if (interaction.customId.startsWith("itest")) {
-                let gameName = interaction.customId.split("-")[1]
-                let guy = interaction.member
-                if (guy.roles.cache.has("606123628693684245")) return interaction.reply({ content: "You are game banned! You cannot join any games", ephemeral: true })
-                if (guy.roles.cache.has("606123676668133428")) return interaction.reply({ content: "You have already joined the game! Check <#606123823074377740> for the link!", ephemeral: true })
-                guy.roles.add("606123676668133428").catch((e) => interaction.guild.channels.cache.get("606123821656702987").send(`Error: ${e.message}`))
-                interaction.guild.channels.cache.find((x) => x.name == "tests").send(`${guy.user.tag} joins match ${gameName}\nUser ID: ${guy.id}`)
-                let jl = await interaction.guild.channels.cache.find((x) => x.name == "tests")
-                jl.send(`<@${guy.id}>, follow the instructions above to join the test!`).then((m) =>
-                    setTimeout(() => {
-                        m.delete()
-                    }, 5000)
-                )
-                let embed = interaction.message.embeds[0]
-                embed.description += `\n${guy.user.tag}`
-                interaction.update({ embeds: [embed] })
-            }
+        if (interaction.customId.startsWith("itest")) {
+            let gameName = interaction.customId.split("-")[1]
+            let guy = interaction.member
+            if (guy.roles.cache.has(ids.gamebanned)) return interaction.reply({ content: "You are game banned! You cannot join any games", ephemeral: true })
+            if (guy.roles.cache.has(ids.joinTest)) return interaction.reply({ content: "You have already joined the test! Check <#955883046710681660> for the instructions!", ephemeral: true })
+            let jl = await interaction.guild.channels.cache.find((x) => x.name == "tests")
+            guy.roles.add(ids.joinTest).catch((e) => jl.send(`Error: ${e.message}`))
+            interaction.guild.channels.cache.find((x) => x.name == "tests").send(`${guy.user.tag} joins test ${gameName}\nUser ID: ${guy.id}`)
+            jl.send(`<@${guy.id}>, follow the instructions above to join the test!`).then((m) =>
+                setTimeout(() => {
+                    m.delete()
+                }, 5000)
+            )
+            let embed = interaction.message.embeds[0]
+            embed.description += `\n${guy.user.tag}`
+            interaction.update({ embeds: [embed] })
         }
     })
 }
