00a170f04c86c73cb498ca47c4ade91aaa255d54
Hackante <kante.hagen@gmail.com>
2022-03-23 18:03:20 +0100

    endtest.js created

---
+++ commands/staff/endtest.js
@@ -0,0 +1,16 @@
+const db = require("quick.db")
+const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
+const { ids } = require("../../config")
+
+module.exports = {
+    name: "endtest",
+    description: "Start a interaction test.",
+    usage: `${process.env.PREFIX}endtest`,
+    staffOnly: true,
+    run: async (message, args, client) => {
+        if (db.get(`game`) == null) return message.channel.send("No test is being hosted.")
+        db.set("game", null)
+        client.guilds.cache.get(ids.server.sim).roles.cache.get(ids.joinTest).members.each(m => m.roles.remove(ids.joinTest, "Test ended!"))
+        message.channel.send("Test ended and roles cleared!")
+    },
+}
