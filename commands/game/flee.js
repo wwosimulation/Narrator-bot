ff978aa1ef1520ad52052302b54a1a8a066d6188
Inno <83353462+inno14@users.noreply.github.com>
2022-03-26 21:19:34 +0530

    Update flee.js

--- commands/game/flee.js
+++ commands/game/flee.js
@@ -14,12 +14,11 @@ module.exports = {
                 let guy = message.guild.members.cache.find((m) => m.nickname === args[0])
                 if (guy) {
                     let role = db.get(`role_${guy.id}`)
-                    db.set(`suicided_${guy.id}`, true)
+                    db.set(`fled_${guy.id}`, true)
                     let day = message.guild.channels.cache.find((c) => c.name === "day-chat")
                     day.send("**" + guy.nickname + " " + guy.user.username + " (" + role + ")** has fled from the village!")
                     guy.roles.add(ids.dead)
                     guy.roles.remove(ids.alive)
-                    message.reply({ content: "Please use `/gamewarn add` to warn the user if necessary." })
                 }
             }
         } else if (message.channel.name.includes("priv") || message.channel.name == "day-chat") {
@@ -28,32 +27,29 @@ module.exports = {
             message.channel.send({ embeds: [new MessageEmbed({ color: "DARK_ORANGE", title: "Are you sure you want to commit suicide?", description: "You will get a game warn if you commit suicide now." })], components: [row] }).then((msg) => {
                 const collector = msg.createMessageComponentCollector({ idle: 15000 })
                 collector.on("collect", async (interaction) => {
-                    if (interaction.user.id !== message.author.id) return interaction.reply({ content: "This is not your suicide message. Don't try to trick me!", ephemeral: true })
-                    if (interaction.customId === "suicide") {
-                        interaction.reply("Suiciding...")
+                    if (interaction.user.id !== message.author.id) return interaction.reply({ content: "This is not your flee message. Don't try to trick me!", ephemeral: true })
+                    if (interaction.customId === "flee") {
+                        interaction.reply("Fleeing...")
                         db.set(`suicided_${message.author.id}`, true)
                         let day = message.guild.channels.cache.find((c) => c.name === "day-chat")
                         let role = await db.fetch(`role_${message.author.id}`)
-                        day.send("**" + message.member.nickname + " " + message.author.username + " (" + role + ")** has commited suicide!")
+                        day.send("**" + message.member.nickname + " " + message.author.username + " (" + role + ")** has fled from village!")
                         message.member.roles.add(ids.dead)
                         message.member.roles.remove(ids.alive)
 
-                        let warn = await gamewarns.create({ user: message.author.id, reason: "Suiciding during a game", gamecode: db.get("gameCode") })
-                        let embed = new MessageEmbed({
-                            title: `You have received a gamewarn! Case: ${warn.index}`,
-                            description: `You have received a gamewarn in ${message.guild.name}!
+                        let warn = await gamewarns.create({user: message.author.id, reason: "Fled from a game", gamecode: db.get("gameCode")})
+                        let embed = new MessageEmbed({title: `You have received a gamewarn! Case: ${warn.index}`, description: 
+`You have received a gamewarn in ${message.guild.name}!
 **Reason:** ${warn.reason}
 **Gamecode:** ${warn.gamecode}
-**Date:** <t:${(Date.now() / 1000).toFixed()}:f>
+**Date:** <t:${(Date.now()/1000).toFixed()}:f>
 
 If you think this gamewarn was given by accident please [open a ticket](https://discord.com/channels/465795320526274561/606230556832825481/905800163069665280) in [#${client.channels.resolve("606230556832825481").name}](https://discord.com/channels/465795320526274561/606230556832825481).
-**Warn ID:** ${warn.index}`,
-                            color: "DARK_RED",
-                        })
-                        try {
-                            await message.author.send({ embeds: [embed] })
-                        } catch (err) {
-                            interaction.editReply({ embeds: [embed] })
+**Warn ID:** ${warn.index}`, color: "DARK_RED"})
+                        try{
+                            await message.author.send({embeds: [embed]})
+                        } catch(err) {
+                            interaction.editReply({embeds: [embed]})
                             message.channel.send("Unable to send direct message.")
                         }
                         client.emit("gamebanned", message.author)
