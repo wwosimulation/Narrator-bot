b7f37031c795f4bc5d2125da6d7daebfa89d435d
Hackante <Hackante@users.noreply.github.com>
2022-03-27 14:05:07 +0000

    Prettified Code!

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ client.on("ready", async () => {
                     msg.edit({ components: [] })
                     let player = await players.findOne({ user: person.id })
                     player.coins += lot.pot
-                    if(!client.guilds.resolve(ids.server.sim).members.fetch(winner.id).roles.cache.has("947629828771831888")) client.guilds.resolve(ids.server.sim).members.fetch(winner.id).roles.add("947629828771831888")
+                    if (!client.guilds.resolve(ids.server.sim).members.fetch(winner.id).roles.cache.has("947629828771831888")) client.guilds.resolve(ids.server.sim).members.fetch(winner.id).roles.add("947629828771831888")
                     let part = []
                     lot.participants.forEach(async (p) => {
                         let arr = Object.entries(p)
