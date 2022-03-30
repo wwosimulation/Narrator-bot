d06dc7fd10252cf27952c3f9b1d4aba4543b5aaa
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-30 10:57:26 +0800

    MessageEmbed class removal
    - Removed the usage of the MessageEmbed class as v14 has renamed it, and it will be more work then, so why not start slowly now.

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -1,4 +1,3 @@
-const { MessageEmbed } = require("discord.js")
 const { fn } = require("../../config")
 
 module.exports = {
@@ -7,7 +6,7 @@ module.exports = {
     usage: `${process.env.PREFIX}help [command | arguments]`,
     run: async (message, args, client) => {
         // help embed
-        let embed = new MessageEmbed({ color: 0x7419b4, thumbnail: { url: client.user.avatarURL() } })
+        let embed = { title: "", description: "", fields: [], footer: {}, color: 0x7419b4, thumbnail: { url: client.user.avatarURL() } }
         let cmd_target
         // Checking if args[0] is a command
         if (args[0]) {
@@ -15,63 +14,52 @@ module.exports = {
         }
         // If args[0] is a command, send a specific command card.
         if (cmd_target) {
-            embed
-                .setTitle(fn.capitalizeFirstLetter(cmd_target.name))
-                .setDescription(
-                    `Prefix: \`${process.env.PREFIX}\`
-Use ${process.env.PREFIX}help to see all commands.
-                `
-                )
-                .addFields({ name: "Description", value: cmd_target.description || "No description given." }, { name: "Usage:", value: `\`${cmd_target.usage || `No usage given or "${process.env.PREFIX + cmd_target.name}"`}\`` })
-                .setFooter({ text: "[] = optional argument\n<> = required argument", iconURL: message.author.avatarURL() })
+            embed.title = fn.capitalizeFirstLetter(cmd_target.name)
+            embed.description = `Prefix: \`${process.env.PREFIX}\`\nUse ${process.env.PREFIX}help to see all commands.\n` +
+            embed.fields.push({ name: "Description", value: cmd_target.description || "No description given." }, { name: "Usage:", value: `\`${cmd_target.usage || `No usage given or "${process.env.PREFIX + cmd_target.name}"`}\`` })
+            embed.footer = { text: "[] = optional argument\n<> = required argument", icon_url: message.author.avatarURL() }
 
             // if the command has aliases add those
             if (cmd_target.aliases) {
-                embed.addFields({ name: "Aliases:", value: `${cmd_target.aliases.length ? cmd_target.aliases.map((alias) => `\`${alias}\``).join(" ") : "No aliases"}` })
+                embed.fields.push({ name: "Aliases:", value: `${cmd_target.aliases.length ? cmd_target.aliases.map((alias) => `\`${alias}\``).join(" ") : "No aliases"}` })
             }
         } else if (["args", "arg", "arguments", "argument"].includes(args[0])) {
-            embed
-                .setTitle(message.l10n("helpHeader"))
-                .setDescription(
-                    `
-**Argument Requirement:**
-\`(sub)command\`
-\`<required>\`
-\`[optional]\`
-\`[option1 | option2]\`, \`<option1 | option2>\`
-\`[<optional> <but_two_required>]\`
-\`<no_limit...>\``
-                )
-                .setTimestamp()
-                .setFooter({ text: `Requested by ${message.author.tag}`, iconURL: message.author.avatarURL() })
-                .addField(
-                    "Arguments",
-                    `
-\`amount\` - Positive number.
-\`description\`- Text that should explain something detailed.
-\`message\` - Some text you want to be sent somewhere else.
-\`player\` - A player in the game. Their number always works.
-\`user\` - UserID, mention, tag, username or nickname. Some commands don't support all options.
-\`command\` - Any command or alias.
-\`color\` - Color from \`${process.env.PREFIX}shop color\`, color or hex code.
-\`emoji\` - Eomoji name from \`${process.env.PREFIX}emojilist\`.
-\`item\` - Item from the shop or your inventory.
-\`quest\` - Quest name.
-\`role\` - Role from the game.
-\`time\` - Duration (\`2 days\`, \`1m\`, etc.)
-\`xp\` - Amount of xp.
-Note: Some arguments were left out as they are obvious to understand like \`nickname\`.`
-                )
+            embed.title = message.l10n("helpHeader")
+            embed.timestamp = Date.now()
+            embed.footer = { text: `Requested by ${message.author.tag}`, iconURL: message.author.avatarURL() }
+            embed.description = `**Argument Requirement:**\n` +
+                `\`(sub)command\`\n` +
+                `\`<required>\`\n` +
+                `\`[optional]\`\n` +
+                `\`[option1 | option2]\`, \`<option1 | option2>\`\n` +
+                `\`[<optional> <but_two_required>]\`\n` +
+                `\`<no_limit...>\``;
+            embed.fields.push({ name: "Arguments",
+                    value: `\`amount\` - Positive number.\n` +
+                        `\`description\`- Text that should explain something detailed.\n` +
+                        `\`message\` - Some text you want to be sent somewhere else.\n` +
+                        `\`player\` - A player in the game. Their number always works.\n` +
+                        `\`user\` - UserID, mention, tag, username or nickname. Some commands don't support all options.\n` +
+                        `\`command\` - Any command or alias.\n` +
+                        `\`color\` - Color from \`${process.env.PREFIX}shop color\`, color or hex code.\n` +
+                        `\`emoji\` - Eomoji name from \`${process.env.PREFIX}emojilist\`.\n` +
+                        `\`item\` - Item from the shop or your inventory.\n` +
+                        `\`quest\` - Quest name.\n` +
+                        `\`role\` - Role from the game.\n` +
+                        `\`time\` - Duration (\`2 days\`, \`1m\`, etc.)\n` +
+                        `\`xp\` - Amount of xp.\n` +
+                        `Note: Some arguments were left out as they are obvious to understand like \`nickname\`.`
+            })
         }
         // if args[0] doesn't exist or it is not a command
         else {
-            embed
-                .setTitle(message.l10n("helpHeader"))
-                .setDescription(message.l10n("helpMain", { gamewarning: "<#606123818305585167>", howtoplay: "<#859001588617445436>", rankedwarn: "<#860552178095882240>", supportandquestions: "<#606123788257591297>", clientUser: `<@${client.user.id}>`, prefix: process.env.PREFIX }))
-                .addField(message.l10n("economy"), message.l10n("helpEconomy"))
-                .addField(message.l10n("fun"), message.l10n("helpFun"))
-                .addField(message.l10n("bot"), message.l10n("helpBot"))
+            embed.title = message.l10n("helpHeader")
+            embed.description = message.l10n("helpMain", { gamewarning: "<#606123818305585167>", howtoplay: "<#859001588617445436>", rankedwarn: "<#860552178095882240>", supportandquestions: "<#606123788257591297>", clientUser: `<@${client.user.id}>`, prefix: process.env.PREFIX })
+            embed.fields.push({ name: message.l10n("economy"), value: message.l10n("helpEconomy") })
+            embed.fields.push({ name: message.l10n("fun"), value: message.l10n("helpFun") })
+            embed.fields.push({ name: message.l10n("bot"), value: message.l10n("helpBot") })
         }
+        
         message.channel.send({ embeds: [embed] })
     },
 }
