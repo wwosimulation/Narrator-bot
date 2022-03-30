a2f85bebcccf45b48b1db7618c95a4b79e7af39a
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-30 05:02:47 +0000

    Prettified Code!

--- commands/dev/gamepanel.js
+++ commands/dev/gamepanel.js
@@ -4,7 +4,7 @@ module.exports = {
     usage: `${process.env.PREFIX}gamepanel`,
     run: async (message, args, client) => {
         if (!client.botAdmin(message.author.id)) return
-        
+
         let button1 = { type: 2, style: 3, label: "Start a Quick Game", custom_id: "gp-start", disabled: true }
         let button2 = { type: 2, style: 1, label: "Request a Full Game", custom_id: "gp-request", disabled: false }
         let button3 = { type: 2, style: 2, label: "See the Last Game's Stats", custom_id: "gp-stats", disabled: false }
