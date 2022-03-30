b0c42000811403fb976eda768532abd6b81a3e84
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-30 13:02:23 +0800

    MessageX class removal
    - Removed the usage of the MessageEmbed, MessageButton and MessageActionRow class as v14 has renamed it, and it will be more work then, so why not start slowly now.

--- commands/dev/gamepanel.js
+++ commands/dev/gamepanel.js
@@ -1,18 +1,16 @@
-const { MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu } = require("discord.js")
-
 module.exports = {
     name: "gamepanel",
     description: "Send the game panel.",
     usage: `${process.env.PREFIX}gamepanel`,
     run: async (message, args, client) => {
         if (!client.botAdmin(message.author.id)) return
-
-        let button1 = new MessageButton().setStyle("SUCCESS").setLabel("Start a Quick Game").setCustomId("gp-start").setDisabled()
-        let button2 = new MessageButton().setStyle("PRIMARY").setLabel("Request a Full Game").setCustomId("gp-request")
-        let button3 = new MessageButton().setStyle("SECONDARY").setLabel("See the Last Game's Stats").setCustomId("gp-stats")
-        let row = new MessageActionRow().addComponents(button1, button2, button3)
+        
+        let button1 = { type: 2, style: 3, label: "Start a Quick Game", custom_id: "gp-start", disabled: true }
+        let button2 = { type: 2, style: 1, label: "Request a Full Game", custom_id: "gp-request", disabled: false }
+        let button3 = { type: 2, style: 2, label: "See the Last Game's Stats", custom_id: "gp-stats", disabled: false }
+        let row = { type: 1, components: [button1, button2, button3] }
         message.channel.send({
-            embeds: [new MessageEmbed({ title: message.guild.name, description: "Welcome to wolvesville Simulation! Select an option below!", color: "PURPLE", thumbnail: { url: message.guild.iconURL({ dynamic: true }) } })],
+            embeds: [{ title: message.guild.name, description: "Welcome to wolvesville Simulation! Select an option below!", color: 0x9b59b6, thumbnail: { url: message.guild.iconURL({ dynamic: true }) } }],
             components: [row],
         })
     },
