7939c9de674dbf36841b8032e6865190379ed5f4
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-30 10:00:18 +0000

    Prettified Code!

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -6,7 +6,7 @@ module.exports = {
     usage: `${process.env.PREFIX}shop [color]`,
     run: async (message, args, client) => {
         if (["color", "colors", "colour", "colours"].includes(args[0])) {
-            let embed = { title: message.l10n("shopTitle"), color: 0x1FFF43, description: `${message.l10n("availableColors")}:\n\n`, footer: { text: message.l10n("shopFooter") } }
+            let embed = { title: message.l10n("shopTitle"), color: 0x1fff43, description: `${message.l10n("availableColors")}:\n\n`, footer: { text: message.l10n("shopFooter") } }
             shop.colors.forEach((x) => {
                 embed.description += `${x.name} Color\n`
             })
@@ -18,7 +18,7 @@ module.exports = {
             }
             let m = await message.reply({ embeds: [shop.embeds[0]], components: [row] })
             setTimeout(() => {
-                row.components.forEach((x) => x.disabled = true)
+                row.components.forEach((x) => (x.disabled = true))
                 m.edit({ components: [row] })
             }, 30000)
         }
