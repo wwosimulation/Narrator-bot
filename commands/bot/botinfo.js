75d1c173e91332438bed26ea668f706da0bdfd1e
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-30 02:46:26 +0000

    Prettified Code!

--- commands/bot/botinfo.js
+++ commands/bot/botinfo.js
@@ -6,24 +6,12 @@ module.exports = {
         let embed = {
             title: client.user.tag,
             fields: [],
-            description: `Narrator Bot is a bot designed specifically for the [Wolvesville Simulation](https://wovsimulation.xyz) server.\n` +
-                `Narrator Bot's main purpose is to automate much of what happens when we run a game of Wolvesville here in Discord. It also runs the server economy and much of the more labor-intensive tasks in the server.\n\n` +
-                `This bot was started in October of 2020 by Ashish who designed much of the core game functionality, and was transfered in March of 2021 to Shadow, who created the economy and completed the bot to release a couple months later.`,
+            description: `Narrator Bot is a bot designed specifically for the [Wolvesville Simulation](https://wovsimulation.xyz) server.\n` + `Narrator Bot's main purpose is to automate much of what happens when we run a game of Wolvesville here in Discord. It also runs the server economy and much of the more labor-intensive tasks in the server.\n\n` + `This bot was started in October of 2020 by Ashish who designed much of the core game functionality, and was transfered in March of 2021 to Shadow, who created the economy and completed the bot to release a couple months later.`,
         }
-        
+
         embed.fields.push({
             name: "**__Credits:__**",
-            value: `Bot Host, Retired Lead Developer: <@439223656200273932>\n` +
-                `Lead Developer: Hackante#1482\n` +
-                `Originally created by: Ashish#0540\n`+
-                `Beta Testers: <@!159717599993790464> <@!719564153072910407> <@!801726595378315264> <@!406412325973786624> <@!620964363729371137> <@!452632560795648000>\n` +
-                `Economy Designers: <@!801726595378315264> <@!406412325973786624>\n\n` +
-                `__Special thanks to the following:__\n`+
-                `<@!719564153072910407> for helping with many testing games, bug finding, organizing suggestions, and testing everything.\n` +
-                `<@!620964363729371137> for organizing basically everything about the bot and GitHub, starting the bug tracker, keeping everything neat.\n` +
-                `<@!263472056753061889> for stepping up everywhere and helping without me asking, both in the bot and on the tracker.\n\n` +
-                `Thank you to all the Staff who come up with new ideas for the bot, and to everyone who has joined a Testes Gae or a Beta Testes game.\n` +
-                `Thank you to all the Translators who provide the ability for us to be multilingual, both in the server and in our games.`
+            value: `Bot Host, Retired Lead Developer: <@439223656200273932>\n` + `Lead Developer: Hackante#1482\n` + `Originally created by: Ashish#0540\n` + `Beta Testers: <@!159717599993790464> <@!719564153072910407> <@!801726595378315264> <@!406412325973786624> <@!620964363729371137> <@!452632560795648000>\n` + `Economy Designers: <@!801726595378315264> <@!406412325973786624>\n\n` + `__Special thanks to the following:__\n` + `<@!719564153072910407> for helping with many testing games, bug finding, organizing suggestions, and testing everything.\n` + `<@!620964363729371137> for organizing basically everything about the bot and GitHub, starting the bug tracker, keeping everything neat.\n` + `<@!263472056753061889> for stepping up everywhere and helping without me asking, both in the bot and on the tracker.\n\n` + `Thank you to all the Staff who come up with new ideas for the bot, and to everyone who has joined a Testes Gae or a Beta Testes game.\n` + `Thank you to all the Translators who provide the ability for us to be multilingual, both in the server and in our games.`,
         })
         message.channel.send({ embeds: [embed] })
     },
