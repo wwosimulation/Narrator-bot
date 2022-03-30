a8f140295c811e12d35d4810879051e4baa4d7bb
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-31 07:55:32 +0800

    MessageX class removal
    - Removed the usage of the MessageEmbed, MessageButton and MessageActionRow class as v14 has renamed it, and it will be more work then, so why not start slowly now

--- commands/narrator/rwhost.js
+++ commands/narrator/rwhost.js
@@ -1,5 +1,4 @@
 const db = require("quick.db")
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js")
 const { ids } = require("../../config")
 
 module.exports = {
@@ -11,12 +10,9 @@ module.exports = {
         if (!message.member.roles.cache.has(narrator.id)) return
         if (db.get(`game`) != null) return message.channel.send("Another game is being hosted!")
         let rs = db.get("rankedseason")
-        let button = new MessageButton()
-            .setStyle("SUCCESS")
-            .setLabel("Join Game")
-            .setCustomId(`gwjoin-rs.${rs}[${args.join(" ")}]`)
-        const row = new MessageActionRow().addComponents(button)
-        const embed = new MessageEmbed({ title: "Player and Spectator List:", description: "** **", color: 0x327210 })
+        let button = { type: 2, style: 3, label: "Join Game", custom_id: `gwjoin-rs.${rs}[${args.join(" ")}]` } 
+        const row = { type: 1, components: [button] }
+        const embed = { title: "Player and Spectator List:", description: "** **", color: 0x327210 }
         let m = await message.guild.channels.cache.get("860552178095882240").send({ content: `<@&606123691889393705>, we are now starting game RS.${rs}[${args.join(" ")}]. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about the game, go to <#862712560511221791> and react with 🏆`, embeds: [embed], components: [row] })
         m.crosspost()
         db.set(`game`, m.id)
