7bf332c90cbc59f340603f353cd3bdc80a6681e7
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-30 23:56:00 +0000

    Prettified Code!

--- commands/narrator/rwhost.js
+++ commands/narrator/rwhost.js
@@ -10,7 +10,7 @@ module.exports = {
         if (!message.member.roles.cache.has(narrator.id)) return
         if (db.get(`game`) != null) return message.channel.send("Another game is being hosted!")
         let rs = db.get("rankedseason")
-        let button = { type: 2, style: 3, label: "Join Game", custom_id: `gwjoin-rs.${rs}[${args.join(" ")}]` } 
+        let button = { type: 2, style: 3, label: "Join Game", custom_id: `gwjoin-rs.${rs}[${args.join(" ")}]` }
         const row = { type: 1, components: [button] }
         const embed = { title: "Player and Spectator List:", description: "** **", color: 0x327210 }
         let m = await message.guild.channels.cache.get("860552178095882240").send({ content: `<@&606123691889393705>, we are now starting game RS.${rs}[${args.join(" ")}]. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about the game, go to <#862712560511221791> and react with 🏆`, embeds: [embed], components: [row] })
