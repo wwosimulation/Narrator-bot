c47e8361b99a65b70c5de89d180853a02deba821
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-03-31 00:07:40 +0000

    Prettified Code!

--- commands/staff/ithost.js
+++ commands/staff/ithost.js
@@ -11,7 +11,7 @@ module.exports = {
         let button = { type: 2, style: 2, label: "Join Test", custom_id: `itest-${args.join(" ")}` }
         const row = { type: 1, components: [button] }
         const embed = { title: "Interaction Test Participants:", description: "** **", color: 0x327210 }
-        let m = await message.guild.channels.cache.find(x => x.id == "606123818305585167").send({ content: `<@&955141762010202173>, we are now starting an interaction test ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about tests, go to <#862712560511221791> and click on 🧪.`, embeds: [embed], components: [row] })
+        let m = await message.guild.channels.cache.find((x) => x.id == "606123818305585167").send({ content: `<@&955141762010202173>, we are now starting an interaction test ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about tests, go to <#862712560511221791> and click on 🧪.`, embeds: [embed], components: [row] })
         db.set("game", m.id)
         db.set("gameCode", args.join(" "))
     },
