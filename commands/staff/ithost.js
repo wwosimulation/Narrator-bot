a2a8906529694d587ab717ada0859f5dce33b303
Shadow <william.shadow@outlook.com>
2022-03-30 21:19:03 -0500

    Rerun prettier manually

--- commands/staff/ithost.js
+++ commands/staff/ithost.js
@@ -19,7 +19,7 @@ module.exports = {
             description: "** **",
             color: 0x327210,
         })
-        let m = await message.guild.channels.cache.find(x => x.id == "606123818305585167").send({ content: `<@&955141762010202173>, we are now starting an interaction test ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about tests, go to <#862712560511221791> and click on 🧪.`, embeds: [embed], components: [row] })
+        let m = await message.guild.channels.cache.find((x) => x.id == "606123818305585167").send({ content: `<@&955141762010202173>, we are now starting an interaction test ${args.join(" ")}. Our host will be <@${message.author.id}>!\nIf you do not wish to get future pings about tests, go to <#862712560511221791> and click on 🧪.`, embeds: [embed], components: [row] })
         db.set("game", m.id)
         db.set("gameCode", args.join(" "))
     },
