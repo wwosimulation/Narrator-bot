fd34db6504671e7997fd6891b979c4baaadef0f6
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-08-10 08:45:33 +0000

    Prettified Code!

--- commands/narrator/day/others/sheriffs.js
+++ commands/narrator/day/others/sheriffs.js
@@ -12,10 +12,10 @@ module.exports = async (client) => {
     // loop through each sheriff
     for (const sher of sheriffs) {
         let sheriff = db.get(`player_${sher}`) // get the sheriff player - Object
-        if (!sheriff.target) continue; // if the sheriff doesn't has a target, don't do anything and check for the next sheriff
+        if (!sheriff.target) continue // if the sheriff doesn't has a target, don't do anything and check for the next sheriff
 
         let guy = db.get(`player_${sheriff.target}`) // get the player who the sheriff had selected to check
-        if (guy.status === "Alive") continue; // if the player is alive, don't do anything and check for the next sheriff
+        if (guy.status === "Alive") continue // if the player is alive, don't do anything and check for the next sheriff
 
         let channel = guild.channels.cache.get(sheriff.channel) // get the channel object - Object
 
