14dc162717ec82706a7334700dc02984a7ece53a
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-08-13 17:57:25 +0800

    Added Evil Detective
    - add evil detective to the command

--- commands/game/check.js
+++ commands/game/check.js
@@ -15,8 +15,8 @@ module.exports = {
         if (!message.channel.name.startsWith("priv")) return // if they are not in the private channel
 
         if (player.status !== "Alive") return await message.channel.send("Listen to me, you need to be ALIVE to check players.")
-        if (!["Seer", "Aura Seer", "Spirit Seer", "Detective", "Wolf Seer", "Sorcerer", "Sheriff"].includes(player.role) && !["Seer", "Aura Seer", "Spirit Seer", "Detective", "Wolf Seer", "Sorcerer", "Sheriff"].includes(player.dreamRole)) return
-        if (["Seer", "Aura Seer", "Spirit Seer", "Detective", "Wolf Seer", "Sorcerer", "Sheriff"].includes(player.dreamRole)) player = db.get(`player_${player.target}`)
+        if (!["Seer", "Aura Seer", "Spirit Seer", "Detective", "Wolf Seer", "Sorcerer", "Sheriff", "Evil Detective"].includes(player.role) && !["Seer", "Aura Seer", "Spirit Seer", "Detective", "Wolf Seer", "Sorcerer", "Sheriff", "Evil Detective"].includes(player.dreamRole)) return
+        if (["Seer", "Aura Seer", "Spirit Seer", "Detective", "Wolf Seer", "Sorcerer", "Sheriff", "Evil Detective"].includes(player.dreamRole)) player = db.get(`player_${player.target}`)
         if (gamePhase % 3 != 0) return await message.channel.send("You do know that you can only check during the night right? Or are you delusional?")
         if (player.jailed) return await message.channel.send("You are jailed. You cannot use your abilities while in jail!")
         if (player.nightmared) return await message.channel.send("You are nightmared. You cannot use your abilities while you're asleep.")
@@ -25,14 +25,14 @@ module.exports = {
         if (!["Spirit Seer", "Detective"].includes(player.role) && args.length !== 1) return await message.channel.send("You need to select 1 player to check!")
         if (player.role === "Wolf Seer" && player.resign) return await message.channel.send("You already resigned from checking!")
         if (player.role === "Spirit Seer" && args.length > 2) return await message.channel.send("You can only select a maximum of 2 players to check!")
-        if (player.role === "Detective" && args.length !== 2) return await message.channel.send("You need to select 2 players to investigate!")
+        if (["Detective", "Evil Detective"].includes(player.role) && args.length !== 2) return await message.channel.send("You need to select 2 players to investigate!")
 
         let target = []
 
         target.push(players[Number(args[0]) - 1] || players.find((p) => p === args[0]) || players.map((p) => db.get(`player_${p}`)).find((p) => p.username === args[0]))
 
         if (args.length > 1) target.push(players[Number(args[1]) - 1] || players.find((p) => p === args[1]) || players.map((p) => db.get(`player_${p}`)).find((p) => p.username === args[1]))
-        if (!player.hypnotized && target.includes(player.id)) return await message.channel.send("You can't check yourself, unless you have trust issues of course.")
+        if (!player.hypnotized && target.includes(player.id) && player.role !== "Evil Detective") return await message.channel.send("You can't check yourself, unless you have trust issues of course.")
         if (!player.hypnotized && player.role === "Wolf Seer" && db.get(`player_${target[0]}`).team === "Werewolf" && db.get(`player_${target[0]}`).role !== "Werewolf Fan") return await message.channel.send("I know have trust issues, but you cannot check your own teammates!")
         if (target.map((a) => db.get(`player_${a}`).role).includes("President")) return await message.channel.send("You cannot check the president.")
 
@@ -41,13 +41,19 @@ module.exports = {
             let { aura, role, team } = db.get(`player_${guy}`)
 
             if (guy.disguised === true) {
-                ;(aura = "Unknown"), (role = "Illusionist"), (team = "Solo")
+                aura = "Unknown"
+                role = "Illusionist"
+                team = "Solo"
             }
             if (!["Wolf Seer", "Sorcerer"].includes(guy.role) && guy.shamanned === true) {
-                ;(aura = "Evil"), (role = "Wolf Shaman"), (team = "Werewolf")
+                aura = "Evil"
+                role = "Wolf Shaman"
+                team = "Werewolf"
             }
             if (guy.role === "Sorcerer" && player.team !== "Werewolf") {
-                ;(aura = "Good"), (role = guy.fakeRole), (team = "Village")
+                aura = "Good"
+                role = guy.fakeRole
+                team = "Village"
             }
 
             result[`p${index + 1}`] = { aura, team, role }
@@ -64,7 +70,7 @@ module.exports = {
             await message.channel.send(`${getEmoji(results.startsWith("do") ? "detnotequal" : "detequal", client)} Player **${players.indexOf(target[0]) + 1} ${db.get(`player_${target[0]}`).username}** and **${players.indexOf(target[1]) + 1} ${db.get(`player_${target[1]}`).username}** __${results}__`)
         }
 
-        db.set(`player_${player.id}.uses`, 0)
+        if (player.role !== "Evil Detective") db.set(`player_${player.id}.uses`, 0)
 
         if (player.role === "Spirit Seer") {
             db.set(`player_${player.id}.target`, target)
@@ -74,6 +80,11 @@ module.exports = {
         if (player.role === "Sheriff") {
             db.set(`player_${player.id}.target`, target[0])
             await message.channel.send(`${getEmoji("snipe", client)} You decided to look out for **${players.indexOf(target[0].id) + 1} ${target[0].username}**!`)
+        }
+        
+        if (player.role === "Evil Detective") {
+            db.set(`player_${player.id}.target`, target)
+            await message.channel.send(`${getEmoji("evildetcheck", client)} You have decided to investigate **${target.map(p => `${players.indexOf(p)+1} ${db.get(`player_${p}`).username}`).join("** and **")}**. These players will be killed if they belong to different teams!`)
         }
     },
 }
