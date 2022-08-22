8090eb23a9599f3b9be198a450ca967490c29248
Hackante <kante.hagen@gmail.com>
2022-08-22 21:44:32 +0200

    closes #543

--- commands/narrator/enable.js
+++ commands/narrator/enable.js
@@ -8,7 +8,8 @@ module.exports = {
     narratorOnly: true,
     run: async (message, args, client) => {
         let mid = db.get("entermsg")
-        if (!args[0]) return message.channel.send(`Please specify the button to enable (join/spec)`)
+        if (!args[0]) return message.channel.send(`Please specify the button to enable (join/spec/all)`)
+        args[0] = args[0].toLowerCase()
         if (args[0] == "join") {
             message.guild.channels.cache
                 .find((c) => c.name == "enter-game")
@@ -20,7 +21,9 @@ module.exports = {
                     let specbutton = row.components[1]
                     let narrbutton = row.components[2]
                     jgbutton.disabled = false
-                    m.edit({ components: [{ type: 1, components: [jgbutton, specbutton, narrbutton] }] })
+                    m.edit({ components: [{ type: 1, components: [jgbutton, specbutton, narrbutton] }] }).then(() => {
+                        message.channel.send(`Enabled join button`)
+                    })
                 })
         } else if (args[0] == "spec") {
             message.guild.channels.cache
@@ -33,7 +36,25 @@ module.exports = {
                     let specbutton = row.components[1]
                     let narrbutton = row.components[2]
                     specbutton.disabled = false
-                    m.edit({ components: [{ type: 1, components: [jgbutton, specbutton, narrbutton] }] })
+                    m.edit({ components: [{ type: 1, components: [jgbutton, specbutton, narrbutton] }] }).then(() => {
+                        message.channel.send(`Enabled spec button`)
+                    })
+                })
+        } else if(args[0] === "all") {
+            message.guild.channels.cache
+                .find((c) => c.name == "enter-game")
+                .messages.fetch(mid)
+                .then((m) => {
+                    let allc = m.components
+                    let row = allc[0]
+                    let jgbutton = row.components[0]
+                    let specbutton = row.components[1]
+                    let narrbutton = row.components[2]
+                    jgbutton.disabled = false
+                    specbutton.disabled = false
+                    m.edit({ components: [{ type: 1, components: [jgbutton, specbutton, narrbutton] }] }).then(() => {
+                        message.channel.send(`Enabled all buttons`)
+                    })
                 })
         } else return message.channel.send(`I could not find the button.`)
     },
