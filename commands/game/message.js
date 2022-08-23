0b9584816665f1d8fd9cde52f3760070edbd39a1
Hackante <Hackante@users.noreply.github.com>
2022-08-23 20:34:33 +0000

    Prettified Code!

--- commands/game/message.js
+++ commands/game/message.js
@@ -31,12 +31,11 @@ module.exports = {
                 if (gamePhase % 3 == 0) return await message.channel.send("You can only send that kind of message during the day.")
                 if (player.uses == 0) return await message.channel.send("You already used up your ability!")
 
-                let wwChat = message.guild.channels.cache.find(c => c.name === "werewolves-chat")
-                wwChat.send(`${getEmoji("alpha_werewolf")} The Alpha Werewolf has sent a message: ${content}\n${message.guild.roles.cache.find(r => r.name === "Alive")}`)
+                let wwChat = message.guild.channels.cache.find((c) => c.name === "werewolves-chat")
+                wwChat.send(`${getEmoji("alpha_werewolf")} The Alpha Werewolf has sent a message: ${content}\n${message.guild.roles.cache.find((r) => r.name === "Alive")}`)
                 db.subtract(`player_${message.author.id}.uses`, 1)
                 return await message.channel.send("You have sent a message to the werewolves.")
             }
-
         } else {
             switch (message.channel.name) {
                 case "dead-chat":
@@ -44,34 +43,32 @@ module.exports = {
                     if (gamePhase % 3 != 0) return message.channel.send("You can only send messages to the medium(s) during the night.")
                     if (content.length > 1850) return await message.channel.send("The maximum amount of characters you can send is 1850! Please shorten your message.")
 
-
-                    let ps = players.filter(p => db.get(`player_${p}`).status === "Alive")
+                    let ps = players
+                        .filter((p) => db.get(`player_${p}`).status === "Alive")
                         .map((p) => db.get(`player_${p}`))
                         .filter((p) => ["Medium", "Ritualist"].includes(p.role) || ["Medium", "Ritualist"].includes(p?.dreamRole))
                     message.channel.send(`${getEmoji(player.corrupted ? "corrupt" : player.role.toLowerCase().replace(/\s/g, "_"), client)} ${players.includes(message.author.id) ? players.indexOf(message.author.id) + 1 : ""} ${player.username || message.author.username}: ${content}`)
                     ps.forEach((p) => {
                         if (!p.hypnotised && !p.jailed && !p.nightmared) {
-                            message.guild.channels.cache.get(p.channel)
-                                .send(`${getEmoji(player.corrupted ? "corrupt" : player.role.toLowerCase().replace(/\s/g, "_"), client)} ${players.includes(message.author.id) ? players.indexOf(message.author.id) + 1 : ""} ${player.username || message.author.username}: ${content}`)
+                            message.guild.channels.cache.get(p.channel).send(`${getEmoji(player.corrupted ? "corrupt" : player.role.toLowerCase().replace(/\s/g, "_"), client)} ${players.includes(message.author.id) ? players.indexOf(message.author.id) + 1 : ""} ${player.username || message.author.username}: ${content}`)
                         }
                     })
-                    break;
+                    break
                 case "jailed-chat":
                     if (!player.jailed) return
                     if (player.status !== "Alive") return message.channel.send("Listen to me, you need to be ALIVE to send messsages.")
                     if (gamePhase % 3 != 0) return message.channel.send("You can only send messages to the jailer during the night.")
                     if (content.length > 1850) return await message.channel.send("The maximum amount of characters you can send is 1850! Please shorten your message.")
 
-
                     let jailer = players.map((p) => db.get(`player_${p}`)).find((p) => p.role === "Jailer" && p.target === message.author.id)
                     if (jailer.hypnotized) jailer = players.map((p) => db.get(`player_${p}`)).find((p) => p.role === "Dreamcatcher" && p.target === jailer.id)
 
-                    message.guild.channels.cache.get(jailer.channel)
-                        .send(`**${getEmoji("jailerselect", client)} ${players.indexOf(message.author.id) + 1} ${db.get(`player_${message.author.id}`).username}**: ${content}`)
+                    message.guild.channels.cache.get(jailer.channel).send(`**${getEmoji("jailerselect", client)} ${players.indexOf(message.author.id) + 1} ${db.get(`player_${message.author.id}`).username}**: ${content}`)
                     message.channel.send(`**${getEmoji("jailerselect", client)} ${players.indexOf(message.author.id) + 1} ${db.get(`player_${message.author.id}`).username}**: ${content}`)
-                    break;
-                default: return
+                    break
+                default:
+                    return
             }
         }
-    }
+    },
 }
