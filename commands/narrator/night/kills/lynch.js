9f6dca213b5e88c3ac2cbb6c1df35372a8f69307
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-08-26 10:18:58 +0000

    Prettified Code!

--- commands/narrator/night/kills/lynch.js
+++ commands/narrator/night/kills/lynch.js
@@ -46,7 +46,7 @@ module.exports = async (client) => {
     console.log(`Results:`)
     console.log(sortedVotes)
 
-    guild.channels.cache.find(c => c.name === "commands")?.send(`So here are the results:\n\nAlive players: ${alivePlayers.length}\n\nPlayers required to lynch: ${Math.floor(alivePlayers.length / 2)}\n\n${sortedVotes.map(a => ` - <@${$a[0]}> had ${a[1]} votes on them.`).join("\n")}\n\nPlayer who gets killed: <@${sortedVotes[0][0]}>`)
+    guild.channels.cache.find((c) => c.name === "commands")?.send(`So here are the results:\n\nAlive players: ${alivePlayers.length}\n\nPlayers required to lynch: ${Math.floor(alivePlayers.length / 2)}\n\n${sortedVotes.map((a) => ` - <@${$a[0]}> had ${a[1]} votes on them.`).join("\n")}\n\nPlayer who gets killed: <@${sortedVotes[0][0]}>`)
 
     // check if the first 2 votes are same or if there are not enough votes
     if (db.get(`game.noVoting`) || sortedVotes.length === 0 || sortedVotes[0][1] < Math.floor(alivePlayers.length / 2) || (sortedVotes.length > 1 && sortedVotes[0][1] === sortedVotes[1][1])) {
