e3ed0f8e5d29261934c929017a05d1122a856a6a
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-08-31 17:04:29 +0800

    Bug fix

--- commands/narrator/playerinfo.js
+++ commands/narrator/playerinfo.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const db = require("quick.db")
 const { getEmoji } = require("../../config")
 
@@ -32,37 +30,3 @@ module.exports = {
         }
     },
 }
-=======
-=======
->>>>>>> be9fa80 (Prettified Code!)
-const db = require("quick.db")
-const { getEmoji } = require("../../config")
-
-module.exports = {
-    name: "playerinfo",
-    description: "Get the playerinfo.",
-    usage: `${process.env.PREFIX}playerinfo`,
-    gameOnly: true,
-    narratorOnly: true,
-    run: async (message, args, client) => {
-        let players = db.get(`players`) || []
-        const embed = { title: "Playerinfo", description: "", color: 0x648620 }
-        content = ""
-        for await (const p of players) {
-            let player = db.get(`player_${p}`)
-            let member = await message.guild.members.fetch(p)
-            if (player.allRoles?.length > 0) content += player.allRoles.map((r) => `${getEmoji(r.toLowerCase().replace(/\s/g, "_"), client)}`).join("")
-            else content += `${getEmoji(player.role.toLowerCase().replace(/\s/g, "_"), client)}`
-
-            content += ` ${players.indexOf(p) + 1}. ${member.user.tag}\n`
-        }
-
-        embed.description = content
-
-        message.channel.send({ embeds: [embed] })
-    },
-}
-<<<<<<< HEAD
->>>>>>> 458fee7 (Bug fixes)
-=======
->>>>>>> be9fa80 (Prettified Code!)
