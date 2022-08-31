b1a8aca7d2cf1093aeb32288a7683050fcefa2a7
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-08-31 20:25:10 +0800

    Instigator and couple fixes

--- commands/game/message.js
+++ commands/game/message.js
@@ -11,7 +11,6 @@ module.exports = {
         const gamePhase = db.get(`gamePhase`)
         const players = db.get(`players`) || []
         let player = db.get(`player_${message.author.id}`) || { status: "Dead" }
-        if (["Medium", "Ritualist", "Jailer"].includes(player.dreamRole)) player = db.get(`player_${player.target}`)
 
         if (!args) return await message.channel.send("You do know that you need to have some content right?")
         let content = args
@@ -21,7 +20,8 @@ module.exports = {
             .replace(/<([@#])+[&]?[\d]{10,20}>/g, "[ping]")
 
         if (message.channel.name.startsWith("priv")) {
-            if (!["Jailer", "Medium", "Ritualist", "Alpha Werewolf", "Sect Leader"].includes(player.role) && !["Jailer", "Medium", "Ritualist", "Alpha Werewolf", "Sect Leader"].includes(player.dreamRole)) return
+            if (!["Jailer", "Medium", "Ritualist", "Alpha Werewolf", "Sect Leader", "Instigator"].includes(player.role) && !["Jailer", "Medium", "Ritualist", "Alpha Werewolf", "Sect Leader", "Instigator"].includes(player.dreamRole)) return
+            if (["Medium", "Ritualist", "Jailer", "Alpha Werewolf", "Sect Leader", "Instigator"].includes(player.dreamRole)) player = db.get(`player_${player.target}`)
             if (player.status !== "Alive") return message.channel.send("Listen to me, you need to be ALIVE to send messsages.")
             if (player.jailed) return await message.channel.send("You are jailed. You cannot use your abilities while in jail!")
             if (player.nightmared) return await message.channel.send("You are nightmared. You cannot use your abilities while you're asleep.")
@@ -35,6 +35,19 @@ module.exports = {
                 wwChat.send(`${getEmoji("alpha_werewolf", client)} The Alpha Werewolf has sent a message: ${content}\n${message.guild.roles.cache.find((r) => r.name === "Alive")}`)
                 db.subtract(`player_${message.author.id}.uses`, 1)
                 return await message.channel.send("You have sent a message to the werewolves.")
+            }
+            if (player.role === "Instigator" || player?.dreamRole === "Instigator") {
+                if (gamePhase % 3 == 0) return await message.channel.send("You can only send that kind of message during the day.")
+                
+                if (!player.target || player.target?.length < 2 || player.target?.map(a => db.get(`player_${a}`)).filter(a => a.status === "Alive").length < 2) return await message.channel.send("You can't send messages to your recruits if they have died!")
+                player.target.forEach(p => {
+                    let guy = db.get(`player_${p}`)
+                    if (guy.status === "Alive") {
+                        let channel = message.guild.channels.cache.get(guy.channel)
+                        await channel.send(`${getEmoji("instigator", client)} ${players.indexOf(player.id) + 1} ${player.username}: ${content}`)
+                        await channel.send(`${message.guild.roles.cache.find(r => r.name === "Alive")}`)
+                    }
+                })
             }
 
             if (player.role === "Sect Leader" || player?.dreamRole == "Sect Leader") {
@@ -50,7 +63,7 @@ module.exports = {
                 if (gamePhase % 3 !== 0) return await message.channel.send("You can only send that kind of message during the night.")
                 let target = db.get(`player_${player.target}`)
                 if (!target) return await message.channel.send("Your target has to be alive for you to send messages!")
-                let jailerChat = message.guild.channels.cache.find((c) => c.name === "jailed-chat")
+                let jailerChat = message.guild.channels.cache.find(c => c.name === "jailed-chat")
                 await message.channel.send(`${getEmoji("jailer", client)} **Jailer**: ${content}`)
                 await jailerChat.send(`${getEmoji("jailer", client)} **Jailer**: ${content}`)
                 if (player.hypnotized) {
@@ -59,7 +72,7 @@ module.exports = {
                 }
             }
             if (["Medium", "Ritualist"].includes(player.role) || ["Medium", "Ritualist"].includes(player?.dreamRole)) {
-                let allDeads = players.map((a) => db.get(`player_${a}`)).filter((a) => ["Medium", "Ritualist"].includes(a.role) || ["Medium", "Ritualist"].includes(a.dreamRole))
+                let allDeads = players.map(a => db.get(`player_${a}`)).filter(a => ["Medium", "Ritualist"].includes(a.role) || ["Medium", "Ritualist"].includes(a.dreamRole))
                 message.channel.send(`${getEmoji(player.corrupted ? "corrupt" : player.role.toLowerCase().replace(/\s/g, "_"), client)} ${players.includes(message.author.id) ? players.indexOf(message.author.id) + 1 : ""} ${player.username || message.author.username}: ${content}`)
                 allDeads.forEach((p) => {
                     if (!p.jailed && !p.nightmared) {
