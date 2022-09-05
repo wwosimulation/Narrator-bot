5b9449b314aa286d91b3cdc5f67a84c0e4db53d0
3061LRTAGSPKJMORMRT <3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-09-05 02:14:33 +0000

    Prettified Code!

--- commands/narrator/clearbutton.js
+++ commands/narrator/clearbutton.js
@@ -8,13 +8,15 @@ module.exports = {
     usage: `${process.env.PREFIX}gwhost [supervisor] <game...>`,
     narratorOnly: true,
     run: async (message, args, client) => {
-
         if (args.length !== 1) return message.channel.send("Please give me the message id so I can remove the buttons from it.")
-        
-        let guild = client.guilds.cache.get(ids.server.sim)
-        let member = await guild.members.fetch(message.author.id)      
 
-        let m = await guild.channels.cache.get("606123818305585167").messages.fetch(args[0]).catch(() => null)
+        let guild = client.guilds.cache.get(ids.server.sim)
+        let member = await guild.members.fetch(message.author.id)
+
+        let m = await guild.channels.cache
+            .get("606123818305585167")
+            .messages.fetch(args[0])
+            .catch(() => null)
 
         if (!m) return message.channel.send("I could not find my message in <#606123818305585167>!")
 
@@ -27,6 +29,5 @@ module.exports = {
         m.edit({ components: m.components })
 
         message.channel.send("Done!")
-
     },
 }
