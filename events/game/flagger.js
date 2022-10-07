849f64147c18764bfab11c1dc13b1271ac6c32a2
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-10-07 17:10:30 +0800

    Flagger fix i guess

--- events/game/flagger.js
+++ events/game/flagger.js
@@ -27,9 +27,9 @@ module.exports = async (interaction) => {
 
     let message = await interaction.followUp({ content: `${getEmoji("flagger_protect", client)} Select a player below to protect`, components: [{ type: 1, components: [droppy1] }], fetchReply: true, ephemeral: true })
 
-    await createCollector(message, "target")
+    await createCollector1(message)
 
-    async function createCollector(msg, action) {
+    async function createCollector1(msg) {
         await msg
             .awaitMessageComponent()
             .then(async (i) => {
@@ -44,33 +44,47 @@ module.exports = async (interaction) => {
                     return
                 }
                 if (db.get(`player_${i.values[0]}`).status !== "Alive") return await i.editReply({ content: "This player is not alive!", ephemeral: true })
-                if (action === "target") {
-                    options.splice(
-                        options.findIndex((a) => a.value === i.values[0]),
-                        1
-                    )
+                
+                options.splice(
+                    options.findIndex((a) => a.value === i.values[0]),
+                    1
+                )
 
-                    droppy2.options.push(...options)
-                    droppy2.options.forEach((o) => {
-                        o.description = o.description.replace("Protect", "Redirect the attack to")
-                    })
-                }
-                db.set(`player_${interaction.user.id}.${action}`, i.values[0])
-                if (action === "target") await i.editReply({ content: `${getEmoji("flagger_kill", client)} Select a player below to redirect the attack`, components: [{ type: 1, components: [droppy2] }] })
-                if (action === "target") {
-                    return createCollector(msg, "redirect")
-                }
-                if (action === "redirect") {
-                    let teammates = fn.teammateCheck({ player: sk.id, target: i.values[0], db })
-                    if (teammates.couple) return await i.editReply({ content: "You can't redirect an attack to your own couple!", ephemeral: true })
-                    if (teammates.recruit) return await i.editReply({ content: "You can't redirect an attack to your own recruit!", ephemeral: true })
-                    if (teammates.instigator) return await i.editReply({ content: "You can't redirect an attack to the Instigator who recruited you!", ephemeral: true })
-                    if (db.get(`player_${i.values[0]}`).role === "President") return await i.editReply({ content: "You can't redirect an attack to the President!", ephemeral: true })
-
-                    await i.editReply({ content: "Done!", components: [] })
-                    await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have decided to protect **${players.indexOf(db.get(`player_${flagger.id}`).target) + 1} ${db.get(`player_${db.get(`player_${flagger.id}`).target}`).username}** by redirecting attacks to **${players.indexOf(i.values[0]) + 1} ${db.get(`player_${i.values[0]}`).username}**!` })
-                }
+                droppy2.options.push(...options)
+                droppy2.options.forEach((o) => {
+                    o.description = o.description.replace("Protect", "Redirect the attack to")
+                })
+                
+                db.set(`player_${interaction.user.id}.target`, i.values[0])
+                await i.editReply({ content: "Done!", components: [] })
+                let reply = await i.followUp({ content: `${getEmoji("flagger_kill", client)} Select a player below to redirect the attack`, components: [{ type: 1, components: [droppy2] }], ephemeral: true, fetchReply: true })
+                return createCollector2(reply)
+                
             })
             .catch(() => null)
+    }
+
+    async function createCollector2 (msg) {
+        await i.deferUpdate()
+        if (db.get(`gamePhase`) % 3 !== 0) return i.followUp({ content: "This action is no longer valid now!", ephemeral: true })
+        if (db.get(`player_${flagger.id}`).status !== "Alive") return i.followUp({ content: "You are not alive!", ephemeral: true })
+        if (i.values[0] === "cancel") {
+            db.delete(`player_${flagger.id}.target`)
+            db.delete(`player_${flagger.id}.redirect`)
+            await i.editReply({ content: "Done!", components: [] })
+            await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have sucessfully canceled your action!` })
+            return
+        }
+        if (db.get(`player_${i.values[0]}`).status !== "Alive") return await i.editReply({ content: "This player is not alive!", ephemeral: true })
+                
+        let teammates = fn.teammateCheck({ player: sk.id, target: i.values[0], db })
+        if (teammates.couple) return await i.editReply({ content: "You can't redirect an attack to your own couple!", ephemeral: true })
+        if (teammates.recruit) return await i.editReply({ content: "You can't redirect an attack to your own recruit!", ephemeral: true })
+        if (teammates.instigator) return await i.editReply({ content: "You can't redirect an attack to the Instigator who recruited you!", ephemeral: true })
+        if (db.get(`player_${i.values[0]}`).role === "President") return await i.editReply({ content: "You can't redirect an attack to the President!", ephemeral: true })
+
+        db.set(`player_${interaction.user.id}.redirect`, i.values[0])
+        await i.editReply({ content: "Done!", components: [] })
+        await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have decided to protect **${players.indexOf(db.get(`player_${flagger.id}`).target) + 1} ${db.get(`player_${db.get(`player_${flagger.id}`).target}`).username}** by redirecting attacks to **${players.indexOf(i.values[0]) + 1} ${db.get(`player_${i.values[0]}`).username}**!` })
     }
 }
