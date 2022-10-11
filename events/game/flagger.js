b608ae61335430902b1e9ea2e3ec86c4600a5473
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-10-11 23:09:16 +0800

    woops

--- events/game/flagger.js
+++ events/game/flagger.js
@@ -64,26 +64,30 @@ module.exports = async (interaction) => {
     }
 
     async function createCollector2(msg) {
-        await i.deferUpdate()
-        if (db.get(`gamePhase`) % 3 !== 0) return i.followUp({ content: "This action is no longer valid now!", ephemeral: true })
-        if (db.get(`player_${flagger.id}`).status !== "Alive") return i.followUp({ content: "You are not alive!", ephemeral: true })
-        if (i.values[0] === "cancel") {
-            db.delete(`player_${flagger.id}.target`)
-            db.delete(`player_${flagger.id}.redirect`)
+        msg.awaitMessageComponent()
+        .then(async i => {
+            await i.deferUpdate()
+            if (db.get(`gamePhase`) % 3 !== 0) return i.followUp({ content: "This action is no longer valid now!", ephemeral: true })
+            if (db.get(`player_${flagger.id}`).status !== "Alive") return i.followUp({ content: "You are not alive!", ephemeral: true })
+            if (i.values[0] === "cancel") {
+                db.delete(`player_${flagger.id}.target`)
+                db.delete(`player_${flagger.id}.redirect`)
+                await i.editReply({ content: "Done!", components: [] })
+                await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have sucessfully canceled your action!` })
+                return
+            }
+            if (db.get(`player_${i.values[0]}`).status !== "Alive") return await i.editReply({ content: "This player is not alive!", ephemeral: true })
+
+            let teammates = fn.teammateCheck({ player: sk.id, target: i.values[0], db })
+            if (teammates.couple) return await i.editReply({ content: "You can't redirect an attack to your own couple!", ephemeral: true })
+            if (teammates.recruit) return await i.editReply({ content: "You can't redirect an attack to your own recruit!", ephemeral: true })
+            if (teammates.instigator) return await i.editReply({ content: "You can't redirect an attack to the Instigator who recruited you!", ephemeral: true })
+            if (db.get(`player_${i.values[0]}`).role === "President") return await i.editReply({ content: "You can't redirect an attack to the President!", ephemeral: true })
+
+            db.set(`player_${interaction.user.id}.redirect`, i.values[0])
             await i.editReply({ content: "Done!", components: [] })
-            await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have sucessfully canceled your action!` })
-            return
-        }
-        if (db.get(`player_${i.values[0]}`).status !== "Alive") return await i.editReply({ content: "This player is not alive!", ephemeral: true })
-
-        let teammates = fn.teammateCheck({ player: sk.id, target: i.values[0], db })
-        if (teammates.couple) return await i.editReply({ content: "You can't redirect an attack to your own couple!", ephemeral: true })
-        if (teammates.recruit) return await i.editReply({ content: "You can't redirect an attack to your own recruit!", ephemeral: true })
-        if (teammates.instigator) return await i.editReply({ content: "You can't redirect an attack to the Instigator who recruited you!", ephemeral: true })
-        if (db.get(`player_${i.values[0]}`).role === "President") return await i.editReply({ content: "You can't redirect an attack to the President!", ephemeral: true })
-
-        db.set(`player_${interaction.user.id}.redirect`, i.values[0])
-        await i.editReply({ content: "Done!", components: [] })
-        await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have decided to protect **${players.indexOf(db.get(`player_${flagger.id}`).target) + 1} ${db.get(`player_${db.get(`player_${flagger.id}`).target}`).username}** by redirecting attacks to **${players.indexOf(i.values[0]) + 1} ${db.get(`player_${i.values[0]}`).username}**!` })
+            await i.followUp({ content: `${getEmoji("flagger_protect", client)} You have decided to protect **${players.indexOf(db.get(`player_${flagger.id}`).target) + 1} ${db.get(`player_${db.get(`player_${flagger.id}`).target}`).username}** by redirecting attacks to **${players.indexOf(i.values[0]) + 1} ${db.get(`player_${i.values[0]}`).username}**!` })
+        })
+        
     }
 }
