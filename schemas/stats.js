a92611b95344252c74ef653bd257f4ef3fab76ef
Gorott <Gorott@users.noreply.github.com>
2022-10-28 07:42:51 +0000

    Prettified Code!

--- schemas/stats.js
+++ schemas/stats.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-  games: { type: Array, default: [] }
+    games: { type: Array, default: [] },
 })
 
 module.exports = mongoose.model("players", schema)
