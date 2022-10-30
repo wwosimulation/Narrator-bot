ac47a1845f89d48b03e7f4a2b221b31c961852cd
Groot <67421407+Gorott@users.noreply.github.com>
2022-10-30 12:25:28 +0100

    forgot something

--- schemas/stats.js
+++ schemas/stats.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const schema = new mongoose.Schema({
     games: { type: Array, default: [] },
     members: { type: Array, default: [] },
+    previousFetch: { type: Array, default: [] },
     newFetch: { type: Date, default: Date.now() },
 })
 
